02c8e369cda03f48bb166d649ec3c8210051d80d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-02-26 18:24:58 -0500

    Meson setup script now creates build dir

--- meson/script/meson-setup.sh
+++ meson/script/meson-setup.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
-script_dir="${BASH_SOURCE%/*}"
+# See: https://gist.github.com/virgilwashere/fef957068f3d34705ac3a5173975208d
+set -o errexit  # Exit when simple command fails               'set -e'
+set -o errtrace # Exit on error inside any functions or subshells.
+set -o nounset  # Trigger error when expanding unset variables 'set -u'
+set -o pipefail # Do not hide errors within pipes              'set -o pipefail'
+IFS=$'\n\t'
 
-meson setup "$("${script_dir}"/print-setup-dir-args.sh)"
+project_root_dir="${BASH_SOURCE%/*}"
+project_build_dir="${project_root_dir}/build"
+
+arch="$(uname -m)"
+if [ "${arch}" == 'x86_64' ]; then
+    arch='x64'
+fi
+
+source "${project_root_dir}/meson/script/detect-os/detect-os.sh"
+os="$(os_name)"
+
+# $1 : Compiler. Ex : 'msvc'.
+# $2 : Build type. Ex : 'debug'.
+setup_build_dir() {
+    local compiler="${1}"
+    local build_type="${2}"
+
+    local build_dir="${project_build_dir}/${os}-${arch}-${compiler}-${build_type}"
+
+    rm -r "${build_dir}"
+    mkdir -p "${build_dir}"
+    meson setup "${build_dir}" "${project_root_dir}"
+}
+
+setup_build_dir 'msvc' 'debug'
--- meson/script/print-setup-dir-args.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-project_root_dir="${BASH_SOURCE%/*}/../../"
-
-# Build directory
-echo -n "${project_root_dir}/build"
-
-# Source directory
-echo -n "${project_root_dir}/"
