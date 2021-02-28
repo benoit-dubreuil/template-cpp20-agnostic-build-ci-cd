c0014a86b74c1ffd3c6918a13b859defe45fe01f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-02-28 11:58:39 -0500

    Throw an error when attempting to directly execute shell_head.sh

--- meson/script/shell_header.sh
+++ meson/script/shell_header.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/false "This script should be sourced in a shell, not executed directly"
+# Code reference : https://unix.stackexchange.com/a/424498
+# shellcheck disable=SC2096
 
-# See: https://gist.github.com/virgilwashere/fef957068f3d34705ac3a5173975208d
+# Code reference : https://gist.github.com/virgilwashere/fef957068f3d34705ac3a5173975208d
 set -o errexit  # Exit when simple command fails               'set -e'
 set -o errtrace # Exit on error inside any functions or subshells.
 set -o nounset  # Trigger error when expanding unset variables 'set -u'
