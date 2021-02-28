295f805cd1f334dc03041481b5bd257e36bdb0bd
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-02-28 16:56:08 -0500

    Removed the deprecated bash scripts

--- script/lib/vars-meson-structure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/false "This script should be sourced in a shell, not executed directly"
-# Code reference : https://unix.stackexchange.com/a/424498
-# shellcheck disable=SC2096
-
-# Don't export
-# shellcheck disable=SC2034
-
-readonly meson_name="meson"
-readonly meson_filename_build="${meson_name}.build"
