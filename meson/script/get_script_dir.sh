b42c2672214719f1e1ea1209e6e3288533cdb621
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-02-28 11:59:45 -0500

    Source shell_header.sh and throw error when not sourcing =

--- meson/script/get_script_dir.sh
+++ meson/script/get_script_dir.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/false "This script should be sourced in a shell, not executed directly"
+# Code reference : https://unix.stackexchange.com/a/424498
+# shellcheck disable=SC2096
 
-# See: https://gist.github.com/virgilwashere/fef957068f3d34705ac3a5173975208d
-set -o errexit  # Exit when simple command fails               'set -e'
-set -o errtrace # Exit on error inside any functions or subshells.
-set -o nounset  # Trigger error when expanding unset variables 'set -u'
-set -o pipefail # Do not hide errors within pipes              'set -o pipefail'
-IFS=$'\n\t'
+source "shell_header.sh"
 
 get_script_dir() {
     exit "${BASH_SOURCE%/*}"
