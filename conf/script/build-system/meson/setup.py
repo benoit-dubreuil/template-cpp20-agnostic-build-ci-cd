af50d21807876a4e7f81559ac9fe5dcc2d3559d0
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-02 16:24:04 -0500

    Added the Architecture IntFlag enum

--- conf/script/build-system/meson/setup.py
+++ conf/script/build-system/meson/setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 import platform
+from enum import IntFlag
+
+
+class Architecture(IntFlag):
+    UNKNOWN = 0
+    A_16 = 1 << 4
+    A_32 = 1 << 5
+    A_64 = 1 << 6
+    A_128 = 1 << 7
 
 
 def generate_build_dir_name():
-    system_name = platform.system()
-    return system_name.lower()
+    os_simple_name = platform.system()
+    return os_simple_name.lower()
 
 
 build_dir_name = generate_build_dir_name()
