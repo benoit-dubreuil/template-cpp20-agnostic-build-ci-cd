f1c20ef189f2fb2425cb9f00ec858c35d08d8c66
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-02 20:35:33 -0500

    Generate build types for build dir names

--- conf/script/build-system/meson/setup/setup.py
+++ conf/script/build-system/meson/setup/setup.py
@@ -8,6 +8,10 @@ import platform
 from data_model import *
 
 
+def fetch_os_name():
+    return platform.system().lower()
+
+
 def detect_arch():
     exclusive_max_word = sys.maxsize + 1
     word_size = exclusive_max_word.bit_length()
@@ -19,14 +23,27 @@ def arch_to_bit_name(arch: Architecture):
     return str(arch.value) + 'bit'
 
 
-def generate_build_dir_name():
-    dir_name_separator: Final = '-'
-
-    os_simple_name = platform.system().lower()
-    arch_bit_name = arch_to_bit_name(detect_arch())
-
-    return os_simple_name + dir_name_separator + arch_bit_name
+def fetch_arch_bit_name():
+    return arch_to_bit_name(detect_arch())
 
 
-build_dir_name = generate_build_dir_name()
-print(build_dir_name)
+def assemble_build_types():
+    return [p_build_type.value for p_build_type in BuildType]
+
+
+def generate_build_dir_name(build_type):
+    sep: Final = '-'
+
+    os_simple_name = fetch_os_name()
+    arch_bit_name = fetch_arch_bit_name()
+
+    return os_simple_name + sep + arch_bit_name + sep + build_type
+
+
+def generate_all_build_dir_names():
+    all_build_types = assemble_build_types()
+    return [generate_build_dir_name(build_type) for build_type in all_build_types]
+
+
+all_build_dir_names = generate_all_build_dir_names()
+print(*all_build_dir_names, sep='\n')
