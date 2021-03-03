cb3a1a3f29a4efcb9277b39435cbda5c368d548d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-02 19:57:07 -0500

    Added data_model

---
+++ conf/script/build-system/meson/setup/data_model.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from enum import Enum, IntFlag, unique
+
+
+@unique
+class Architecture(IntFlag):
+    UNKNOWN = 0
+    A_16 = 2 ** 4
+    A_32 = 2 ** 5
+    A_64 = 2 ** 6
+    A_128 = 2 ** 7
+
+
+class Compiler(Enum):
+    MSVC = 'msvc'
+    CLANG = 'clang'
+    GCC = 'gcc'
