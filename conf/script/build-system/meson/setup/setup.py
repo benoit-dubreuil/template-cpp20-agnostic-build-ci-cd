6486b6b0e0f2574fa80206962e668d668ced30c6
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-05 15:40:38 -0500

    Finished generate_all_build_dir_names

--- conf/script/build-system/meson/setup/setup.py
+++ conf/script/build-system/meson/setup/setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 from typing import Final
-from pprint import pp
 import sys
 import platform
 
 from data_model import *
 from compiler_reqs import CompilerReqs
+from compiler_version import CompilerVersion
 
 
 def fetch_os_name() -> str:
@@ -22,11 +22,6 @@ def fetch_filtered_compilers_reqs_by_os(os_family: OSFamily) -> list[CompilerReq
     return CompilerReqs.filter_by_os(all_compilers_reqs, os_family)
 
 
-def fetch_filtered_compilers_reqs() -> list[CompilerReqs]:
-    os_family = fetch_os_family()
-    return fetch_filtered_compilers_reqs_by_os(os_family)
-
-
 def detect_arch() -> Architecture:
     exclusive_max_word = sys.maxsize + 1
     word_size = exclusive_max_word.bit_length()
@@ -34,35 +29,36 @@ def detect_arch() -> Architecture:
     return Architecture(word_size)
 
 
-def arch_to_bit_name(arch: Architecture) -> str:
-    return str(arch.value) + 'bit'
+def assemble_build_types() -> list[BuildType]:
+    return list(BuildType)
 
 
-def fetch_arch_bit_name() -> str:
-    return arch_to_bit_name(detect_arch())
-
-
-def assemble_build_types() -> list[str]:
-    return [p_build_type.value for p_build_type in BuildType]
-
-
-def generate_build_dir_name(build_type: str) -> str:
+def generate_build_dir_name(os_family: OSFamily, compiler: Compiler, compiler_version: CompilerVersion, arch: Architecture, build_type: BuildType) -> str:
     sep: Final = '-'
 
-    os_simple_name = fetch_os_name()
-    arch_bit_name = fetch_arch_bit_name()
+    os_family_name = os_family.value
+    arch_bit_name = arch.arch_to_bit_name()
+    compiler_name = compiler.value
+    compiler_version_name = str(compiler_version)
+    build_type_name = build_type.value
 
-    return os_simple_name + sep + arch_bit_name + sep + build_type
+    return os_family_name + sep + arch_bit_name + sep + compiler_name + sep + compiler_version_name + sep + build_type_name
 
 
 def generate_all_build_dir_names() -> list[str]:
-    filtered_compilers_reqs_by_os = fetch_filtered_compilers_reqs()
-    pp(filtered_compilers_reqs_by_os)
-
+    os_family = fetch_os_family()
+    filtered_compilers_reqs_by_os = fetch_filtered_compilers_reqs_by_os(os_family)
     all_build_types = assemble_build_types()
+    arch = detect_arch()
 
-    return [generate_build_dir_name(build_type) for build_type in all_build_types]
+    build_dir_names = []
+    for compiler_reqs in filtered_compilers_reqs_by_os:
+        for build_type in all_build_types:
+            generated = generate_build_dir_name(os_family, compiler_reqs.compiler, compiler_reqs.version, arch, build_type)
+            build_dir_names.append(generated)
+
+    return build_dir_names
 
 
 all_build_dir_names = generate_all_build_dir_names()
-print(*all_build_dir_names, sep='\n')
+print(*all_build_dir_names, sep='\n', end='')
