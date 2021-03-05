32d46511f4ab10c0b7b9173a12913e1c6ca5d195
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-05 14:23:09 -0500

    Added auto_repr file

---
+++ conf/script/build-system/meson/setup/auto_repr.py
@@ -0,0 +1,10 @@
+# From https://stackoverflow.com/a/33800620/2924010
+def auto_repr(cls):
+    def __repr__(self):
+        return '%s(%s)' % (
+            type(self).__name__,
+            ', '.join('%s=%s' % item for item in vars(self).items())
+            )
+
+    cls.__repr__ = __repr__
+    return cls
--- conf/script/build-system/meson/setup/compiler_version.py
+++ conf/script/build-system/meson/setup/compiler_version.py
@@ -1,6 +1,8 @@
+from auto_repr import auto_repr
 from data_model import CompilerReqsSectionScheme
 
 
+@auto_repr
 class CompilerVersion:
     def __init__(self, major: int, minor: int = 0):
         self.major = major
@@ -8,9 +10,6 @@ class CompilerVersion:
 
     def __str__(self) -> str:
         return '%u.%u' % (self.major, self.minor)
-
-    def __repr__(self) -> str:
-        return 'CompilerVersion(%s)' % str(self)
 
     @classmethod
     def create_from_config_compiler_reqs_section(cls, config_compiler_reqs_section) -> 'CompilerVersion':
