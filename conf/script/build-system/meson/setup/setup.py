75a37f959b0abae98509757fbfc822976b82b733
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-05 15:02:54 -0500

    Added fetch_filtered_compilers_reqs methods

--- conf/script/build-system/meson/setup/setup.py
+++ conf/script/build-system/meson/setup/setup.py
@@ -17,6 +17,16 @@ def fetch_os_family() -> OSFamily:
     return OSFamily(fetch_os_name())
 
 
+def fetch_filtered_compilers_reqs_by_os(os_family: OSFamily) -> list[CompilerReqs]:
+    all_compilers_reqs = CompilerReqs.create_all_from_file()
+    return CompilerReqs.filter_by_os(all_compilers_reqs, os_family)
+
+
+def fetch_filtered_compilers_reqs() -> list[CompilerReqs]:
+    os_family = fetch_os_family()
+    return fetch_filtered_compilers_reqs_by_os(os_family)
+
+
 def detect_arch() -> Architecture:
     exclusive_max_word = sys.maxsize + 1
     word_size = exclusive_max_word.bit_length()
