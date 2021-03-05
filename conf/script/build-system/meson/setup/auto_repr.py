f23c23fa709da9587da388bb3ba251c04ba123b7
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-05 14:32:30 -0500

    Added auto_str

--- conf/script/build-system/meson/setup/auto_repr.py
+++ conf/script/build-system/meson/setup/auto_repr.py
@@ -8,3 +8,15 @@ def auto_repr(cls):
 
     cls.__repr__ = __repr__
     return cls
+
+
+# From https://stackoverflow.com/a/33800620/2924010
+def auto_str(cls):
+    def __str__(self):
+        return '%s(%s)' % (
+            type(self).__name__,
+            ', '.join('%s' % item[1] for item in vars(self).items())
+            )
+
+    cls.__str__ = __str__
+    return cls
