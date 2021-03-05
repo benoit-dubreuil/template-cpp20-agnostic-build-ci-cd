452c6dd1322a3831463006a72845ec26ac69be40
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-05 15:11:46 -0500

    Reformat

--- conf/script/build-system/meson/setup/data_model.py
+++ conf/script/build-system/meson/setup/data_model.py
@@ -4,10 +4,10 @@ from enum import Enum, IntFlag, unique
 @unique
 class Architecture(IntFlag):
     UNKNOWN = 0
-    A_16 = 2 ** 4
-    A_32 = 2 ** 5
-    A_64 = 2 ** 6
-    A_128 = 2 ** 7
+    A_16 = 2**4
+    A_32 = 2**5
+    A_64 = 2**6
+    A_128 = 2**7
 
     def arch_to_bit_name(self) -> str:
         return str(self.value) + 'bit'
