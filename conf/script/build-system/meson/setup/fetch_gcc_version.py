40312bcce2da652f1f42f60223a67dc473c0f5fd
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-07 16:06:41 -0500

    Verify the compiler path accessibility

--- conf/script/build-system/meson/setup/fetch_gcc_version.py
+++ conf/script/build-system/meson/setup/fetch_gcc_version.py
@@ -6,9 +6,17 @@ from pathlib import Path
 
 from data_model import Compiler
 from compiler_version import CompilerVersion
+from file_path_integrity import assure_file_path_integrity, cmd_exists
+
+
+def assure_compiler_path_integrity(compiler_path: Path):
+    if not cmd_exists(str(compiler_path)):
+        raise FileNotFoundError('Compiler at the supplied path does not exist or requires ungranted permissions')
 
 
 def fetch_gcc_version(compiler: Path) -> CompilerVersion:
+    assure_compiler_path_integrity(compiler)
+
     result: subprocess.CompletedProcess = subprocess.run(
         [compiler, '-dumpversion'], capture_output=True, text=True, check=True
     )
@@ -19,12 +27,12 @@ def fetch_gcc_version(compiler: Path) -> CompilerVersion:
 
 # Run as a script
 if __name__ == '__main__':
-    compiler = Compiler.GCC.value
+    compiler_arg = Compiler.GCC.value
 
     arg_parser = argparse.ArgumentParser(description='Fetches GCC compiler\'s version')
-    arg_parser.add_argument(compiler, type=Path, nargs='?', const=compiler, default=compiler, help='The GCC compiler\'s executable path')
+    arg_parser.add_argument(compiler_arg, type=Path, nargs='?', const=compiler_arg, default=compiler_arg, help='The GCC compiler\'s executable path')
 
     args = arg_parser.parse_args()
-    compiler = args.compiler
+    compiler: Path = getattr(args, compiler_arg)
 
     print(fetch_gcc_version(compiler), end='')
