b9f5de55e0407994b7fc9fbf1d5319934317cf00
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-07 15:00:57 -0500

    Parse gcc arg

--- conf/script/build-system/meson/setup/fetch_gcc_version.py
+++ conf/script/build-system/meson/setup/fetch_gcc_version.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import subprocess
+import argparse
+from pathlib import Path
 
 from data_model import Compiler
 from compiler_version import CompilerVersion
 
 
-def fetch_gcc_version() -> CompilerVersion:
+def fetch_gcc_version(compiler: Path) -> CompilerVersion:
     result: subprocess.CompletedProcess = subprocess.run(
-        [Compiler.GCC.value, '-dumpversion'], capture_output=True, text=True, check=True
+        [compiler, '-dumpversion'], capture_output=True, text=True, check=True
     )
 
     compiler_version_str: str = result.stdout.strip()
@@ -17,4 +19,12 @@ def fetch_gcc_version() -> CompilerVersion:
 
 # Run as a script
 if __name__ == '__main__':
-    print(fetch_gcc_version(), end='')
+    compiler = Compiler.GCC.value
+
+    arg_parser = argparse.ArgumentParser(description='Fetches GCC compiler\'s version')
+    arg_parser.add_argument(compiler, type=Path, nargs='?', const=compiler, default=compiler, help='The GCC compiler\'s executable path')
+
+    args = arg_parser.parse_args()
+    compiler = args.compiler
+
+    print(fetch_gcc_version(compiler), end='')
