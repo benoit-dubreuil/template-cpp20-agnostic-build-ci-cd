9c9f7603d85b21359f7bbde45b573892d578d585
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-07 11:16:32 -0500

    Imported the subprocess module

--- conf/script/build-system/meson/setup/fetch_gcc_version.py
+++ conf/script/build-system/meson/setup/fetch_gcc_version.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+import subprocess
+
 from data_model import Compiler
 import compiler_version
 
+
 def fetch_gcc_version():
     pass
+
 
 # Run as a script
 if __name__ == '__main__':
