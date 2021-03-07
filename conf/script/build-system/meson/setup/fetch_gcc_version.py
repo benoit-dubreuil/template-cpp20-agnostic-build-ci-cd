5d1998d1a7a56d88c8f18e50fbbd5b720da117a2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-07 11:44:52 -0500

    Fetch the GCC version as a string

--- conf/script/build-system/meson/setup/fetch_gcc_version.py
+++ conf/script/build-system/meson/setup/fetch_gcc_version.py
@@ -7,9 +7,14 @@ import compiler_version
 
 
 def fetch_gcc_version():
-    pass
+    result: subprocess.CompletedProcess = subprocess.run(
+        [Compiler.GCC.value, '-dumpversion'], capture_output=True, text=True, check=True
+    )
+
+    print('stdout:', result.stdout.strip(), end='')
 
 
 # Run as a script
 if __name__ == '__main__':
+    fetch_gcc_version()
     pass
