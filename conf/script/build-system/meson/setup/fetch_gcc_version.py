bada63173e0879f85ae48db0b2f2564bb3c106a8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-07 14:35:06 -0500

    Refactor

--- conf/script/build-system/meson/setup/fetch_gcc_version.py
+++ conf/script/build-system/meson/setup/fetch_gcc_version.py
@@ -10,12 +10,11 @@ def fetch_gcc_version() -> CompilerVersion:
     result: subprocess.CompletedProcess = subprocess.run(
         [Compiler.GCC.value, '-dumpversion'], capture_output=True, text=True, check=True
     )
-    
+
     compiler_version_str: str = result.stdout.strip()
     return CompilerVersion.create_from_str(compiler_version_str)
 
 
 # Run as a script
 if __name__ == '__main__':
-    gcc_version = fetch_gcc_version()
-    print(gcc_version, end='')
+    print(fetch_gcc_version(), end='')
