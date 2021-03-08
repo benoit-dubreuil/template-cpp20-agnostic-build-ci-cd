9af2a7835e6a17441d449c4e2a47fd3b43bdb149
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-07 21:56:37 -0500

    Integrated the latest cli_fetch_gnu_compiler_version function into gcc fetch vers

--- conf/script/build-system/meson/setup/fetch_gcc_version.py
+++ conf/script/build-system/meson/setup/fetch_gcc_version.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
 
-import argparse
-from pathlib import Path
-
 from data_model import Compiler
-from fetch_version_of_gnu_compatible_compiler import fetch_gnu_compiler_version
+from fetch_version_of_gnu_compatible_compiler import cli_fetch_gnu_compiler_version
 
 # Run as a script
 if __name__ == '__main__':
-    compiler_arg = Compiler.GCC.value
-
-    arg_parser = argparse.ArgumentParser(description='Fetches GCC compiler\'s version')
-    arg_parser.add_argument(compiler_arg, type=Path, nargs='?', const=compiler_arg, default=compiler_arg, help='The GCC compiler\'s executable path')
-
-    args = arg_parser.parse_args()
-    compiler: Path = getattr(args, compiler_arg)
-
-    print(fetch_gnu_compiler_version(compiler), end='')
+    cli_fetch_gnu_compiler_version(Compiler.GCC)
