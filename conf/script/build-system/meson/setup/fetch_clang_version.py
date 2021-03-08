a7e7f2ccccf4e2c90eb2dfa4acc4a6cfdece7457
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-07 21:58:45 -0500

    Added script to fetch installed clang's version

---
+++ conf/script/build-system/meson/setup/fetch_clang_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from data_model import Compiler
+from fetch_version_of_gnu_compatible_compiler import cli_fetch_gnu_compiler_version
+
+# Run as a script
+if __name__ == '__main__':
+    cli_fetch_gnu_compiler_version(Compiler.CLANG)
