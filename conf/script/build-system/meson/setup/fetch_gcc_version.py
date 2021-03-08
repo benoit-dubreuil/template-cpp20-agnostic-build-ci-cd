882dcc299a27115223e7609342d26b65e3a70ba1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-07 21:49:19 -0500

    Integrated the newest fetch_version_of_gnu_compatible_compiler script

--- conf/script/build-system/meson/setup/fetch_gcc_version.py
+++ conf/script/build-system/meson/setup/fetch_gcc_version.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python3
 
-import subprocess
 import argparse
 from pathlib import Path
 
 from data_model import Compiler
-from compiler_version import CompilerVersion
-from file_path_integrity import assure_file_path_integrity, cmd_exists
-
-
-def assure_compiler_path_integrity(compiler_path: Path):
-    if not cmd_exists(str(compiler_path)):
-        raise FileNotFoundError('Compiler at the supplied path does not exist or requires ungranted permissions')
-
-
-def fetch_gcc_version(compiler: Path) -> CompilerVersion:
-    assure_compiler_path_integrity(compiler)
-
-    result: subprocess.CompletedProcess = subprocess.run(
-        [compiler, '-dumpversion'], capture_output=True, text=True, check=True
-    )
-
-    compiler_version_str: str = result.stdout.strip()
-    return CompilerVersion.create_from_str(compiler_version_str)
-
+from fetch_version_of_gnu_compatible_compiler import fetch_gnu_compiler_version
 
 # Run as a script
 if __name__ == '__main__':
@@ -35,4 +16,4 @@ if __name__ == '__main__':
     args = arg_parser.parse_args()
     compiler: Path = getattr(args, compiler_arg)
 
-    print(fetch_gcc_version(compiler), end='')
+    print(fetch_gnu_compiler_version(compiler), end='')
