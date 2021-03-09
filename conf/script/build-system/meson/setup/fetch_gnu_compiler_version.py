e0c7267d05c460c4da600498b8120c6b2915607a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-09 10:26:59 -0500

    Added documentation

--- conf/script/build-system/meson/setup/fetch_gnu_compiler_version.py
+++ conf/script/build-system/meson/setup/fetch_gnu_compiler_version.py
@@ -8,6 +8,10 @@ from file_path_integrity import cmd_exists
 
 
 def assure_compiler_path_integrity(compiler_path: Path):
+    """Assures the integrity of the supplied :param:`compiler_path`
+
+    :param compiler_path: The path to the compiler executable file. It must not be a directory.
+    """
     if not cmd_exists(str(compiler_path)):
         raise FileNotFoundError('Compiler at the supplied path does not exist or requires ungranted permissions')
 
