16c8d10a0ba26a584a2a359eada1149910da2689
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-09 10:57:23 -0500

    Extracted the _fetch_raw_gnu_compiler_version func

--- conf/script/build-system/meson/setup/fetch_gnu_compiler_version.py
+++ conf/script/build-system/meson/setup/fetch_gnu_compiler_version.py
@@ -1,20 +1,25 @@
 import argparse
 import subprocess
 from pathlib import Path
+from typing import AnyStr
 
 from compiler_version import CompilerVersion
 from data_model import Compiler
-from fetch_compiler_version import assure_compiler_path_integrity, interpret_fetched_compiler_version
+from fetch_compiler_version import assure_compiler_path_integrity, fetch_compiler_version
 
 
-def fetch_gnu_compiler_version(compiler: Path) -> CompilerVersion:
+def _fetch_raw_gnu_compiler_version(compiler: Path) -> AnyStr:
     assure_compiler_path_integrity(compiler)
 
     result: subprocess.CompletedProcess = subprocess.run(
         [compiler, '-dumpversion'], capture_output=True, text=True, check=True
     )
 
-    return interpret_fetched_compiler_version(result.stdout)
+    return result.stdout
+
+
+def fetch_gnu_compiler_version(compiler: Path) -> CompilerVersion:
+    return fetch_compiler_version(compiler, _fetch_raw_gnu_compiler_version)
 
 
 def cli_fetch_gnu_compiler_version(compiler: Compiler):
