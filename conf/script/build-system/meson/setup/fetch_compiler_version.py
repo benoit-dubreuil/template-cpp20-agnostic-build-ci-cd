97fea9c72adbc99d7de1a527a952a082d4141aa5
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-09 10:32:32 -0500

    Extracted the assure_compiler_path_integrity function

--- conf/script/build-system/meson/setup/fetch_compiler_version.py
+++ conf/script/build-system/meson/setup/fetch_compiler_version.py
@@ -1,5 +1,16 @@
 import colorama
+from pathlib import Path
+from file_path_integrity import cmd_exists
 
 
 def cli_init():
     colorama.init()
+
+
+def assure_compiler_path_integrity(compiler_path: Path):
+    """Assures the integrity of the supplied :param:`compiler_path`
+
+    :param compiler_path: The path to the compiler executable file. It must not be a directory.
+    """
+    if not cmd_exists(str(compiler_path)):
+        raise FileNotFoundError('Compiler at the supplied path does not exist or requires ungranted permissions')
--- conf/script/build-system/meson/setup/fetch_gnu_compiler_version.py
+++ conf/script/build-system/meson/setup/fetch_gnu_compiler_version.py
@@ -4,16 +4,7 @@ from pathlib import Path
 
 from compiler_version import CompilerVersion
 from data_model import Compiler
-from file_path_integrity import cmd_exists
-
-
-def assure_compiler_path_integrity(compiler_path: Path):
-    """Assures the integrity of the supplied :param:`compiler_path`
-
-    :param compiler_path: The path to the compiler executable file. It must not be a directory.
-    """
-    if not cmd_exists(str(compiler_path)):
-        raise FileNotFoundError('Compiler at the supplied path does not exist or requires ungranted permissions')
+from fetch_compiler_version import assure_compiler_path_integrity
 
 
 def fetch_gnu_compiler_version(compiler: Path) -> CompilerVersion:
