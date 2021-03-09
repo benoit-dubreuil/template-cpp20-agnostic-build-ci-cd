528911dd3596167e0da414fae69aa0537d5d04b3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-09 12:08:35 -0500

    Integrated cli_fetch_compiler_version_with_default_path into cli_fetch_gnu_compiler_version

--- conf/script/build-system/meson/setup/cli_fetch_gnu_compiler_version.py
+++ conf/script/build-system/meson/setup/cli_fetch_gnu_compiler_version.py
@@ -1,17 +1,7 @@
-import argparse
-from pathlib import Path
-
+from cli_fetch_compiler_version import cli_fetch_compiler_version_with_default_path
 from data_model import Compiler
 from fetch_gnu_compiler_version import fetch_gnu_compiler_version
 
 
 def cli_fetch_gnu_compiler_version(compiler: Compiler):
-    compiler_arg = compiler.value
-
-    arg_parser = argparse.ArgumentParser(description=f'Fetches {compiler.name} compiler\'s version')
-    arg_parser.add_argument(compiler_arg, type=Path, nargs='?', const=compiler_arg, default=compiler_arg, help=f'The {compiler.name} compiler\'s executable path')
-
-    args = arg_parser.parse_args()
-    compiler_path = getattr(args, compiler_arg)
-
-    print(fetch_gnu_compiler_version(compiler_path), end='')
+    cli_fetch_compiler_version_with_default_path(compiler, fetch_gnu_compiler_version)
