e650843f1468d1eb2dffb832f23c0aff6ef52241
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-09 11:43:50 -0500

    Extracted and generalised cli_fetch_compiler_version func

--- conf/script/build-system/meson/setup/cli_fetch_compiler_version.py
+++ conf/script/build-system/meson/setup/cli_fetch_compiler_version.py
@@ -1,5 +1,32 @@
+import argparse
+from pathlib import Path
+from typing import Callable
+
 import colorama
+from overload import overload
+
+from compiler_version import CompilerVersion
+from data_model import Compiler
 
 
 def cli_init():
     colorama.init()
+
+
+@overload
+def cli_fetch_compiler_version(compiler: Compiler, default_compiler_path: Path or None, fetch_compiler_version_func: Callable[[Path], CompilerVersion]) -> None:
+    compiler_arg = compiler.value
+
+    arg_parser = argparse.ArgumentParser(description=f'Fetches {compiler.name} compiler\'s version')
+    arg_parser.add_argument(compiler_arg, type=Path, nargs='?', const=default_compiler_path, default=default_compiler_path, help=f'The {compiler.name} compiler\'s executable path')
+
+    args = arg_parser.parse_args()
+    compiler_path: Path or None = getattr(args, compiler_arg)
+    compiler_version = fetch_compiler_version_func(compiler_path)
+
+    print(compiler_version, end='')
+
+
+@cli_fetch_compiler_version.add
+def cli_fetch_compiler_version(compiler: Compiler, fetch_compiler_version_func: Callable[[Path], CompilerVersion]) -> None:
+    cli_fetch_compiler_version(compiler, compiler.value, fetch_compiler_version_func)
