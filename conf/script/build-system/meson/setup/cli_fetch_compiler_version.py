5ab8256ebe371b6b76dca589d69e01df6a3a5916
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-09 12:08:04 -0500

    Removed overload

--- conf/script/build-system/meson/setup/cli_fetch_compiler_version.py
+++ conf/script/build-system/meson/setup/cli_fetch_compiler_version.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from typing import Callable
 
 import colorama
-from overload import overload
 
 from compiler_version import CompilerVersion
 from data_model import Compiler
@@ -13,7 +12,7 @@ def cli_init():
     colorama.init()
 
 
-@overload
+def cli_fetch_compiler_version(compiler: Compiler, fetch_compiler_version_func: Callable[[Path or None], CompilerVersion], default_compiler_path: Path or None = None) -> None:
 def cli_fetch_compiler_version(compiler: Compiler, default_compiler_path: Path or None, fetch_compiler_version_func: Callable[[Path], CompilerVersion]) -> None:
     compiler_arg = compiler.value
 
@@ -27,6 +26,6 @@ def cli_fetch_compiler_version(compiler: Compiler, default_compiler_path: Path o
     print(compiler_version, end='')
 
 
-@cli_fetch_compiler_version.add
+def cli_fetch_compiler_version_with_default_path(compiler: Compiler, fetch_compiler_version_func: Callable[[Path], CompilerVersion]) -> None:
 def cli_fetch_compiler_version(compiler: Compiler, fetch_compiler_version_func: Callable[[Path], CompilerVersion]) -> None:
     cli_fetch_compiler_version(compiler, compiler.value, fetch_compiler_version_func)
