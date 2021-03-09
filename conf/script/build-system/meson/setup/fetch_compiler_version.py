645828ea9d72365915cb206cc62a84554a520b50
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-09 10:52:12 -0500

    Added a more generic fetch_compiler_version function

--- conf/script/build-system/meson/setup/fetch_compiler_version.py
+++ conf/script/build-system/meson/setup/fetch_compiler_version.py
@@ -1,5 +1,6 @@
 import colorama
 from pathlib import Path
+from typing import NoReturn, AnyStr, Callable
 
 from file_path_integrity import cmd_exists
 from compiler_version import CompilerVersion
@@ -9,7 +10,7 @@ def cli_init():
     colorama.init()
 
 
-def assure_compiler_path_integrity(compiler_path: Path):
+def assure_compiler_path_integrity(compiler_path: Path) -> NoReturn:
     """Assures the integrity of the supplied :param:`compiler_path`
 
     :param compiler_path: The path to the compiler executable file. It must not be a directory.
@@ -18,5 +19,10 @@ def assure_compiler_path_integrity(compiler_path: Path):
         raise FileNotFoundError('Compiler at the supplied path does not exist or requires ungranted permissions')
 
 
-def interpret_fetched_compiler_version(compiler_version_str: str) -> CompilerVersion:
+def interpret_fetched_compiler_version(compiler_version_str: AnyStr) -> CompilerVersion:
     return CompilerVersion.create_from_str(compiler_version_str.strip())
+
+
+def fetch_compiler_version(compiler: Path, fetch_compiler_version_func: Callable[[Path], AnyStr]) -> CompilerVersion:
+    compiler_version_str: AnyStr = fetch_compiler_version_func(compiler)
+    return interpret_fetched_compiler_version(compiler_version_str)
