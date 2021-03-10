8c3426ca4fad17362dc0333a37ed36b0233e04ce
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-10 14:39:00 -0500

    Renamed to cli

--- conf/script/build_system/compiler/host/get_info/version/msvc/__main__.py
+++ conf/script/build_system/compiler/host/get_info/version/msvc/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import cli_fetch_compiler_info
-from cli_fetch_msvc_version import cli_fetch_msvc_version
+from cli import cli_fetch_msvc_version
 
 # Run as a script
 if __name__ == '__main__':
