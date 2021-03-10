00a9e433228325495c72b80def3ed9f55a98da50
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-10 11:09:29 -0500

    Added find_msvc_location.py

---
+++ conf/script/build-system/meson/setup/find_msvc_location.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import cli_fetch_compiler_info
+
+from cli_find_msvc_location import cli_find_msvc_location
+
+# Run as a script
+if __name__ == '__main__':
+    cli_fetch_compiler_info.cli_init()
+    cli_find_msvc_location()
