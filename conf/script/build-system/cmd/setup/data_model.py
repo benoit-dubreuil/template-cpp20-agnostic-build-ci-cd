f92be1b10effc35dbd3cc7846c344e6e38d4c99d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-10 13:56:25 -0500

    Extracted architecture enum

--- conf/script/build-system/cmd/setup/data_model.py
+++ conf/script/build-system/cmd/setup/data_model.py
@@ -1,16 +1,4 @@
-from enum import Enum, IntFlag, unique
-
-
-@unique
-class Architecture(IntFlag):
-    UNKNOWN = 0
-    A_16 = 2 ** 4
-    A_32 = 2 ** 5
-    A_64 = 2 ** 6
-    A_128 = 2 ** 7
-
-    def arch_to_bit_name(self) -> str:
-        return str(self.value) + 'bit'
+from enum import Enum, unique
 
 
 @unique
