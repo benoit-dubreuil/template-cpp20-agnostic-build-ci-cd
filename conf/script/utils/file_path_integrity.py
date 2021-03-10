4a78d1e933c07f33b84b65eabbbcb68e8ad9ca1a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-10 14:00:15 -0500

    Extracted cmd_integrity script

--- conf/script/utils/file_path_integrity.py
+++ conf/script/utils/file_path_integrity.py
@@ -1,4 +1,3 @@
-import shutil
 from pathlib import Path
 
 
@@ -7,16 +6,3 @@ def assure_file_path_integrity(file_path: Path):
         if file_path.is_dir():
             raise IsADirectoryError()
         raise FileNotFoundError()
-
-
-# From https://stackoverflow.com/a/28909933/2924010
-def cmd_exists(cmd) -> bool:
-    return shutil.which(cmd) is not None
-
-
-def get_cmd_path(cmd) -> (Path, bool):
-    cmd_path_str = shutil.which(cmd)
-    cmd_path = Path(cmd_path_str)
-    exists = cmd_path_str is None
-
-    return cmd_path, exists
