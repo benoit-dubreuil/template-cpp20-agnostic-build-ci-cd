a4c4f7cd45f478583f33170e6be81c3f3bef7c7f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-10 11:13:49 -0500

    Refactor

--- conf/script/build-system/meson/setup/fetch_msvc_version_impl.py
+++ conf/script/build-system/meson/setup/fetch_msvc_version_impl.py
@@ -11,7 +11,7 @@ _PROP_VERSION: Final[str] = 'installationVersion'
 
 def fetch_msvc_version(compiler_installation_path: Optional[Path] = None) -> Optional[CompilerVersion]:
     interpreted_compiler_version: Optional[CompilerVersion] = None
-    compiler_installation_path = find_msvc_installation_path() if compiler_installation_path is None else compiler_installation_path
+    compiler_installation_path = find_msvc_installation_path(compiler_installation_path)
 
     if compiler_installation_path is not None:
         fetched_compiler_version: Optional[str] = vswhere.find_first(prop=_PROP_VERSION, path=compiler_installation_path)
