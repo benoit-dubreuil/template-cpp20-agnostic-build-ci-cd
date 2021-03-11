e4674c696fcc32faa48a9b30ed58591b43cc4a13
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 11:35:16 -0500

    Bug fix : wrong lambda param

--- conf/script/build_system/compiler/host/get_info/version/gnu/cli.py
+++ conf/script/build_system/compiler/host/get_info/version/gnu/cli.py
@@ -3,8 +3,7 @@ from build_system.compiler.compiler import Compiler
 from build_system.compiler.host.get_info import cli
 
 from build_system.compiler.host.get_info.version import gnu
-import build_system.compiler.host.get_info.version.gnu.fetch_gnu_compiler_version
 
 
 def cli_fetch_gnu_compiler_version(compiler: Compiler):
-    cli.cli_fetch_compiler_info_with_default_path(compiler, gnu.fetch.fetch_compiler_version)
+    cli.cli_fetch_compiler_info_with_default_path(compiler, gnu.fetch)
