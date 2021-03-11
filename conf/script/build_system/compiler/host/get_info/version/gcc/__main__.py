0331a8c2fcd26ec7c4069e96df8e207c01852979
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 11:40:19 -0500

    Simplified imports

--- conf/script/build_system/compiler/host/get_info/version/gcc/__main__.py
+++ conf/script/build_system/compiler/host/get_info/version/gcc/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import build_system.compiler.host.get_info.version.gnu.cli
+from build_system.compiler.host.get_info.version import gnu
 from build_system.compiler.compiler import Compiler
 from build_system.compiler.host.get_info import cli
 from build_system.compiler.host.get_info import version
--- conf/script/build_system/compiler/host/get_info/version/gnu/cli.py
+++ conf/script/build_system/compiler/host/get_info/version/gnu/cli.py
@@ -1,8 +1,8 @@
 from build_system.compiler.compiler import Compiler
 
 from build_system.compiler.host.get_info import cli
-from build_system.compiler.host.get_info.version import gnu
+from build_system.compiler.host.get_info import version
 
 
 def cli_fetch_gnu_compiler_version(compiler: Compiler):
-    cli.cli_fetch_compiler_info_with_default_path(compiler, gnu.fetch)
+    cli.cli_fetch_compiler_info_with_default_path(compiler, version.gnu.fetch)
