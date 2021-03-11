a3ddaf432507e3a57233b7fad8ccf246f1e16a0f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 10:54:08 -0500

    Fixed imports

--- conf/script/build_system/compiler/host/get_info/version/clang/__main__.py
+++ conf/script/build_system/compiler/host/get_info/version/clang/__main__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import cli_fetch_compiler_info
-from cli_fetch_gnu_compiler_version import cli_fetch_gnu_compiler_version
-from data_model import Compiler
+import build_system.compiler.host.get_info.version.gnu.cli
+from build_system.compiler.compiler import Compiler
+from build_system.compiler.host.get_info import cli
+from build_system.compiler.host.get_info import version
 
 # Run as a script
 if __name__ == '__main__':
-    cli_fetch_compiler_info.cli_init()
-    cli_fetch_gnu_compiler_version(Compiler.CLANG)
+    cli.cli_init()
+    version.gnu.cli.cli_fetch_gnu_compiler_version(Compiler.CLANG)
