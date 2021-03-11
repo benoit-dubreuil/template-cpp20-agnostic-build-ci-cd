31e925d9997c3dd71e443d10dcb31c4a9819ec3e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 16:52:08 -0500

    Finished cli

--- conf/script/build_system/cmd/hierarchy/find_root/cli.py
+++ conf/script/build_system/cmd/hierarchy/find_root/cli.py
@@ -8,12 +8,13 @@ def get_error_formatted_msg_root_not_found() -> str:
     return utils.cli.format_error_msg(hierarchy.find_root.get_error_msg_root_not_found())
 
 
-def find_root() -> Path:
+def find_root():
     arg_parser = argparse.ArgumentParser(
         description=f"Fetches the project's root folder, where the '{hierarchy.find_root.VCS_DIR_NAME}' is. It searches recursively parent folders upwards.")
 
     try:
-        return hierarchy.find_root.find_root(get_error_formatted_msg_root_not_found)
+        project_root: Path = hierarchy.find_root.find_root(get_error_formatted_msg_root_not_found)
+        print(project_root)
     except FileNotFoundError as raised_exception:
         raised_exception_msg = str(raised_exception)
         arg_parser.error(raised_exception_msg)
