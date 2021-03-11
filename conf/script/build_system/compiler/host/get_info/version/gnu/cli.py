9e390f0e8ee3af672ae46334c3d7d48fb0b83045
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-10 20:35:52 -0500

    Refixed imports

--- conf/script/build_system/compiler/host/get_info/version/gnu/cli.py
+++ conf/script/build_system/compiler/host/get_info/version/gnu/cli.py
@@ -3,7 +3,7 @@ from build_system.compiler.compiler import Compiler
 from build_system.compiler.host.get_info import cli
 
 from build_system.compiler.host.get_info.version import gnu
-from build_system.compiler.host.get_info.version.gnu.fetch_gnu_compiler_version import fetch_gnu_compiler_version
+import build_system.compiler.host.get_info.version.gnu.fetch_gnu_compiler_version
 
 
 def cli_fetch_gnu_compiler_version(compiler: Compiler):
