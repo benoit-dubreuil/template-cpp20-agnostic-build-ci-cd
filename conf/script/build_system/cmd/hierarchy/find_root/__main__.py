53d634ee5ad18757d7b8e438ae7de819af933d08
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 16:36:37 -0500

    Completed main

--- conf/script/build_system/cmd/hierarchy/find_root/__main__.py
+++ conf/script/build_system/cmd/hierarchy/find_root/__main__.py
@@ -1,5 +1,7 @@
 import utils.cli
+from build_system.cmd.hierarchy.find_root import find_root
+from build_system.cmd.hierarchy.find_root import cli
 
 if __name__ == '__main__':
     utils.cli.init()
-    # TODO
+    find_root(cli.get_error_formatted_msg_root_not_found)
