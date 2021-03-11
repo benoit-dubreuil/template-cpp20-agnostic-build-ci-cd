1406ceb38a35da03127d444b3bf356ee0d469edb
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 16:19:28 -0500

    Added exceptions for finding the root

--- conf/script/build_system/cmd/hierarchy/find_root/find_root.py
+++ conf/script/build_system/cmd/hierarchy/find_root/find_root.py
@@ -1,3 +1,16 @@
-from typing import Final
+from pathlib import Path
+from typing import Callable, Final, NoReturn
 
 _VCS_DIR_NAME: Final[str] = '.git'
+
+
+def get_error_msg_root_not_found() -> str:
+    return 'Root directory not found'
+
+
+def _error_root_not_found(get_error_msg: Callable[[], str] = get_error_msg_root_not_found) -> NoReturn:
+    raise FileNotFoundError(get_error_msg())
+
+
+def find_root(get_error_msg: Callable[[], str] = get_error_msg_root_not_found) -> Path:
+    _error_root_not_found(get_error_msg)
