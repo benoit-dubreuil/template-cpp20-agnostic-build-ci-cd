185f222a17d9ae1587f6368f8c26e9ad263d199e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 16:30:56 -0500

    Added formatted error msg func

---
+++ conf/script/build_system/cmd/hierarchy/find_root/cli.py
@@ -0,0 +1,6 @@
+import utils.cli
+from build_system.cmd.hierarchy.find_root import find_root
+
+
+def get_error_formatted_msg_root_not_found() -> str:
+    return utils.cli.format_error_msg(find_root.get_error_msg_root_not_found())
