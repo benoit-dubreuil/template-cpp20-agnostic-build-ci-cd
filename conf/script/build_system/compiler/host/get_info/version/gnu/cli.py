8a76488488fa117346af9ad0048abf9f41251bee
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 12:19:37 -0500

    Fixed var name

--- conf/script/build_system/compiler/host/get_info/version/gnu/cli.py
+++ conf/script/build_system/compiler/host/get_info/version/gnu/cli.py
@@ -3,4 +3,4 @@ from build_system.compiler.host.get_info import cli, version
 
 
 def cli_fetch_gnu_compiler_version(compiler_family: CompilerFamily):
-    cli.cli_fetch_compiler_info_with_default_path(compiler, version.gnu.fetch)
+    cli.cli_fetch_compiler_info_with_default_path(compiler_family, version.gnu.fetch)
