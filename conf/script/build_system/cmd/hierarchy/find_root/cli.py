0e1f6d7447f34f86c9f9bce450390793d08da02e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 16:45:01 -0500

    Use arg_parser to throw CLI error

--- conf/script/build_system/cmd/hierarchy/find_root/cli.py
+++ conf/script/build_system/cmd/hierarchy/find_root/cli.py
@@ -1,6 +1,18 @@
 import utils.cli
-from build_system.cmd.hierarchy import find_root
+from build_system.cmd import hierarchy
+from pathlib import Path
+import argparse
 
 
 def get_error_formatted_msg_root_not_found() -> str:
-    return utils.cli.format_error_msg(find_root.get_error_msg_root_not_found())
+    return utils.cli.format_error_msg(hierarchy.find_root.get_error_msg_root_not_found())
+
+
+def find_root() -> Path:
+    arg_parser = argparse.ArgumentParser()
+
+    try:
+        return hierarchy.find_root.find_root(get_error_formatted_msg_root_not_found)
+    except FileNotFoundError as raised_exception:
+        raised_exception_msg = str(raised_exception)
+        arg_parser.error(raised_exception_msg)
