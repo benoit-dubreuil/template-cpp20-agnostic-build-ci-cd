d51232627ca650e17e30e9bf4055575405e67052
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 21:20:23 -0500

    Added is_dir_root func

--- conf/script/build_system/cmd/hierarchy/find_root/find_root.py
+++ conf/script/build_system/cmd/hierarchy/find_root/find_root.py
@@ -8,6 +8,13 @@ def get_error_msg_root_not_found() -> str:
     return 'Root directory not found'
 
 
+def is_dir_root(root_dir: Path) -> bool:
+    assert root_dir.is_dir()
+
+    vcs_dir = root_dir / VCS_DIR_NAME
+    return vcs_dir.is_dir()
+
+
 def _error_root_not_found(get_error_msg: Callable[[], str] = get_error_msg_root_not_found) -> NoReturn:
     raise FileNotFoundError(get_error_msg())
 
