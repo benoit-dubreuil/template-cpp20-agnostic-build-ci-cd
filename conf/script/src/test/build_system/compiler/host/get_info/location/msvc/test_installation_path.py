7bc3f3e3727dc907cb62cad7bc2d6c46dcf29f3a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-12 15:52:56 -0500

    Moved the test root dir into src

---
+++ conf/script/src/test/build_system/compiler/host/get_info/location/msvc/test_installation_path.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from build_system.compiler.host.get_info.location.msvc import installation_path
+
+
+class TestFind:
+
+    def test_no_arg_not_found(self):
+        assert True
