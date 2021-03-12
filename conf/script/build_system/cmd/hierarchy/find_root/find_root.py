bf0be66699a346644e7800a9bf98dd11a29b8966
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-11 20:35:03 -0500

    Added _walk_parent_path func

--- conf/script/build_system/cmd/hierarchy/find_root/find_root.py
+++ conf/script/build_system/cmd/hierarchy/find_root/find_root.py
@@ -12,5 +12,9 @@ def _error_root_not_found(get_error_msg: Callable[[], str] = get_error_msg_root_
     raise FileNotFoundError(get_error_msg())
 
 
+def _walk_parent_path(current_path: Path = Path()) -> (Path, Path):
+    return current_path.parent, current_path
+
+
 def find_root(get_error_msg: Callable[[], str] = get_error_msg_root_not_found) -> Path:
     _error_root_not_found(get_error_msg)
