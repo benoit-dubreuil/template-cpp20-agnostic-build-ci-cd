5e7bfe737c866d7079b4988ee8de607035d6ebac
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-13 10:10:07 -0500

    Added basic test for finding the location

--- conf/script/src/test/test_build_system/test_compiler/test_host/test_get_info/test_location/test_msvc/test_installation_path.py
+++ conf/script/src/test/test_build_system/test_compiler/test_host/test_get_info/test_location/test_msvc/test_installation_path.py
@@ -12,4 +12,9 @@ class TestFind(unittest.TestCase):
     @mock.patch('build_system.compiler.host.get_info.location.msvc.installation_path.Path')
     @mock.patch('build_system.compiler.host.get_info.location.msvc.installation_path.vswhere')
     def test_no_arg_not_found(self, mock_vswhere: MagicMock, mock_path: MagicMock):
-        assert True
+        mock_vswhere.find_first.return_value = None
+        result = installation_path.find()
+
+        mock_vswhere.find_first.assert_called_once_with(latest=True, prerelease=True, products=installation_path._ALL_PRODUCTS, prop=installation_path._PROP_INSTALLATION_PATH,
+                                                        requires=installation_path._DEFAULT_REQUIRES)
+        self.assertIsNone(result)
