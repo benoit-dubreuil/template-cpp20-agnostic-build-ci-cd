bac9b169e5843fe84425e420d9eba8ed9eaa4d74
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-12 20:29:47 -0500

    Made mock decorators work

--- conf/script/src/test/test_build_system/test_compiler/test_host/test_get_info/test_location/test_msvc/test_installation_path.py
+++ conf/script/src/test/test_build_system/test_compiler/test_host/test_get_info/test_location/test_msvc/test_installation_path.py
@@ -2,12 +2,14 @@
 
 import unittest
 from unittest import mock
+from unittest.mock import MagicMock
 
 from build_system.compiler.host.get_info.location.msvc import installation_path
 
 
 class TestFind(unittest.TestCase):
 
-    # @mock.patch('installation_path.vswhere')
-    def test_no_arg_not_found(self):
+    @mock.patch('build_system.compiler.host.get_info.location.msvc.installation_path.Path')
+    @mock.patch('build_system.compiler.host.get_info.location.msvc.installation_path.vswhere')
+    def test_no_arg_not_found(self, mock_vswhere: MagicMock, mock_path: MagicMock):
         assert True
