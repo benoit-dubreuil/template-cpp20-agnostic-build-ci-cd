fa61c435cb280e330f5cf5a394c33bca52b6ed6c
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-13 11:38:01 -0500

    Refactor & added test_random_arg_return_none test

--- conf/script/src/test/test_build_system/test_compiler/test_host/test_get_info/test_version/test_msvc/test_fetch_msvc_version.py
+++ conf/script/src/test/test_build_system/test_compiler/test_host/test_get_info/test_version/test_msvc/test_fetch_msvc_version.py
@@ -4,6 +4,8 @@ import unittest
 from unittest import mock
 from unittest.mock import MagicMock
 
+from typing import Final
+
 from build_system.compiler.host.get_info.version.msvc import fetch_msvc_version
 
 
@@ -12,13 +14,32 @@ class TestFetch(unittest.TestCase):
     @mock.patch('build_system.compiler.host.get_info.version.msvc.fetch_msvc_version.vswhere')
     @mock.patch('build_system.compiler.host.get_info.version.msvc.fetch_msvc_version.msvc.installation_path')
     def test_no_arg_return_none(self, mock_installation_path: MagicMock, mock_vswhere: MagicMock):
-        mock_installation_path.find.return_value = None
+        expected_return_value: Final = None
+        expected_installation_path_args: Final = None
+        expected_installation_path_calls: Final[list[callable]] = [mock.call.find(expected_installation_path_args)]
 
-        result = fetch_msvc_version.fetch()
+        mock_installation_path.find.return_value = expected_return_value
 
-        mock_installation_path.find.assert_called_once_with(None)
+        return_value = fetch_msvc_version.fetch()
 
-        assert len(mock_installation_path.mock_calls) == 1
+        assert mock_installation_path.mock_calls == expected_installation_path_calls
         assert len(mock_vswhere.mock_calls) == 0
 
-        self.assertIsNone(result)
+        assert return_value is expected_return_value
+
+    @mock.patch('build_system.compiler.host.get_info.version.msvc.fetch_msvc_version.Path')
+    @mock.patch('build_system.compiler.host.get_info.version.msvc.fetch_msvc_version.vswhere')
+    @mock.patch('build_system.compiler.host.get_info.version.msvc.fetch_msvc_version.msvc.installation_path')
+    def test_random_arg_return_none(self, mock_installation_path: MagicMock, mock_vswhere: MagicMock, mock_path: MagicMock):
+        expected_return_value: Final = None
+        expected_installation_path_args: Final = mock_path('../randompath')
+        expected_installation_path_calls: Final[list[callable]] = [mock.call.find(expected_installation_path_args)]
+
+        mock_installation_path.find.return_value = None
+
+        return_value = fetch_msvc_version.fetch(expected_installation_path_args)
+
+        assert mock_installation_path.mock_calls == expected_installation_path_calls
+        assert len(mock_vswhere.mock_calls) == 0
+
+        assert return_value is expected_return_value
