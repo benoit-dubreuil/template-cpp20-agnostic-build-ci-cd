228948fd2fe37e661d533d0fbd1c4332b3209f95
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-12 19:37:52 -0500

    Renamed every test package

--- conf/script/src/test/test_build_system/test_compiler/test_host/test_get_info/test_location/test_msvc/test_installation_path.py
+++ conf/script/src/test/test_build_system/test_compiler/test_host/test_get_info/test_location/test_msvc/test_installation_path.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import unittest
+from unittest import mock
+
 from build_system.compiler.host.get_info.location.msvc import installation_path
 
 
-class TestFind:
+class TestFind(unittest.TestCase):
 
+    # @mock.patch('installation_path.vswhere')
     def test_no_arg_not_found(self):
         assert True
