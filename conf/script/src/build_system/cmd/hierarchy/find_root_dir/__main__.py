d6c2d0752bdd63d6ee1fee1e388fa6cbed3a231f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 18:49:30 -0400

    Wrapped find_root main

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/__main__.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/__main__.py
@@ -4,10 +4,10 @@ import utils.cli
 from build_system.cmd.hierarchy.find_root_dir import find_root_dir
 from build_system.cmd.hierarchy.find_root_dir import cli
 
-if __name__ == '__main__':
-    utils.cli.init()
 
+def main():
     root = find_root_dir(cli.get_error_formatted_msg_root_not_found)
     print(root, end=str())
 
-    utils.cli.deinit()
+
+utils.cli.wrap_main(main)
