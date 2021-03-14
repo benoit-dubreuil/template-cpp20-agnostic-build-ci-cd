a9f4358a45f386733be955b7f5394c341c00a82a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 18:52:43 -0400

    Wrapped main of gcc version

--- conf/script/src/build_system/compiler/host/get_info/version/gcc/__main__.py
+++ conf/script/src/build_system/compiler/host/get_info/version/gcc/__main__.py
@@ -5,8 +5,9 @@ from build_system.compiler.family import CompilerFamily
 from build_system.compiler.host.get_info import version
 from build_system.compiler.host.get_info.version import gnu
 
-# Run as a script
-if __name__ == '__main__':
-    utils.cli.init()
+
+def main():
     version.gnu.cli.cli_fetch_gnu_compiler_version(CompilerFamily.GCC)
-    utils.cli.deinit()
+
+
+utils.cli.wrap_main(main)
