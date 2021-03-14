cbc963cce740a66088bc774db1d59081c4a6ab00
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 12:46:08 -0400

    Set root if optional param is None

--- conf/script/src/build_system/cmd/clean_build/clean_build.py
+++ conf/script/src/build_system/cmd/clean_build/clean_build.py
@@ -10,7 +10,8 @@ BUILD_DIR_NAME: Final[str] = 'build'
 def clean_build(root: Optional[Path] = None, ignore_errors=False) -> bool:
     has_successfuly_cleaned_build = True
 
-    build_system.cmd.hierarchy.find_root.find_root()
+    if root is None:
+        root = build_system.cmd.hierarchy.find_root.find_root()
 
     # TODO
     def _on_rmtree_error(function: Callable, path: str, excinfo: str):
