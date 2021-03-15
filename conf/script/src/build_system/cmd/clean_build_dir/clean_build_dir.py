ba065e97c0827236f13b8703126db3635b7e8e23
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-15 10:28:54 -0400

    Integrated find_build_dir into clean_build_dir

--- conf/script/src/build_system/cmd/clean_build_dir/clean_build_dir.py
+++ conf/script/src/build_system/cmd/clean_build_dir/clean_build_dir.py
@@ -1,36 +1,9 @@
 import shutil
 from pathlib import Path
-from typing import Final, NoReturn, Optional
+from typing import Optional
 
-import build_system.cmd.hierarchy.find_root_dir
-import utils.cli
-
-BUILD_DIR_NAME: Final[str] = 'build'
-
-
-def _get_error_msg_build_not_found() -> str:
-    return utils.cli.format_error_msg('Build directory not found')
-
-
-def _error_build_not_found() -> NoReturn:
-    raise FileNotFoundError(_get_error_msg_build_not_found())
-
-
-def find_build_dir(root_dir: Optional[Path] = None) -> Path:
-    if root_dir is None:
-        root_dir = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
-
-    build_dir = root_dir / BUILD_DIR_NAME
-
-    try:
-        build_dir = build_dir.resolve(True)
-    except FileNotFoundError:
-        _error_build_not_found()
-
-    if not build_dir.is_dir():
-        _error_build_not_found()
-
-    return build_dir
+import build_system.cmd.hierarchy.find_build_dir.find_build_dir
+from build_system import cmd
 
 
 def clean_build_dir(root_dir: Optional[Path] = None, ignore_errors=False) -> bool:
@@ -43,7 +16,7 @@ def clean_build_dir(root_dir: Optional[Path] = None, ignore_errors=False) -> boo
     build_dir: Optional[Path] = None
 
     try:
-        build_dir = find_build_dir(root_dir)
+        build_dir = cmd.hierarchy.find_build_dir.find_build_dir(root_dir)
     except OSError as raised_exception:
         if not ignore_errors:
             raise raised_exception
