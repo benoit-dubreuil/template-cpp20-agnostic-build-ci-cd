968879ceacfaeba066dae122cb8f99c214e9628d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 20:35:26 -0400

    Added basic structure for finding the root in the main

--- conf/script/src/build_system/cmd/clean_build_dir/cli.py
+++ conf/script/src/build_system/cmd/clean_build_dir/cli.py
@@ -1,10 +1,15 @@
 import argparse
 from build_system import cmd
-
+import build_system.cmd.hierarchy.find_root_dir
 
 def clean_build_dir():
     arg_parser = argparse.ArgumentParser(
         description=f"Cleans the project's build folder, where the build system organizes into subfolders specific builds.")
+
+    try:
+        cmd.hierarchy.find_root_dir.find_root_dir()
+    except FileNotFoundError as raised_exception:
+        pass
 
     try:
         cmd.clean_build_dir.clean_build_dir()
