4f099c42b1fd215958dd2f5bd6de457fac5f087a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 20:30:12 -0400

    Added basic structure for the clean_build_dir func call

--- conf/script/src/build_system/cmd/clean_build_dir/cli.py
+++ conf/script/src/build_system/cmd/clean_build_dir/cli.py
@@ -1,6 +1,12 @@
 import argparse
+from build_system import cmd
 
 
 def clean_build_dir():
     arg_parser = argparse.ArgumentParser(
         description=f"Cleans the project's build folder, where the build system organizes into subfolders specific builds.")
+
+    try:
+        cmd.clean_build_dir.clean_build_dir()
+    except FileNotFoundError as raised_exception:
+        pass
