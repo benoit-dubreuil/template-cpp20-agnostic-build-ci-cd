f89565eb8fcac34cc7adfccf29a214b1ee24afe8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 20:56:00 -0400

    Simplified exit error messages whilst controlling the exit status

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from build_system.cmd import hierarchy
 from build_system.cmd.hierarchy.find_root_dir.find_root_dir import VCS_DIR_NAME
 
+ROOT_NOT_FOUND_ERROR_STATUS = 1
+
 
 def find_root_dir():
     arg_parser = argparse.ArgumentParser(
@@ -14,4 +16,4 @@ def find_root_dir():
         print(project_root, end=str())
     except FileNotFoundError as raised_exception:
         raised_exception_msg = str(raised_exception)
-        arg_parser.error(raised_exception_msg)
+        arg_parser.exit(ROOT_NOT_FOUND_ERROR_STATUS, raised_exception_msg)
