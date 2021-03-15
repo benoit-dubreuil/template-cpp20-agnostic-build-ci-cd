e66b12d452dfbbfb72f141f472b4fb0add85a871
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 20:34:50 -0400

    Bug fix : main find_root_dir did not use cli code

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/__main__.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/__main__.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 import utils.cli
-from build_system.cmd.hierarchy.find_root_dir import find_root_dir
-from build_system.cmd.hierarchy.find_root_dir import cli
+from build_system import cmd
 
 
 def main():
-    root = find_root_dir(cli.get_error_formatted_msg_root_not_found)
-    print(root, end=str())
+    cmd.hierarchy.find_root_dir.cli.find_root_dir()
 
 
 utils.cli.wrap_main(main)
