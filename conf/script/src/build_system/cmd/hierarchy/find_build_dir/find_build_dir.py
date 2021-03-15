1c5e0e7c2b1eeac55aea68f2220bcf499f030ff4
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-15 11:00:55 -0400

    Raises BuildDirNotFoundError instead of generic error

--- conf/script/src/build_system/cmd/hierarchy/find_build_dir/find_build_dir.py
+++ conf/script/src/build_system/cmd/hierarchy/find_build_dir/find_build_dir.py
@@ -1,33 +1,25 @@
 import shutil
 from pathlib import Path
-from typing import Final, NoReturn, Optional
+from typing import Final, Optional
 
 import build_system.cmd.hierarchy.find_root_dir
-import utils.cli
+from build_system import cmd
 
 BUILD_DIR_NAME: Final[str] = 'build'
 
 
-def _get_error_msg_build_not_found() -> str:
-    return utils.cli.format_error_msg('Build directory not found')
-
-
-def _error_build_not_found() -> NoReturn:
-    raise FileNotFoundError(_get_error_msg_build_not_found())
-
-
 def find_build_dir(root_dir: Optional[Path] = None) -> Path:
     if root_dir is None:
-        root_dir = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
+        root_dir = cmd.hierarchy.find_root_dir.find_root_dir()
 
     build_dir = root_dir / BUILD_DIR_NAME
 
     try:
         build_dir = build_dir.resolve(True)
     except FileNotFoundError:
-        _error_build_not_found()
+        raise cmd.hierarchy.find_build_dir.error.BuildDirNotFoundError()
 
     if not build_dir.is_dir():
-        _error_build_not_found()
+        raise cmd.hierarchy.find_build_dir.error.BuildDirNotFoundError()
 
     return build_dir
