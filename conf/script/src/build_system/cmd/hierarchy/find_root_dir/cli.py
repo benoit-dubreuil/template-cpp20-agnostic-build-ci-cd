d67a59e57c6cdfaf6493dd208a7228923efe23d2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 21:19:40 -0400

    Support other OSErrors for find_root_dir

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
@@ -6,6 +6,7 @@ from build_system.cmd import hierarchy
 from build_system.cmd.hierarchy.find_root_dir.find_root_dir import VCS_DIR_NAME
 
 ROOT_NOT_FOUND_ERROR_STATUS: Final[int] = 1
+UNSUPPORTED_ERROR_STATUS: Final[int] = 1 + ROOT_NOT_FOUND_ERROR_STATUS
 
 
 def find_root_dir():
@@ -13,8 +14,11 @@ def find_root_dir():
         description=f"Fetches the project's root folder, where the '{VCS_DIR_NAME}' is. It searches recursively parent folders upwards.")
 
     try:
-        project_root: Path = hierarchy.find_root_dir.find_root_dir()
-        print(project_root, end=str())
-    except FileNotFoundError as raised_exception:
-        raised_exception_msg = str(raised_exception)
-        arg_parser.exit(ROOT_NOT_FOUND_ERROR_STATUS, raised_exception_msg)
+        try:
+            project_root: Path = hierarchy.find_root_dir.find_root_dir()
+            print(project_root, end=str())
+        except FileNotFoundError as raised_exception:
+            arg_parser.exit(ROOT_NOT_FOUND_ERROR_STATUS, str(raised_exception))
+
+    except OSError as raised_exception:
+        arg_parser.exit(UNSUPPORTED_ERROR_STATUS, str(raised_exception))
