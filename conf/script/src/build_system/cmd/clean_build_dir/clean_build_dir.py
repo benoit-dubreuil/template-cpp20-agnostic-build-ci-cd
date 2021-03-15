d8cd9ff480d571329e016a18ec88592e3324d49a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 21:27:51 -0400

    Better support build dir not found

--- conf/script/src/build_system/cmd/clean_build_dir/clean_build_dir.py
+++ conf/script/src/build_system/cmd/clean_build_dir/clean_build_dir.py
@@ -21,7 +21,11 @@ def find_build_dir(root_dir: Optional[Path] = None) -> Path:
         root_dir = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
 
     build_dir = root_dir / BUILD_DIR_NAME
-    build_dir = build_dir.resolve(True)
+
+    try:
+        build_dir = build_dir.resolve(True)
+    except FileNotFoundError:
+        _error_build_not_found()
 
     if not build_dir.is_dir():
         _error_build_not_found()
