109a981ebd4f1bcb20593a0316d6fbb80c8aedf9
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 20:10:18 -0400

    Hide private func

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/__init__.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/__init__.py
@@ -1,2 +1,2 @@
 import build_system.cmd.hierarchy.find_root_dir.cli
-from build_system.cmd.hierarchy.find_root_dir.find_root_dir import find_root_dir, _get_error_msg_root_not_found, VCS_DIR_NAME
+from build_system.cmd.hierarchy.find_root_dir.find_root_dir import find_root_dir, VCS_DIR_NAME
