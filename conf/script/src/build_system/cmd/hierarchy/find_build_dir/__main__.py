acf1b35ab00bf0e2bc6d7090cdcdc03b79bb6c7c
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-15 11:12:44 -0400

    Developed main for find_build_dir

--- conf/script/src/build_system/cmd/hierarchy/find_build_dir/__main__.py
+++ conf/script/src/build_system/cmd/hierarchy/find_build_dir/__main__.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+import build_system.cmd.hierarchy.find_build_dir.cli
+import utils.cli
+from build_system import cmd
+
+
+def main():
+    cmd.hierarchy.find_build_dir.cli.find_build_dir()
+
+
+utils.cli.wrap_main(main)
--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/__main__.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/__main__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
+import build_system.cmd.hierarchy.find_root_dir.cli
 import utils.cli
 from build_system import cmd
-import build_system.cmd.hierarchy.find_root_dir.cli
 
 
 def main():
