0e15f6d522336098819873fbd9ee041ec8ecacde
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-14 21:06:13 -0400

    Find the root_dir an exit with the appropriate code if there's an error

--- conf/script/src/build_system/cmd/clean_build_dir/cli.py
+++ conf/script/src/build_system/cmd/clean_build_dir/cli.py
@@ -1,18 +1,22 @@
 import argparse
 from build_system import cmd
 import build_system.cmd.hierarchy.find_root_dir
+import build_system.cmd.hierarchy.find_root_dir.cli
 
 
 def clean_build_dir():
     arg_parser = argparse.ArgumentParser(
         description=f"Cleans the project's build folder, where the build system organizes into subfolders specific builds.")
 
-    try:
-        cmd.hierarchy.find_root_dir.find_root_dir()
-    except FileNotFoundError as raised_exception:
-        pass
+    root_dir = None
 
     try:
-        cmd.clean_build_dir.clean_build_dir()
+        root_dir = cmd.hierarchy.find_root_dir.find_root_dir()
+    except FileNotFoundError as raised_exception:
+        raised_exception_msg = str(raised_exception)
+        arg_parser.exit(cmd.hierarchy.find_root_dir.cli.ROOT_NOT_FOUND_ERROR_STATUS, raised_exception_msg)
+
+    try:
+        cmd.clean_build_dir.clean_build_dir(root_dir)
     except FileNotFoundError as raised_exception:
         pass
