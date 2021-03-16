82be1ee08c5598f85ba39e7416399053e6a44877
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-15 21:01:18 -0400

    Added UnknownParsedArgError

--- conf/script/src/utils/cli_error.py
+++ conf/script/src/utils/cli_error.py
@@ -1,9 +1,16 @@
 import enum
 from enum import IntEnum
 
+import utils.formatted_error
+
 
 class ErrorStatus(IntEnum):
     ARG_PARSER_DEFAULT = 2
     UNKNOWN_PARSED_ARG = enum.auto()
     EMPTY_PARSED_ARG = enum.auto()
 
+
+class UnknownParsedArgError(TypeError, utils.formatted_error.FormattedException):
+
+    def __init__(self, unknown_parsed_args: list[str]):
+        super().__init__(f"Unsupported argument '{unknown_parsed_args}'")
