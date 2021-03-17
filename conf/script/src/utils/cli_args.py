b5038d225bc7ae2b17a3efc8bdffd1052c65d429
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-17 11:56:45 -0400

    Switched Path to AnyPath

--- conf/script/src/utils/cli_args.py
+++ conf/script/src/utils/cli_args.py
@@ -10,7 +10,7 @@ DEFAULT_PATH_ARG_NAME: Final[str] = 'path'
 DEFAULT_PATH_ARG: Final[str] = '-' + DEFAULT_PATH_ARG_NAME
 
 
-def add_optional_path_arg(arg_parser: argparse.ArgumentParser, path_arg: AnyStr = DEFAULT_PATH_ARG, path_arg_default_value: Optional[Path] = None,
+def add_optional_path_arg(arg_parser: argparse.ArgumentParser, path_arg: AnyStr = DEFAULT_PATH_ARG, path_arg_default_value: AnyPath = None,
                           path_arg_help: Optional[AnyStr] = None):
     arg_parser.add_argument(path_arg, type=str, nargs='?', const=None, default=path_arg_default_value, help=path_arg_help)
 
@@ -27,8 +27,8 @@ def _assure_no_unknown_parsed_args(arg_parser: argparse.ArgumentParser, unknown_
             raise error
 
 
-def _assure_nonempty_parsed_path(arg_parser: argparse.ArgumentParser, path_arg: str, parsed_path: str):
-    if parsed_path == str():
+def _assure_nonempty_parsed_path(arg_parser: argparse.ArgumentParser, path_arg: str, parsed_path: AnyPath):
+    if str(parsed_path) == str():
         error = utils.cli_error.EmptyParsedArgError(path_arg)
 
         # noinspection PyUnresolvedReferences
@@ -39,11 +39,11 @@ def _assure_nonempty_parsed_path(arg_parser: argparse.ArgumentParser, path_arg: 
             raise error
 
 
-def parse_optional_path_arg(arg_parser: argparse.ArgumentParser, path_arg: str = DEFAULT_PATH_ARG) -> Optional[Path]:
+def parse_optional_path_arg(arg_parser: argparse.ArgumentParser, path_arg: str = DEFAULT_PATH_ARG) -> AnyPath:
     parsed_args, unknown_parsed_args = arg_parser.parse_known_args([path_arg])
 
     _assure_no_unknown_parsed_args(arg_parser, unknown_parsed_args)
-    parsed_path: Optional[str] = getattr(parsed_args, path_arg) if path_arg in parsed_args else None
+    parsed_path: AnyPath = getattr(parsed_args, path_arg) if path_arg in parsed_args else None
     _assure_nonempty_parsed_path(arg_parser, path_arg, parsed_path)
 
-    return Path(parsed_path) if parsed_path is not None else None
+    return parsed_path
