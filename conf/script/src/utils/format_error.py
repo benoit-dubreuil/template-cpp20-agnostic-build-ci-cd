d10888616e262e594302c529d1ee3b6102a63c63
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 12:07:41 -0400

    Added BaseFormattedError

--- conf/script/src/utils/format_error.py
+++ conf/script/src/utils/format_error.py
@@ -1,6 +1,9 @@
+import abc
 from typing import AnyStr
 
 import colorama
+
+import utils.cli.error_meta
 
 
 def format_error_msg(message: AnyStr) -> AnyStr:
@@ -11,13 +14,29 @@ def format_success_msg(message: AnyStr) -> AnyStr:
     return colorama.Style.BRIGHT + colorama.Fore.GREEN + message + colorama.Style.RESET_ALL
 
 
-class FormattedError(Exception):
+class BaseFormattedError(Exception, abc.ABC, metaclass=utils.cli.error_meta.ManagedErrorMeta):
+
+    @staticmethod
+    @abc.abstractmethod
+    def _format_msg(message: str) -> str:
+        ...
+
+
+class FormattedError(BaseFormattedError):
 
     def __init__(self, message: str, *args):
         super().__init__(format_error_msg(message), *args)
 
+    @staticmethod
+    def _format_msg(message: str) -> str:
+        return format_error_msg(message)
 
-class FormattedSuccess(Exception):
+
+class FormattedSuccess(BaseFormattedError):
 
     def __init__(self, message: str, *args):
         super().__init__(format_success_msg(message), *args)
+
+    @staticmethod
+    def _format_msg(message: str) -> str:
+        return format_success_msg(message)
