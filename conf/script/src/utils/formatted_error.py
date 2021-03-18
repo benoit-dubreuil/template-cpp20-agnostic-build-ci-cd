c7079f1b271751214ac6b45e9e8b5148141e3828
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 10:50:01 -0400

    Simplified FormattedError

--- conf/script/src/utils/formatted_error.py
+++ conf/script/src/utils/formatted_error.py
@@ -9,6 +9,5 @@ def format_error_msg(error_msg: AnyStr) -> AnyStr:
 
 class FormattedError(BaseException):
 
-    def __init__(self, *args):
-        formatted_args = [format_error_msg(arg) if isinstance(arg, str) else arg for arg in args]
-        super().__init__(*formatted_args)
+    def __init__(self, error_msg: str, *args):
+        super().__init__(format_error_msg(error_msg), *args)
