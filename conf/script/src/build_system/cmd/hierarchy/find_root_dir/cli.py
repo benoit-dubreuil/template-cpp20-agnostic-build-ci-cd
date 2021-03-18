92a1d9f27f420dda762328224e1e5252424e9fd1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 10:32:29 -0400

    Integrated new errors statuses inside find root cmd

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
@@ -1,11 +1,8 @@
 import argparse
-from typing import Final
 
+import utils.cli.error
 from build_system import cmd
 from build_system.cmd.hierarchy.find_root_dir.find_root_dir import VCS_DIR_NAME
-
-ROOT_DIR_NOT_FOUND_ERROR_STATUS: Final[int] = 1
-UNSUPPORTED_ERROR_STATUS: Final[int] = 1 + ROOT_DIR_NOT_FOUND_ERROR_STATUS
 
 
 def find_root_dir():
@@ -17,7 +14,7 @@ def find_root_dir():
         print(project_root, end=str())
 
     except cmd.hierarchy.find_root_dir.error.RootDirNotFoundError as raised_exception:
-        arg_parser.exit(ROOT_DIR_NOT_FOUND_ERROR_STATUS, str(raised_exception))
+        arg_parser.exit(utils.cli.error.ErrorStatus.ROOT_DIR_NOT_FOUND, str(raised_exception))
 
     except OSError as raised_exception:
-        arg_parser.exit(UNSUPPORTED_ERROR_STATUS, str(raised_exception))
+        arg_parser.exit(utils.cli.error.ErrorStatus.UNSUPPORTED_ERROR, str(raised_exception))
