a3fe1d1d1081e1778825e5929f48db71e8b067cb
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 11:36:15 -0400

    Added FormattedSuccess base exception

--- conf/script/src/utils/format_error.py
+++ conf/script/src/utils/format_error.py
@@ -3,11 +3,21 @@ from typing import AnyStr
 import colorama
 
 
-def format_error_msg(error_msg: AnyStr) -> AnyStr:
-    return colorama.Style.BRIGHT + colorama.Fore.RED + error_msg + colorama.Style.RESET_ALL
+def format_error_msg(message: AnyStr) -> AnyStr:
+    return colorama.Style.BRIGHT + colorama.Fore.RED + message + colorama.Style.RESET_ALL
+
+
+def format_success_msg(message: AnyStr) -> AnyStr:
+    return colorama.Style.BRIGHT + colorama.Fore.GREEN + message + colorama.Style.RESET_ALL
 
 
 class FormattedError(BaseException):
 
     def __init__(self, message: str, *args):
         super().__init__(format_error_msg(message), *args)
+
+
+class FormattedSuccess(BaseException):
+
+    def __init__(self, message: str, *args):
+        super().__init__(format_success_msg(message), *args)
