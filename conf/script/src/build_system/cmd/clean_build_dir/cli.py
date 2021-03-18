4a3e5c683a49d75461c77342a5a2d064ba827e0c
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 10:30:39 -0400

    Integrated new error statuses for clean cmd

--- conf/script/src/build_system/cmd/clean_build_dir/cli.py
+++ conf/script/src/build_system/cmd/clean_build_dir/cli.py
@@ -1,12 +1,8 @@
 import argparse
-from typing import Final
 
 import build_system.cmd.hierarchy.find_root_dir
-import build_system.cmd.hierarchy.find_root_dir.cli
+import utils.cli.error
 from build_system import cmd
-
-ANY_BUILD_DIR_NOT_FOUND_ERROR_STATUS: Final[int] = 1 + cmd.hierarchy.find_root_dir.cli.ROOT_DIR_NOT_FOUND_ERROR_STATUS
-UNSUPPORTED_ERROR_STATUS: Final[int] = 1 + ANY_BUILD_DIR_NOT_FOUND_ERROR_STATUS
 
 
 def clean_build_dir():
@@ -19,12 +15,12 @@ def clean_build_dir():
         try:
             root_dir = cmd.hierarchy.find_root_dir.find_root_dir()
         except cmd.hierarchy.find_root_dir.error.RootDirNotFoundError as raised_exception:
-            arg_parser.exit(cmd.hierarchy.find_root_dir.cli.ROOT_DIR_NOT_FOUND_ERROR_STATUS, str(raised_exception))
+            arg_parser.exit(utils.cli.error.ErrorStatus.ROOT_DIR_NOT_FOUND, str(raised_exception))
 
         try:
             cmd.clean_build_dir.clean_build_dir(root_dir)
         except FileNotFoundError as raised_exception:
-            arg_parser.exit(ANY_BUILD_DIR_NOT_FOUND_ERROR_STATUS, str(raised_exception))
+            arg_parser.exit(utils.cli.error.ErrorStatus.BUILD_DIR_NOT_FOUND, str(raised_exception))
 
     except OSError as raised_exception:
-        arg_parser.exit(UNSUPPORTED_ERROR_STATUS, str(raised_exception))
+        arg_parser.exit(utils.cli.error.ErrorStatus.UNSUPPORTED_ERROR, str(raised_exception))
