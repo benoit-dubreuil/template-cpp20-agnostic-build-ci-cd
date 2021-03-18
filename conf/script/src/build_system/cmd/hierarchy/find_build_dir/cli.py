44b2a8a76546fe87547e9210bf5fa161b3925e1d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 10:35:02 -0400

    Integrated new error statuses inside find build dir cmd

--- conf/script/src/build_system/cmd/hierarchy/find_build_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_build_dir/cli.py
@@ -4,12 +4,10 @@ from typing import Final
 import build_system.cmd.hierarchy.find_root_dir.cli
 import utils.cli.arg
 import utils.cli.arg_parsing
+import utils.cli.error
 from build_system import cmd
 from build_system.cmd.hierarchy.find_build_dir.find_build_dir import BUILD_DIR_NAME
 from utils.more_typing import AnyPath
-
-BUILD_DIR_NOT_FOUND_ERROR_STATUS: Final[int] = 1 + cmd.hierarchy.find_root_dir.cli.ROOT_DIR_NOT_FOUND_ERROR_STATUS
-UNSUPPORTED_ERROR_STATUS: Final[int] = 1 + BUILD_DIR_NOT_FOUND_ERROR_STATUS
 
 ROOT_DIR_ARG: Final[utils.cli.arg.CLIArg] = utils.cli.arg.CLIArg('rootdir')
 
@@ -25,10 +23,10 @@ def find_build_dir():
         print(build_dir, end=str())
 
     except cmd.hierarchy.find_root_dir.error.RootDirNotFoundError as raised_exception:
-        arg_parser.exit(cmd.hierarchy.find_root_dir.cli.ROOT_DIR_NOT_FOUND_ERROR_STATUS, str(raised_exception))
+        arg_parser.exit(utils.cli.error.ErrorStatus.ROOT_DIR_NOT_FOUND, str(raised_exception))
 
     except cmd.hierarchy.find_build_dir.error.BuildDirNotFoundError as raised_exception:
-        arg_parser.exit(BUILD_DIR_NOT_FOUND_ERROR_STATUS, str(raised_exception))
+        arg_parser.exit(utils.cli.error.ErrorStatus.BUILD_DIR_NOT_FOUND, str(raised_exception))
 
     except OSError as raised_exception:
-        arg_parser.exit(UNSUPPORTED_ERROR_STATUS, str(raised_exception))
+        arg_parser.exit(utils.cli.error.ErrorStatus.UNSUPPORTED_ERROR, str(raised_exception))
