500262ba13798caadad96d042722783f449a35b0
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 21:28:58 -0400

    Fixed imports

--- conf/script/src/utils/error/synthesis.py
+++ conf/script/src/utils/error/synthesis.py
@@ -1,10 +1,10 @@
 from typing import Final, Type
 
 import utils.error.cls_def
-import utils.error.status
 import utils.error.managed
+import utils.error.status
 
-ALL_ERRORS: Final[list[Type[utils.cli.managed_error.ManagedError]]] = [
+ALL_ERRORS: Final[list[Type[utils.error.managed.ManagedError]]] = [
     utils.error.cls_def.SuccessWarning,
     utils.error.cls_def.UnsupportedError,
     utils.error.cls_def.ArgParserError,
@@ -14,5 +14,5 @@ ALL_ERRORS: Final[list[Type[utils.cli.managed_error.ManagedError]]] = [
     utils.error.cls_def.BuildDirNotFoundError,
 ]
 
-ALL_ERRORS_BY_STATUS: Final[dict[utils.error.status.ErrorStatus, Type[utils.cli.managed_error.ManagedError]]] = {status: ALL_ERRORS[status] for status in
-                                                                                                                 utils.error.status.ErrorStatus}
+ALL_ERRORS_BY_STATUS: Final[dict[utils.error.status.ErrorStatus, Type[utils.error.managed.ManagedError]]] = {status: ALL_ERRORS[status] for status in
+                                                                                                             utils.error.status.ErrorStatus}
