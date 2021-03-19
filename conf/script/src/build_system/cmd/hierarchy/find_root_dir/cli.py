8b25614c6e7ec52920ce97ad8b556923e9effe16
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-19 17:44:51 -0400

    Integrated the new errors into find_root_dir.cli

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
@@ -1,6 +1,7 @@
 import argparse
 
 import utils.error.cls_def
+import utils.error.managed
 import utils.error.status
 from build_system import cmd
 from build_system.cmd.hierarchy.find_root_dir.find_root_dir import VCS_DIR_NAME
@@ -14,8 +15,10 @@ def find_root_dir():
         project_root = cmd.hierarchy.find_root_dir.find_root_dir()
         print(project_root, end=str())
 
-    except utils.error.cls_def.RootDirNotFoundError as raised_exception:
-        arg_parser.exit(utils.error.status.ErrorStatus.ROOT_DIR_NOT_FOUND, str(raised_exception))
+    except utils.error.cls_def.RootDirNotFoundError as raised_error:
+        raised_error.raise_or_exit_cli(arg_parser)
 
-    except OSError as raised_exception:
-        arg_parser.exit(utils.error.status.ErrorStatus.UNSUPPORTED, str(raised_exception))
+    except OSError as raised_error:
+        if not isinstance(raised_error, utils.error.managed.ManagedError):
+            unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
+            unsupported_error.raise_or_exit_cli(arg_parser)
