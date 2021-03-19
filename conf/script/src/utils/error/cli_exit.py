0b22adb36e89bdaf6da563d8fae35f973220fdb9
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-19 17:12:45 -0400

    Added raise_or_exit_cli func

--- conf/script/src/utils/error/cli_exit.py
+++ conf/script/src/utils/error/cli_exit.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import typing
 
 import utils.error.meta
 import utils.error.status
@@ -7,10 +8,17 @@ import utils.error.status
 
 class ExitCLIError(metaclass=utils.error.meta.ErrorMeta):
 
-    def exit_cli(self, arg_parser: argparse.ArgumentParser, print_usage: bool = False) -> None:
+    def exit_cli(self, arg_parser: argparse.ArgumentParser, print_usage: bool = False) -> typing.NoReturn:
         assert isinstance(self, utils.error.status.EncodedError)
 
         if print_usage:
             arg_parser.print_usage(sys.stderr)
 
         arg_parser.exit(self.get_error_status(), str(self))
+
+    def raise_or_exit_cli(self, arg_parser: argparse.ArgumentParser, print_usage: bool = False) -> typing.NoReturn:
+        # noinspection PyUnresolvedReferences
+        if arg_parser.exit_on_error:
+            self.exit_cli(arg_parser, print_usage)
+        else:
+            raise self
