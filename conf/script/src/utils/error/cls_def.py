ba0a8cbdd382b019b2070defbc44bfc024b2af9c
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 22:02:16 -0400

    Impl encoded_error_status for less code

--- conf/script/src/utils/error/cls_def.py
+++ conf/script/src/utils/error/cls_def.py
@@ -3,22 +3,17 @@ from typing import Union
 
 import utils.error.format
 import utils.error.managed
-import utils.error.meta
 import utils.error.status
 
 
-@utils.error.managed.manage(error_formatter_cls=utils.error.format.FormattedSuccess)
+@utils.error.managed.manage(error_formatter_cls=utils.error.format.FormattedSuccess, encoded_error_status=utils.error.status.ErrorStatus.SUCCESS)
 class SuccessWarning(UserWarning):
 
     def __init__(self):
         super().__init__('Success')
 
-    @staticmethod
-    def get_error_status() -> utils.error.status.ErrorStatus:
-        return utils.error.status.ErrorStatus.SUCCESS
 
-
-@utils.error.managed.manage
+@utils.error.managed.manage(encoded_error_status=utils.error.status.ErrorStatus.ARG_PARSER)
 class ArgParserError(RuntimeError):
 
     def __init__(self, arg_parser_exception: Union[argparse.ArgumentError, argparse.ArgumentTypeError]):
@@ -31,61 +26,37 @@ class ArgParserError(RuntimeError):
 
         super().__init__(error_msg)
 
-    @staticmethod
-    def get_error_status() -> utils.error.status.ErrorStatus:
-        return utils.error.status.ErrorStatus.ARG_PARSER
 
-
-@utils.error.managed.manage
+@utils.error.managed.manage(encoded_error_status=utils.error.status.ErrorStatus.UNSUPPORTED)
 class UnsupportedError(RuntimeError):
 
     def __init__(self):
         super().__init__('Unsupported error')
 
-    @staticmethod
-    def get_error_status() -> utils.error.status.ErrorStatus:
-        return utils.error.status.ErrorStatus.UNSUPPORTED
 
-
-@utils.error.managed.manage
+@utils.error.managed.manage(encoded_error_status=utils.error.status.ErrorStatus.UNKNOWN_PARSED_ARG)
 class UnknownParsedArgError(TypeError):
 
     def __init__(self, unknown_parsed_args: list[str]):
         super().__init__(f"Unsupported argument '{unknown_parsed_args}'")
 
-    @staticmethod
-    def get_error_status() -> utils.error.status.ErrorStatus:
-        return utils.error.status.ErrorStatus.UNKNOWN_PARSED_ARG
 
-
-@utils.error.managed.manage
+@utils.error.managed.manage(encoded_error_status=utils.error.status.ErrorStatus.EMPTY_PARSED_ARG)
 class EmptyParsedArgError(ValueError):
 
     def __init__(self, arg: str):
         super().__init__(f"'{arg}' argument must be followed by a path string")
 
-    @staticmethod
-    def get_error_status() -> utils.error.status.ErrorStatus:
-        return utils.error.status.ErrorStatus.EMPTY_PARSED_ARG
 
-
-@utils.error.managed.manage
+@utils.error.managed.manage(encoded_error_status=utils.error.status.ErrorStatus.ROOT_DIR_NOT_FOUND)
 class RootDirNotFoundError(FileNotFoundError):
 
     def __init__(self):
         super().__init__('Root directory not found')
 
-    @staticmethod
-    def get_error_status() -> utils.error.status.ErrorStatus:
-        return utils.error.status.ErrorStatus.ROOT_DIR_NOT_FOUND
 
-
-@utils.error.managed.manage
+@utils.error.managed.manage(encoded_error_status=utils.error.status.ErrorStatus.BUILD_DIR_NOT_FOUND)
 class BuildDirNotFoundError(FileNotFoundError):
 
     def __init__(self):
         super().__init__('Build directory not found')
-
-    @staticmethod
-    def get_error_status() -> utils.error.status.ErrorStatus:
-        return utils.error.status.ErrorStatus.BUILD_DIR_NOT_FOUND
