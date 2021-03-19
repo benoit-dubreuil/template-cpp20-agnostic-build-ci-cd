3a8b1f9cc4a10f5490b9c785c96513b0d1cfd820
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 22:12:08 -0400

    Added test_decorate_warning

--- conf/script/src/test/test_utils/error/test_managed.py
+++ conf/script/src/test/test_utils/error/test_managed.py
@@ -10,12 +10,25 @@ from utils.error.managed import manage
 
 class TestManage(unittest.TestCase):
 
+    def test_decorate_warning(self):
+        @manage
+        class DecoratedError(RuntimeWarning):
+
+            def __init__(self):
+                super().__init__(str(unittest))
+
+            @staticmethod
+            def get_error_status() -> utils.error.status.ErrorStatus:
+                return utils.error.status.ErrorStatus.UNSUPPORTED
+
+        DecoratedError()
+
     def test_decorate_success_with_formatter(self):
         @manage(error_formatter_cls=utils.error.format.FormattedSuccess)
         class DecoratedError(UserWarning):
 
             def __init__(self):
-                super().__init__('Success')
+                super().__init__(str(unittest))
 
             @staticmethod
             def get_error_status() -> utils.error.status.ErrorStatus:
@@ -28,7 +41,7 @@ class TestManage(unittest.TestCase):
         class DecoratedError(UserWarning):
 
             def __init__(self):
-                super().__init__('Success')
+                super().__init__(str(unittest))
 
         DecoratedError()
 
