9b8083bd2d58d30f56dfa2df4d7b433535072fce
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-19 12:03:49 -0400

    Fixed the cls param

--- conf/script/src/utils/error/managed.py
+++ conf/script/src/utils/error/managed.py
@@ -13,17 +13,18 @@ class ManagedError(utils.error.status.EncodedError, utils.error.format.BaseForma
 
 class ManageClass:
 
-    def __init__(self, error_formatter_cls: Type[utils.error.format.BaseFormattedError] = utils.error.format.FormattedError,
+    def __init__(self, cls_to_decorate: type, error_formatter_cls: Type[utils.error.format.BaseFormattedError] = utils.error.format.FormattedError,
                  encoded_error_status: Optional[utils.error.status.ErrorStatus] = None):
+        self.cls_to_decorate = cls_to_decorate
         self.error_formatter_cls = error_formatter_cls
 
         if [encoded_error_status is not None]:
             self.encoded_error_status = encoded_error_status
 
-    def __call__(self, cls) -> Callable[[], type]:
+    def __call__(self) -> Callable[[], type]:
         def inner_cls():
             # noinspection PyAbstractClass
-            class DecoratedManagedError(cls, ManagedError, self.error_formatter_cls, metaclass=utils.error.meta.ErrorMeta):
+            class DecoratedManagedError(self.cls_to_decorate, ManagedError, self.error_formatter_cls, metaclass=utils.error.meta.ErrorMeta):
                 if hasattr(self, 'encoded_error_status'):
                     @staticmethod
                     def get_error_status() -> utils.error.status.ErrorStatus:
