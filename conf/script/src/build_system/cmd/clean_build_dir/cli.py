f6193ee593159e8b928a349e6c840c5efe37e630
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-19 17:37:57 -0400

    Integrated new errors into clean_buil_dir.cli

--- conf/script/src/build_system/cmd/clean_build_dir/cli.py
+++ conf/script/src/build_system/cmd/clean_build_dir/cli.py
@@ -1,7 +1,7 @@
 import argparse
 
-import build_system.cmd.hierarchy.find_root_dir
 import utils.error.cls_def
+import utils.error.managed
 import utils.error.status
 from build_system import cmd
 
@@ -10,18 +10,13 @@ def clean_build_dir():
     arg_parser = argparse.ArgumentParser(
         description=f"Cleans the project's build folder, where the build system organizes into subfolders specific builds.")
 
-    root_dir = None
-
     try:
-        try:
-            root_dir = cmd.hierarchy.find_root_dir.find_root_dir()
-        except utils.error.cls_def.RootDirNotFoundError as raised_exception:
-            arg_parser.exit(utils.error.status.ErrorStatus.ROOT_DIR_NOT_FOUND, str(raised_exception))
+        cmd.clean_build_dir.clean_build_dir()
 
-        try:
-            cmd.clean_build_dir.clean_build_dir(root_dir)
-        except FileNotFoundError as raised_exception:
-            arg_parser.exit(utils.error.status.ErrorStatus.BUILD_DIR_NOT_FOUND, str(raised_exception))
+    except (utils.error.cls_def.RootDirNotFoundError, utils.error.cls_def.BuildDirNotFoundError) as raised_error:
+        raised_error.raise_or_exit_cli(arg_parser)
 
-    except OSError as raised_exception:
-        arg_parser.exit(utils.error.status.ErrorStatus.UNSUPPORTED, str(raised_exception))
+    except OSError as raised_error:
+        if not isinstance(raised_error, utils.error.managed.ManagedError):
+            unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
+            unsupported_error.raise_or_exit_cli(arg_parser)
