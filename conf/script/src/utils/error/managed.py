b487540531a359687b30530215ac1c0a0ac9a871
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-18 21:54:38 -0400

    Fixed manage decorator

--- conf/script/src/utils/error/managed.py
+++ conf/script/src/utils/error/managed.py
@@ -11,7 +11,11 @@ class ManagedError(abc.ABC, utils.error.status.EncodedError, utils.error.format.
 
 
 def manage(cls: type, error_formatter_cls: Type[utils.error.format.BaseFormattedError] = utils.error.format.FormattedError):
-    class NewlyManagedError(cls, ManagedError, error_formatter_cls, metaclass=utils.error.meta.ErrorMeta):
-        ...
+    def decorator_impl(impl_cls, impl_error_formatter_cls):
+        # noinspection PyAbstractClass
+        class NewlyManagedError(impl_cls, ManagedError, impl_error_formatter_cls, metaclass=utils.error.meta.ErrorMeta):
+            ...
 
-    return NewlyManagedError
+        return NewlyManagedError
+
+    return decorator_impl(cls, error_formatter_cls)
