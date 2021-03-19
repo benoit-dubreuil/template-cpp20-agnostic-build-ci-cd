1683380f051a5eb4e66e01709e9d0397c9a63752
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-19 11:13:12 -0400

    Removed duplicated abc inheriting

--- conf/script/src/test/test_utils/error/test_meta.py
+++ conf/script/src/test/test_utils/error/test_meta.py
@@ -9,14 +9,14 @@ from utils.error.meta import ErrorMeta
 
 class TestErrorMeta(unittest.TestCase):
 
-    def test_impl_extends_abc_exception(self):
-        class ErrorMetaImpl(abc.ABC, Exception, metaclass=ErrorMeta):
+    def test_impl_extends_exception(self):
+        class ErrorMetaImpl(Exception, metaclass=ErrorMeta):
             ...
 
         ErrorMetaImpl()
 
-    def test_impl_extends_abc_exception_abstractmethod(self):
-        class ErrorMetaImplParent(abc.ABC, Exception, metaclass=ErrorMeta):
+    def test_impl_extends_exception_abstractmethod(self):
+        class ErrorMetaImplParent(Exception, metaclass=ErrorMeta):
 
             @abc.abstractmethod
             def dummy(self):
