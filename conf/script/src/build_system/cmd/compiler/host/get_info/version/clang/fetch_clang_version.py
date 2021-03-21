8670b501c8fdb6d5cc85ad889d8dedf8b7420b13
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 10:42:45 -0400

    Fixed enum usage

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/clang/fetch_clang_version.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/clang/fetch_clang_version.py
@@ -4,4 +4,4 @@ import build_system.compiler.version
 
 
 def fetch() -> build_system.compiler.version.CompilerVersion:
-    return build_system.cmd.compiler.host.get_info.version.gnu.fetch(build_system.compiler.family.CompilerFamily.clang.value)
+    return build_system.cmd.compiler.host.get_info.version.gnu.fetch(build_system.compiler.family.CompilerFamily.CLANG.value)
