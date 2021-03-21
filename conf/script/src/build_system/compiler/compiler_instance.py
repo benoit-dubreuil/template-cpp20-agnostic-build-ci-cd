ab2188347c58b85a2a8b86ce3a8925a6b0ee25f2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-20 21:50:43 -0400

    Refactor

--- conf/script/src/build_system/compiler/compiler_instance.py
+++ conf/script/src/build_system/compiler/compiler_instance.py
@@ -7,10 +7,6 @@ from build_system.compiler.version import CompilerVersion
 
 @dataclass(order=True, frozen=True)
 class CompilerInstance:
-    compiler: CompilerFamily
+    compiler_family: CompilerFamily
     os_families: list[OSFamily]
     version: CompilerVersion
-
-    @classmethod
-    def filter_by_os(cls, all_compilers_reqs: dict[CompilerFamily, 'CompilerReqs'], os_family: OSFamily) -> list['CompilerReqs']:
-        return [compiler_reqs for compiler, compiler_reqs in all_compilers_reqs.items() if os_family in compiler_reqs.os_families]
