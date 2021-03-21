834079b0009ac75f87787c9d7d39e7d91dc15c1b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 11:04:22 -0400

    Added getter for current os_family

--- conf/script/src/build_system/compiler/compiler_instance.py
+++ conf/script/src/build_system/compiler/compiler_instance.py
@@ -16,3 +16,6 @@ class CompilerInstance:
     def create_from_installed_compiler(cls, compiler_family: CompilerFamily, os_family: OSFamily) -> 'CompilerInstance':
         version = build_system.cmd.compiler.host.get_info.version.fetch_by_compiler_family(compiler_family)
         return cls(compiler_family=compiler_family, os_families=[os_family], version=version)
+
+    def get_current_os_family(self) -> build_system.compiler.host.os_family.OSFamily:
+        return self.os_families[0]
