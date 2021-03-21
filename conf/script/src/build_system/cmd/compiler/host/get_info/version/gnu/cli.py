0185df085ab5cd3f4bfe6d2abe7d584d8bbb446d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 14:36:06 -0400

    Fixed imports

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/gnu/cli.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/gnu/cli.py
@@ -1,6 +1,8 @@
-from build_system.cmd.compiler.host.get_info import cli, version
-from build_system.compiler.family import CompilerFamily
+import build_system.cmd.compiler.host.get_info.cli
+import build_system.cmd.compiler.host.get_info.version
+import build_system.compiler.family
 
 
-def cli_fetch_gnu_compiler_version(compiler_family: CompilerFamily):
-    cli.cli_fetch_compiler_info_with_default_path(compiler_family, version.gnu.fetch)
+def cli_fetch_gnu_compiler_version(compiler_family: build_system.compiler.family.CompilerFamily):
+    build_system.cmd.compiler.host.get_info.cli.cli_fetch_compiler_info_with_default_path(compiler_family,
+                                                                                          build_system.cmd.compiler.host.get_info.version.gnu.fetch)
