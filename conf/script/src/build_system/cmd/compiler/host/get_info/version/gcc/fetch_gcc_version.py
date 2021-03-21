613b8ce28d0820257fb73ba26e1b7b1414324021
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-20 22:07:55 -0400

    Added fetch_gcc_verions script

---
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/gcc/fetch_gcc_version.py
@@ -0,0 +1,7 @@
+import build_system.cmd.compiler.host.get_info.version.gnu.fetch_gnu_compiler_version
+import build_system.compiler.family
+import build_system.compiler.version
+
+
+def fetch() -> build_system.compiler.version.CompilerVersion:
+    return build_system.cmd.compiler.host.get_info.version.gnu.fetch(build_system.compiler.family.CompilerFamily.GCC.value)
