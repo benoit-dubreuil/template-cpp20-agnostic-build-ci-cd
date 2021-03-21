d2220963a9ecc553e86fa8bf08a25bf07ffce0d7
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 10:56:59 -0400

    Added fetch_by_family script

---
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/fetch_by_family.py
@@ -0,0 +1,12 @@
+import build_system.compiler.family
+import build_system.cmd.compiler.host.get_info.version.msvc
+import build_system.cmd.compiler.host.get_info.version.clang
+import build_system.cmd.compiler.host.get_info.version.gcc
+
+fetch_func_by_family = {build_system.compiler.family.CompilerFamily.MSVC: build_system.cmd.compiler.host.get_info.version.msvc.fetch,
+                        build_system.compiler.family.CompilerFamily.CLANG: build_system.cmd.compiler.host.get_info.version.clang.fetch,
+                        build_system.compiler.family.CompilerFamily.GCC: build_system.cmd.compiler.host.get_info.version.gcc.fetch}
+
+
+def fetch_by_family(compiler_family: build_system.compiler.family.CompilerFamily) -> build_system.compiler.version.CompilerVersion:
+    return fetch_func_by_family[compiler_family]()
