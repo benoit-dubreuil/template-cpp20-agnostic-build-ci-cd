8629e67c4a92e776fe5bfd229558c8e4b5cb8f8f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 10:59:46 -0400

    Rename

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/__init__.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/__init__.py
@@ -1,2 +1,2 @@
 from build_system.cmd.compiler.host.get_info.version import compiler
-from build_system.cmd.compiler.host.get_info.version.fetch_by_family import fetch_by_family
+from build_system.cmd.compiler.host.get_info.version.fetch_by_compiler_family import fetch_by_compiler_family
