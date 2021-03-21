f3f03c69b781008e4bfcbf3bca77363d1fc39249
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 14:26:49 -0400

    Fixed imports

--- conf/script/src/build_system/cmd/compiler/host/get_info/location/msvc/cli.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/location/msvc/cli.py
@@ -1,19 +1,18 @@
 from pathlib import Path
 from typing import NoReturn, Optional, Union
 
-from build_system.cmd.compiler.host.get_info import location
-from build_system.cmd.compiler.host.get_info.cli import cli_fetch_compiler_info
-from build_system.compiler.family import CompilerFamily
-from utils.error.format import format_error_msg
+import build_system.cmd.compiler.host.get_info.location
+import build_system.compiler.family
+import utils.error.format
 
 
 def error_compiler_not_found() -> NoReturn:
-    error_msg = format_error_msg(f'{CompilerFamily.MSVC.name} compiler matching the requirements not found')
+    error_msg = utils.error.format.format_error_msg(f'{build_system.compiler.family.CompilerFamily.MSVC.name} compiler matching the requirements not found')
     raise FileNotFoundError(error_msg)
 
 
 def _find_no_arg(compiler_installation_path: Optional[Path] = None) -> Union[Path, NoReturn]:
-    compiler_installation_path: Optional[Path] = location.msvc.find(compiler_installation_path)
+    compiler_installation_path: Optional[Path] = build_system.cmd.compiler.host.get_info.location.msvc.find(compiler_installation_path)
 
     if compiler_installation_path is None:
         error_compiler_not_found()
@@ -22,4 +21,6 @@ def _find_no_arg(compiler_installation_path: Optional[Path] = None) -> Union[Pat
 
 
 def find() -> None:
-    cli_fetch_compiler_info(CompilerFamily.MSVC, _find_no_arg, desc_compiler_info='location', help_path_meaning='installation')
+    build_system.cmd.compiler.host.get_info.cli.cli_fetch_compiler_info(build_system.compiler.family.CompilerFamily.MSVC,
+                                                                        _find_no_arg, desc_compiler_info='location',
+                                                                        help_path_meaning='installation')
