46cbc33b1e3be69b2c2e2ea824f22edc0dcdf364
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 11:04:14 -0400

    Added create_from_installed_compiler factory method

--- conf/script/src/build_system/compiler/compiler_instance.py
+++ conf/script/src/build_system/compiler/compiler_instance.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from build_system.compiler.family import CompilerFamily
 from build_system.compiler.host.os_family import OSFamily
 from build_system.compiler.version import CompilerVersion
+import build_system.cmd.compiler.host.get_info.version
 
 
 @dataclass(order=True, frozen=True)
@@ -10,3 +11,8 @@ class CompilerInstance:
     compiler_family: CompilerFamily
     os_families: list[OSFamily]
     version: CompilerVersion
+
+    @classmethod
+    def create_from_installed_compiler(cls, compiler_family: CompilerFamily, os_family: OSFamily) -> 'CompilerInstance':
+        version = build_system.cmd.compiler.host.get_info.version.fetch_by_compiler_family(compiler_family)
+        return cls(compiler_family=compiler_family, os_families=[os_family], version=version)
