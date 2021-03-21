98ef73388f89e9cb8aeaac5179421f75cd0cf61e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 14:48:28 -0400

    Fixed imports

--- conf/script/src/build_system/cmd/setup/__main__.py
+++ conf/script/src/build_system/cmd/setup/__main__.py
@@ -2,11 +2,10 @@
 
 import build_system.cmd.setup.cli
 import utils.cli.main
-from build_system import cmd
 
 
 def main():
-    cmd.setup.cli.setup()
+    build_system.cmd.setup.cli.setup()
 
 
 utils.cli.main.wrap_main(main)
