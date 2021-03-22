6ba2cedd0dddf32db4596f0b8697394581de59ce
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 10:42:28 -0400

    Removed useless check

--- conf/script/src/build_system/cmd/setup/cli.py
+++ conf/script/src/build_system/cmd/setup/cli.py
@@ -17,6 +17,5 @@ def setup():
         raised_error.raise_or_exit_cli(arg_parser)
 
     except OSError as raised_error:
-        if not isinstance(raised_error, utils.error.managed.ManagedErrorMixin):
-            unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
-            unsupported_error.raise_or_exit_cli(arg_parser)
+        unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
+        unsupported_error.raise_or_exit_cli(arg_parser)
