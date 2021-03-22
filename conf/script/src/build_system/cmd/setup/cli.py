1e32a58b765d32147dbd7a0165adf430fda46578
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 11:29:11 -0400

    Integrated try_Cmd into setup cli

--- conf/script/src/build_system/cmd/setup/cli.py
+++ conf/script/src/build_system/cmd/setup/cli.py
@@ -1,21 +1,16 @@
 import argparse
 
 import build_system.cmd.hierarchy.find_build_dir
-import utils.error.cls_def
-import utils.error.managed
+import utils.cli.try_cmd
 
 
 def setup():
     arg_parser = argparse.ArgumentParser(
         description=f"Creates the {build_system.cmd.hierarchy.find_build_dir.BUILD_DIR_NAME} folder and setup specific build system builds inside.")
+
     # TODO
 
-    try:
+    def cli_cmd():
         build_system.cmd.setup.setup_build_system()
 
-    except utils.error.managed.ManagedErrorMixin as raised_error:
-        raised_error.raise_or_exit_cli(arg_parser)
-
-    except OSError as raised_error:
-        unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
-        unsupported_error.raise_or_exit_cli(arg_parser)
+    utils.cli.try_cmd.try_cmd_except_managed_errors(cli_cmd, arg_parser)
