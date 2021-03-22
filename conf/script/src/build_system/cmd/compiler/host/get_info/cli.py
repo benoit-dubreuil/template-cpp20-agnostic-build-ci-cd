8b1cd2cf1a1fe9750b2f421fa467a480f4ac88d8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 11:15:25 -0400

    Fixed error catching

--- conf/script/src/build_system/cmd/compiler/host/get_info/cli.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/cli.py
@@ -4,6 +4,7 @@ from typing import Any, Callable, Optional
 
 import build_system.compiler.family
 import utils.cli.arg_parsing
+import utils.error.cls_def
 import utils.error.managed
 
 
@@ -25,7 +26,9 @@ def fetch_compiler_info(compiler_family: build_system.compiler.family.CompilerFa
     except utils.error.managed.ManagedErrorMixin as raised_error:
         raised_error.raise_or_exit_cli(arg_parser)
 
-    # TODO : Ceck for OSErrors
+    except OSError as raised_error:
+        unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
+        unsupported_error.raise_or_exit_cli(arg_parser)
 
 
 def fetch_compiler_info_with_default_path(compiler_family: build_system.compiler.family.CompilerFamily, fetch_compiler_info_func: Callable[[Path], Any]) -> None:
