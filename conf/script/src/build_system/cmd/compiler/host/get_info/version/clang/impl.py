cd255cb341d511401d0c10f07471e67ce257d3ca
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 10:50:35 -0400

    Renamed func

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/clang/impl.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/clang/impl.py
@@ -3,5 +3,5 @@ import build_system.compiler.family
 import build_system.compiler.version
 
 
-def fetch() -> build_system.compiler.version.CompilerVersion:
+def fetch_version() -> build_system.compiler.version.CompilerVersion:
     return build_system.cmd.compiler.host.get_info.version.gnu.fetch_version(build_system.compiler.family.CompilerFamily.CLANG.value)
--- conf/script/src/build_system/cmd/compiler/host/get_info/version/fetch_by_criteria.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/fetch_by_criteria.py
@@ -5,7 +5,7 @@ import build_system.compiler.family
 import build_system.compiler.version
 
 fetch_func_by_compiler_family = {build_system.compiler.family.CompilerFamily.MSVC: build_system.cmd.compiler.host.get_info.version.msvc.fetch_version,
-                                 build_system.compiler.family.CompilerFamily.CLANG: build_system.cmd.compiler.host.get_info.version.clang.fetch,
+                                 build_system.compiler.family.CompilerFamily.CLANG: build_system.cmd.compiler.host.get_info.version.clang.fetch_version,
                                  build_system.compiler.family.CompilerFamily.GCC: build_system.cmd.compiler.host.get_info.version.gcc.fetch_version}
 
 
