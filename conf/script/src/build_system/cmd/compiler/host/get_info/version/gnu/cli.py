77a9664f3672f9f48a3ee0f233093cf6f9d38008
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 10:52:17 -0400

    Renamed func

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/gnu/cli.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/gnu/cli.py
@@ -2,6 +2,6 @@ import build_system.cmd.compiler.host.get_info.cli
 import build_system.compiler.family
 
 
-def cli_fetch_gnu_compiler_version(compiler_family: build_system.compiler.family.CompilerFamily):
+def fetch_gnu_compiler_version(compiler_family: build_system.compiler.family.CompilerFamily):
     build_system.cmd.compiler.host.get_info.cli.fetch_compiler_info_with_default_path(compiler_family,
                                                                                       build_system.cmd.compiler.host.get_info.version.gnu.fetch_version)
