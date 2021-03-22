c81a0c607033f7f114abc0eb27c37975c6f0abee
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 11:28:25 -0400

    Integrated try_cmd into find_root_dir and fixed imports

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
@@ -1,23 +1,16 @@
 import argparse
 
 import build_system.cmd.hierarchy.find_root_dir
-import build_system.cmd.hierarchy.find_root_dir.impl
-import utils.error.cls_def
-import utils.error.managed
+import utils.cli.try_cmd
 
 
 def find_root_dir():
     arg_parser = argparse.ArgumentParser(
-        description=f"Finds the project's root folder, where the '{build_system.cmd.hierarchy.find_root_dir.impl.BUILD_SYSTEM_CONF_FILENAME}' folder is. "
+        description=f"Finds the project's root folder, where the '{build_system.cmd.hierarchy.find_root_dir.BUILD_SYSTEM_CONF_FILENAME}' folder is. "
                     'It searches recursively parent folders upwards.')
 
-    try:
+    def cli_cmd():
         project_root = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
         print(project_root, end=str())
 
-    except utils.error.managed.ManagedErrorMixin as raised_error:
-        raised_error.raise_or_exit_cli(arg_parser)
-
-    except OSError as raised_error:
-        unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
-        unsupported_error.raise_or_exit_cli(arg_parser)
+    utils.cli.try_cmd.try_cmd_except_managed_errors(cli_cmd, arg_parser)
