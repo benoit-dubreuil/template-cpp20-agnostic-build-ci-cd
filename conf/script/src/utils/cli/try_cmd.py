2d7eec6d52ee11f04787a3fb3295732fef8e1a83
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 11:20:20 -0400

    Added try_cmd script

---
+++ conf/script/src/utils/cli/try_cmd.py
@@ -0,0 +1,17 @@
+import argparse
+from typing import Callable
+
+import utils.error.cls_def
+import utils.error.managed
+
+
+def try_cmd_except_managed_errors(cmd_func: Callable, arg_parser: argparse.ArgumentParser):
+    try:
+        cmd_func()
+
+    except utils.error.managed.ManagedErrorMixin as raised_error:
+        raised_error.raise_or_exit_cli(arg_parser)
+
+    except OSError as raised_error:
+        unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
+        unsupported_error.raise_or_exit_cli(arg_parser)
