6efdba8ef6485daefd5b33019859513853a38230
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 21:57:23 -0400

    Fixed imports

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/__init__.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/__init__.py
@@ -1,2 +1,1 @@
-from build_system.cmd.compiler.host.get_info.version import generic_fetch
 from build_system.cmd.compiler.host.get_info.version.fetch_by_compiler_family import fetch_by_compiler_family
