031e89da525d7ded93877849d44376f76a492a1b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 15:04:03 -0400

    Added arg parser info to find_root_dir

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
@@ -12,6 +12,8 @@ def find_root_dir():
                     f"'{colorama.Fore.LIGHTBLACK_EX}{build_system.cmd.hierarchy.find_root_dir.BUILD_SYSTEM_CONF_FILENAME}{colorama.Style.RESET_ALL}' folder is. "
                     'It searches recursively parent folders upwards.')
 
+    arg_parser.parse_args()
+
     def cli_cmd():
         project_root = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
         print(project_root, end=str())
