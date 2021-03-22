73163fd7aa1dae71de60ab0db755268d8bb95dbc
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 22:18:35 -0400

    Special case : fixed imports

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 from typing import Optional
 
-import build_system.cmd.setup.build_name
-import build_system.cmd.setup.create_build_dir
-
 
 def setup_build_system(root_dir: Optional[Path] = None):
+    import build_system.cmd.setup.build_name
+    import build_system.cmd.setup.create_build_dir
+
     build_dir = build_system.cmd.setup.create_build_dir.find_or_create_build_dir(root_dir)
 
     all_build_subdir_names = build_system.cmd.setup.build_name.generate_all_build_subdir_names()
