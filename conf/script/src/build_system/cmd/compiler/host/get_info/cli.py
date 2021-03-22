a94f2ed84ada26897b53f6a135d68509e2091a11
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 11:23:35 -0400

    Integrated try_cmd into host/get_info/cli

--- conf/script/src/build_system/cmd/compiler/host/get_info/cli.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/cli.py
@@ -4,8 +4,7 @@ from typing import Any, Callable, Optional
 
 import build_system.compiler.family
 import utils.cli.arg_parsing
-import utils.error.cls_def
-import utils.error.managed
+import utils.cli.try_cmd
 
 
 def fetch_compiler_info(compiler_family: build_system.compiler.family.CompilerFamily,
@@ -19,16 +18,11 @@ def fetch_compiler_info(compiler_family: build_system.compiler.family.CompilerFa
 
     compiler_path: Optional[Path] = utils.cli.arg_parsing.parse_optional_path_arg(arg_parser)
 
-    try:
+    def cli_cmd():
         compiler_info = fetch_compiler_info_func(compiler_path)
         print(compiler_info, end=str())
 
-    except utils.error.managed.ManagedErrorMixin as raised_error:
-        raised_error.raise_or_exit_cli(arg_parser)
-
-    except OSError as raised_error:
-        unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
-        unsupported_error.raise_or_exit_cli(arg_parser)
+    utils.cli.try_cmd.try_cmd_except_managed_errors(cli_cmd, arg_parser)
 
 
 def fetch_compiler_info_with_default_path(compiler_family: build_system.compiler.family.CompilerFamily, fetch_compiler_info_func: Callable[[Path], Any]) -> None:
