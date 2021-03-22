cf8ce38303f1e9f44c84849bc32c85ead36aa697
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 11:22:28 -0400

    Integrated try_cmd into clean_build_dir

--- conf/script/src/build_system/cmd/clean_build_dir/cli.py
+++ conf/script/src/build_system/cmd/clean_build_dir/cli.py
@@ -4,8 +4,7 @@ from typing import Final
 import build_system.cmd.hierarchy.find_build_dir
 import utils.cli.arg
 import utils.cli.arg_parsing
-import utils.error.managed
-import utils.error.cls_def
+import utils.cli.try_cmd
 from utils.more_typing import AnyPath
 
 ROOT_DIR_ARG: Final[utils.cli.arg.CLIArg] = utils.cli.arg.CLIArg('rootdir')
@@ -18,12 +17,7 @@ def clean_build_dir():
 
     root_dir: AnyPath = utils.cli.arg_parsing.parse_optional_path_arg(arg_parser, ROOT_DIR_ARG)
 
-    try:
+    def cli_cmd():
         build_system.cmd.clean_build_dir.clean_build_dir(root_dir)
 
-    except utils.error.managed.ManagedErrorMixin as raised_error:
-        raised_error.raise_or_exit_cli(arg_parser)
-
-    except OSError as raised_error:
-        unsupported_error = utils.error.cls_def.UnsupportedError(raised_error)
-        unsupported_error.raise_or_exit_cli(arg_parser)
+    utils.cli.try_cmd.try_cmd_except_managed_errors(cli_cmd, arg_parser)
