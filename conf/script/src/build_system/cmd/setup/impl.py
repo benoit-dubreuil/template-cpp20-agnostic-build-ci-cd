92a671e05f79da67dd14c405cb77a4cc6ef6a181
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-21 22:33:56 -0400

    Finally create the build dirs :)

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -9,7 +9,4 @@ def setup_build_system(root_dir: Optional[Path] = None):
     build_dir = build_system.cmd.setup.create_build_dir.find_or_create_build_dir(root_dir)
 
     all_build_subdir_names = build_system.cmd.setup.build_name.generate_all_build_subdir_names()
-    # create_all_build_subdirs(build_dir, all_build_subdir_names)
-
-    # TODO : Remove when #58 is done
-    print(*all_build_subdir_names, sep='\n', end=str())
+    build_system.cmd.setup.create_build_dir.create_all_build_subdirs(build_dir, all_build_subdir_names)
