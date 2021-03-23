fcfcbed7e5dfc3b956e9a1823be734105e281ab4
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 22:02:04 -0400

    build_dir as a param instead of root_dir

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
@@ -5,20 +5,17 @@ import utils.error.cls_def
 import utils.more_path
 
 
-def create_target_build_dirs(root_dir: Optional[Path] = None) -> list[Path]:
+def create_target_build_dirs(build_dir: Optional[Path] = None) -> list[Path]:
     import build_system.cmd.hierarchy.clean_build_dir
     import build_system.cmd.hierarchy.create_build_dir
-    import build_system.cmd.hierarchy.find_root_dir
     import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_name_generation
     import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation
 
-    if root_dir is None:
-        root_dir = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
+    if build_dir is None:
+        build_dir = build_system.cmd.hierarchy.create_build_dir.create_build_dir(root_dir=build_dir)
     else:
-        if not root_dir.exists():
-            raise utils.error.cls_def.RootDirNotFoundError()
-
-    build_dir = build_system.cmd.hierarchy.create_build_dir.create_build_dir(root_dir=root_dir)
+        if not build_dir.exists():
+            raise utils.error.cls_def.BuildDirNotFoundError()
 
     if build_dir.exists():
         if not utils.more_path.is_dir_empty(build_dir):
