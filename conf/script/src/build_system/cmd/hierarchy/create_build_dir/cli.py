1c6f0302002d6efee940584fbc74b1d86607f5d3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 22:00:58 -0400

    Refactor

--- conf/script/src/build_system/cmd/hierarchy/create_build_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/create_build_dir/cli.py
@@ -12,7 +12,7 @@ from utils.more_typing import AnyPath
 def create_build_dir():
     arg_parser = argparse.ArgumentParser(
         description=f"Creates the project's '{colorama.Fore.LIGHTBLACK_EX}{build_system.cmd.hierarchy.consts.BUILD_DIR_NAME}{colorama.Style.RESET_ALL}' folder.")
-    utils.cli.arg_parsing.add_optional_path_arg(arg_parser, build_system.cmd.hierarchy.consts.ROOT_DIR_ARG, path_arg_help=f"The project's root directory")
+    utils.cli.arg_parsing.add_optional_path_arg(arg_parser, build_system.cmd.hierarchy.consts.ROOT_DIR_ARG, path_arg_help="The project's root directory")
 
     root_dir: AnyPath = utils.cli.arg_parsing.parse_optional_path_arg(arg_parser, build_system.cmd.hierarchy.consts.ROOT_DIR_ARG)
     arg_parser.parse_args()
