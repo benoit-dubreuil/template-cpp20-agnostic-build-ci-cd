2c054f3014568c5f5dbcae28f1f9e2510e48d01d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-23 14:48:13 -0400

    Integrated assure_root_dir_exists into create_build_dir

--- conf/script/src/build_system/cmd/hierarchy/create_build_dir/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/create_build_dir/impl.py
@@ -1,20 +1,15 @@
 from pathlib import Path
 from typing import Optional
 
+import build_system.cmd.hierarchy.assure_arg_integrity
 import build_system.cmd.hierarchy.consts
 import build_system.cmd.hierarchy.find_build_dir
-import build_system.cmd.hierarchy.find_root_dir
 import utils.error.cls_def
 
 
 def create_build_dir(root_dir: Optional[Path] = None) -> Path:
-    if root_dir is None:
-        root_dir = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
-    else:
-        if not root_dir.exists():
-            raise utils.error.cls_def.RootDirNotFoundError()
-
-    build_dir = build_system.cmd.hierarchy.find_build_dir.find_build_dir_path(root_dir)
+    root_dir = build_system.cmd.hierarchy.assure_arg_integrity.assure_root_dir_exists(root_dir=root_dir)
+    build_dir = build_system.cmd.hierarchy.find_build_dir.find_build_dir_path(root_dir=root_dir)
 
     if build_dir.exists():
         if not build_dir.is_dir():
