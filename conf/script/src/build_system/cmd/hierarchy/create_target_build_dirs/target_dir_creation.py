ecf96781457628285dbb66578c49d3d7b3e97c0b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 21:48:51 -0400

    Added returns to target_dir_creation

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_creation.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_creation.py
@@ -3,11 +3,18 @@ from pathlib import Path
 import build_system.cmd.hierarchy.consts
 
 
-def create_target_build_dir(build_dir: Path, build_subdir: str):
-    build_subdir_path = build_dir / build_subdir
-    build_subdir_path.mkdir(mode=build_system.cmd.hierarchy.consts.BUILD_DIR_PERMISSIONS, exist_ok=True)
+def create_target_build_dir(build_dir: Path, target_build_dir_name: str) -> Path:
+    target_build_dir = build_dir / target_build_dir_name
+    target_build_dir.mkdir(mode=build_system.cmd.hierarchy.consts.BUILD_DIR_PERMISSIONS, exist_ok=True)
+
+    return target_build_dir
 
 
-def create_all_target_build_dirs(build_dir: Path, all_build_subdirs: list[str]):
-    for build_subdir in all_build_subdirs:
-        create_target_build_dir(build_dir, build_subdir)
+def create_all_target_build_dirs(build_dir: Path, all_target_build_dir_names: list[str]) -> list[Path]:
+    all_target_build_dirs: list[Path] = []
+
+    for target_build_dir_name in all_target_build_dir_names:
+        target_build_dir = create_target_build_dir(build_dir, target_build_dir_name)
+        all_target_build_dirs.append(target_build_dir)
+
+    return all_target_build_dirs
