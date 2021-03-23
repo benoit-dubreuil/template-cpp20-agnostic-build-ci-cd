b6d1445c5b43b5ac99d078b739816525dc8d24fa
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-23 14:32:12 -0400

    Extracted _assure_build_dir_is_empty func

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
@@ -17,16 +17,18 @@ def _assure_build_dir_exists(build_dir_cli_arg):
     return build_dir_cli_arg
 
 
+def _assure_build_dir_is_empty(build_dir):
+    if not utils.more_path.is_dir_empty(build_dir):
+        raise utils.error.cls_def.BuildDirNotEmptyError()
+
+
 def create_target_build_dirs(build_dir: Optional[Path] = None) -> list[Path]:
     import build_system.cmd.hierarchy.clean_build_dir
     import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_name_generation
     import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation
 
     build_dir = _assure_build_dir_exists(build_dir)
-
-    if build_dir.exists():
-        if not utils.more_path.is_dir_empty(build_dir):
-            raise utils.error.cls_def.BuildDirNotEmptyError()
+    _assure_build_dir_is_empty(build_dir)
 
     target_build_dir_names = build_system.cmd.hierarchy.create_target_build_dirs.target_dir_name_generation.generate_target_build_dir_names()
 
