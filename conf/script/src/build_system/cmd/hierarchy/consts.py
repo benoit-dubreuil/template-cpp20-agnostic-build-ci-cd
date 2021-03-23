b67a820db1d249424125ba7ccfb9f03ca8b3f8c1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 21:57:01 -0400

    Added BUILD_DIR_ARG const

--- conf/script/src/build_system/cmd/hierarchy/consts.py
+++ conf/script/src/build_system/cmd/hierarchy/consts.py
@@ -3,5 +3,6 @@ from typing import Final
 import utils.cli.arg
 
 ROOT_DIR_ARG: Final[utils.cli.arg.CLIArg] = utils.cli.arg.CLIArg('rootdir')
+BUILD_DIR_ARG: Final[utils.cli.arg.CLIArg] = utils.cli.arg.CLIArg('builddir')
 BUILD_DIR_NAME: Final[str] = 'build'
 BUILD_DIR_PERMISSIONS: Final[int] = 0o770
