39771d6908e6b9ab68850f1a0f8701df735fedb0
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-22 20:47:08 -0400

    Refactor

--- conf/script/src/build_system/cmd/setup/build_name.py
+++ conf/script/src/build_system/cmd/setup/build_name.py
@@ -3,20 +3,25 @@ from typing import Final
 import build_system.build_target.build_type
 import build_system.build_target.target_name
 import build_system.cmd.setup.required_host_info
+import build_system.compiler.compiler_instance
 import build_system.compiler.family
 import build_system.compiler.host.architecture
 import build_system.compiler.host.os_family
 import build_system.compiler.version
 
 
-def assemble_target_build_types() -> list[build_system.build_target.build_type.TargetBuildType]:
+def _assemble_target_build_types() -> list[build_system.build_target.build_type.TargetBuildType]:
     return list(build_system.build_target.build_type.TargetBuildType)
 
 
-def generate_all_build_subdir_names() -> list[str]:
+def _fetch_supported_compiler_instance() -> list[build_system.compiler.compiler_instance.CompilerInstance]:
     os_family = build_system.cmd.setup.required_host_info.fetch_os_family()
-    supported_compiler_instances = build_system.cmd.setup.required_host_info.fetch_supported_compiler_instances_by_os(os_family)
-    target_build_types = assemble_target_build_types()
+    return build_system.cmd.setup.required_host_info.fetch_supported_compiler_instances_by_os(os_family)
+
+
+def generate_all_build_subdir_names() -> list[str]:
+    supported_compiler_instances = _fetch_supported_compiler_instance()
+    target_build_types = _assemble_target_build_types()
     arch = build_system.cmd.setup.required_host_info.detect_arch()
 
     build_dir_names = []
