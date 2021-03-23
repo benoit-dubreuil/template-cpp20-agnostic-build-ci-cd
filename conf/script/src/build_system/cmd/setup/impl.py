0e9610b1bd6e1d4805c080e3ea22868374f90f73
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-23 14:53:39 -0400

    Extracted _recreate_build_dir func

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -4,17 +4,23 @@ from typing import Optional
 import build_system.cmd.hierarchy.assure_arg_integrity
 
 
-def setup_build_system(root_dir: Optional[Path] = None):
+def _recreate_build_dir(root_dir):
     import build_system.cmd.hierarchy.clean_build_dir
     import build_system.cmd.hierarchy.create_build_dir
     import build_system.cmd.hierarchy.find_build_dir
-    import build_system.cmd.hierarchy.create_target_build_dirs
-
-    root_dir = build_system.cmd.hierarchy.assure_arg_integrity.assure_root_dir_exists(root_dir=root_dir)
 
     build_dir = build_system.cmd.hierarchy.find_build_dir.find_build_dir_path(root_dir=root_dir)
     build_system.cmd.hierarchy.clean_build_dir.clean_build_dir(build_dir=build_dir, ignore_errors=True)
     build_dir = build_system.cmd.hierarchy.create_build_dir.create_build_dir(root_dir=root_dir)
+
+    return build_dir
+
+
+def setup_build_system(root_dir: Optional[Path] = None):
+    import build_system.cmd.hierarchy.create_target_build_dirs
+
+    root_dir = build_system.cmd.hierarchy.assure_arg_integrity.assure_root_dir_exists(root_dir=root_dir)
+    build_dir = _recreate_build_dir(root_dir)
 
     target_build_dirs = build_system.cmd.hierarchy.create_target_build_dirs.create_target_build_dirs(build_dir=build_dir)
     # TODO
