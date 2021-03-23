09573c9b53155584ecb5a21a3cc8fce40ef4603d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-23 14:43:05 -0400

    Added assure_build_dir_exists to assure_arg_integrity

--- conf/script/src/build_system/cmd/hierarchy/assure_arg_integrity.py
+++ conf/script/src/build_system/cmd/hierarchy/assure_arg_integrity.py
@@ -14,3 +14,15 @@ def assure_root_dir_exists(root_dir: Optional[Path] = None) -> Path:
             raise utils.error.cls_def.RootDirNotFoundError()
 
     return root_dir
+
+
+def assure_build_dir_exists(build_dir: Optional[Path] = None) -> Path:
+    import build_system.cmd.hierarchy.create_build_dir
+
+    if build_dir is None:
+        build_dir = build_system.cmd.hierarchy.create_build_dir.create_build_dir()
+    else:
+        if not build_dir.exists():
+            raise utils.error.cls_def.BuildDirNotFoundError()
+
+    return build_dir
