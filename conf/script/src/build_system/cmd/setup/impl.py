459896a4357bf2197213f5109c57a792bca6cbc3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-23 14:56:21 -0400

    Extracted _create_target_build_dirs func

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -4,7 +4,7 @@ from typing import Optional
 import build_system.cmd.hierarchy.assure_arg_integrity
 
 
-def _recreate_build_dir(root_dir):
+def _recreate_build_dir(root_dir: Optional[Path] = None) -> Path:
     import build_system.cmd.hierarchy.clean_build_dir
     import build_system.cmd.hierarchy.create_build_dir
     import build_system.cmd.hierarchy.find_build_dir
@@ -16,11 +16,18 @@ def _recreate_build_dir(root_dir):
     return build_dir
 
 
-def setup_build_system(root_dir: Optional[Path] = None):
+def _create_target_build_dirs(root_dir: Optional[Path] = None) -> list[Path]:
     import build_system.cmd.hierarchy.create_target_build_dirs
 
     root_dir = build_system.cmd.hierarchy.assure_arg_integrity.assure_root_dir_exists(root_dir=root_dir)
     build_dir = _recreate_build_dir(root_dir)
-
     target_build_dirs = build_system.cmd.hierarchy.create_target_build_dirs.create_target_build_dirs(build_dir=build_dir)
+
+    return target_build_dirs
+
+
+def setup_build_system(root_dir: Optional[Path] = None):
+    target_build_dirs = _create_target_build_dirs(root_dir)
     # TODO
+
+    print(target_build_dirs)
