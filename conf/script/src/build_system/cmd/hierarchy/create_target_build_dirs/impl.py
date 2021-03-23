8ad117b27d233fb539faa5d91f2a2dca089f4349
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-23 14:30:38 -0400

    Extracted _assure_build_dir_exists func

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
@@ -5,17 +5,24 @@ import utils.error.cls_def
 import utils.more_path
 
 
+def _assure_build_dir_exists(build_dir_cli_arg):
+    import build_system.cmd.hierarchy.create_build_dir
+
+    if build_dir_cli_arg is None:
+        build_dir_cli_arg = build_system.cmd.hierarchy.create_build_dir.create_build_dir()
+    else:
+        if not build_dir_cli_arg.exists():
+            raise utils.error.cls_def.BuildDirNotFoundError()
+
+    return build_dir_cli_arg
+
+
 def create_target_build_dirs(build_dir: Optional[Path] = None) -> list[Path]:
     import build_system.cmd.hierarchy.clean_build_dir
-    import build_system.cmd.hierarchy.create_build_dir
     import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_name_generation
     import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation
 
-    if build_dir is None:
-        build_dir = build_system.cmd.hierarchy.create_build_dir.create_build_dir()
-    else:
-        if not build_dir.exists():
-            raise utils.error.cls_def.BuildDirNotFoundError()
+    build_dir = _assure_build_dir_exists(build_dir)
 
     if build_dir.exists():
         if not utils.more_path.is_dir_empty(build_dir):
