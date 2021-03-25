84de94877b40d4329dfb43b31eac92ae7b49d623
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-25 19:53:26 -0400

    Implemented GCC cli script

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/gcc/cli.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/gcc/cli.py
@@ -1,12 +1,8 @@
 from pathlib import Path
-from typing import NoReturn, Optional, Union
 
-import build_system.cmd.compiler.host.get_info.cli
-import build_system.cmd.compiler.host.get_info.location.msvc.cli
+import build_system.cmd.compiler.host.get_info.version.gnu.cli
 import build_system.compiler.family
-import build_system.compiler.version
-import utils.error.cls_def
 
 
 def fetch_gcc_version() -> None:
-    build_system.cmd.compiler.host.get_info.cli.fetch_compiler_info(build_system.compiler.family.CompilerFamily.MSVC, _fetch_version_no_arg, help_path_meaning='installation')
+    build_system.cmd.compiler.host.get_info.version.gnu.cli.fetch_version(compiler_family=build_system.compiler.family.CompilerFamily.GCC)
