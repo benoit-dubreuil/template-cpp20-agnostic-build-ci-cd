4965c8c464229c7c8e8b3ea931d8eb7546c64729
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-26 15:52:11 -0400

    Made CompilerInstance abstract and added installation_dir member var

--- conf/script/src/build_system/compiler/compiler_instance.py
+++ conf/script/src/build_system/compiler/compiler_instance.py
@@ -1,4 +1,6 @@
+import abc
 import dataclasses
+import pathlib
 
 import build_system.compiler.family
 import build_system.compiler.host.architecture
@@ -7,16 +9,24 @@ import build_system.compiler.version
 
 
 @dataclasses.dataclass(order=True, frozen=True)
-class CompilerInstance:
+class CompilerInstance(metaclass=abc.ABCMeta):
     compiler_family: build_system.compiler.family.CompilerFamily
     os_family: build_system.compiler.host.os_family.OSFamily
     version: build_system.compiler.version.CompilerVersion
+    installation_dir: pathlib.Path
 
     @classmethod
     def create_from_installed_compiler(cls,
                                        compiler_family: build_system.compiler.family.CompilerFamily,
-                                       os_family: build_system.compiler.host.os_family.OSFamily,
+                                       os_family: build_system.compiler.host.os_family.OSFamily) -> 'CompilerInstance':
         import build_system.cmd.compiler.host.get_info.version.fetch_by_criteria
 
+        installation_dir = cls._find_installation_dir_by_compiler_family()
         version = build_system.cmd.compiler.host.get_info.version.fetch_by_criteria.fetch_by_compiler_family(compiler_family)
-        return cls(compiler_family=compiler_family, os_family=os_family, version=version, installation_path=installation_path)
+
+        return cls(compiler_family=compiler_family, os_family=os_family, version=version, installation_dir=installation_dir)
+
+    @staticmethod
+    @abc.abstractmethod
+    def _find_installation_dir_by_compiler_family():
+        ...
