118582b04d3c3fc884dafc110bedd17ef86c6020
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-26 15:43:55 -0400

    Made os_families single inside CompilerInstance

--- conf/script/src/build_system/compiler/compiler_instance.py
+++ conf/script/src/build_system/compiler/compiler_instance.py
@@ -9,18 +9,14 @@ import build_system.compiler.version
 @dataclasses.dataclass(order=True, frozen=True)
 class CompilerInstance:
     compiler_family: build_system.compiler.family.CompilerFamily
-    # TODO : Make single, i.e. not list
-    os_families: list[build_system.compiler.host.os_family.OSFamily]
+    os_family: build_system.compiler.host.os_family.OSFamily
     version: build_system.compiler.version.CompilerVersion
 
     @classmethod
     def create_from_installed_compiler(cls,
                                        compiler_family: build_system.compiler.family.CompilerFamily,
-                                       os_family: build_system.compiler.host.os_family.OSFamily) -> 'CompilerInstance':
+                                       os_family: build_system.compiler.host.os_family.OSFamily,
         import build_system.cmd.compiler.host.get_info.version.fetch_by_criteria
 
         version = build_system.cmd.compiler.host.get_info.version.fetch_by_criteria.fetch_by_compiler_family(compiler_family)
-        return cls(compiler_family=compiler_family, os_families=[os_family], version=version)
-
-    def get_current_os_family(self) -> build_system.compiler.host.os_family.OSFamily:
-        return self.os_families[0]
+        return cls(compiler_family=compiler_family, os_family=os_family, version=version, installation_path=installation_path)
