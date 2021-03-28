734c5c5db34901baf7ab448700c26ed5befde588
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-28 15:47:58 -0400

    Refactor

--- conf/script/src/build_system/compiler/supported_installed_instance/__init__.py
+++ conf/script/src/build_system/compiler/supported_installed_instance/__init__.py
@@ -1,1 +1,2 @@
-from build_system.compiler.supported_installed_instance.fetch_supported_installed_instances import fetch_supported_installed_compiler_instances_by_os as fetch_by_os
+from build_system.compiler.supported_installed_instance.fetch_supported_installed_instances import fetch_supported_installed_compiler_instances as fetch_all, \
+    fetch_supported_installed_compiler_instances_by_os_and_arch as fetch_by_os
