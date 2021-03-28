fe4adf0915af53c6f9a19a71031543a7308817af
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-28 15:50:00 -0400

    Renamed pkg

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_name_generation.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_name_generation.py
@@ -3,7 +3,7 @@ import build_system.build_target.name
 import build_system.compiler.host.architecture
 import build_system.compiler.host.os_family
 import build_system.compiler.installed_instance
-import build_system.compiler.supported_installed_instance
+import build_system.compiler.supported_installed_instances
 
 
 def _assemble_target_build_types() -> list[build_system.build_target.build_type.TargetBuildType]:
@@ -11,11 +11,11 @@ def _assemble_target_build_types() -> list[build_system.build_target.build_type.
 
 
 def generate_target_build_dir_names() -> list[str]:
-    supported_compiler_instances = build_system.compiler.supported_installed_instance.fetch_all()
+    TO_RENAME = build_system.compiler.supported_installed_instances.fetch_all()
     target_build_types = _assemble_target_build_types()
 
     build_dir_names = []
-    for compiler_instance in supported_compiler_instances:
+    for compiler_instance in TO_RENAME:
         for target_build_type in target_build_types:
             target_name = build_system.build_target.name.TargetBuildName(compiler_instance=compiler_instance, target_build_type=target_build_type)
             build_dir_names.append(str(target_name))
