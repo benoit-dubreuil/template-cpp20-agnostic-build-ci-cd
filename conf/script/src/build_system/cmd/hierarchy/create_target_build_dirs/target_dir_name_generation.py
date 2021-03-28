a632f016f9b2df065bd30b0860e33c18791c898a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-28 15:51:31 -0400

    Fixed imoports

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_name_generation.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_name_generation.py
@@ -1,8 +1,5 @@
 import build_system.build_target.build_type
 import build_system.build_target.name
-import build_system.compiler.host.architecture
-import build_system.compiler.host.os_family
-import build_system.compiler.installed_instance
 import build_system.compiler.supported_installed_instances
 
 
@@ -11,11 +8,11 @@ def _assemble_target_build_types() -> list[build_system.build_target.build_type.
 
 
 def generate_target_build_dir_names() -> list[str]:
-    TO_RENAME = build_system.compiler.supported_installed_instances.fetch_all()
+    host_compilers = build_system.compiler.supported_installed_instances.fetch_all()
     target_build_types = _assemble_target_build_types()
 
     build_dir_names = []
-    for compiler_instance in TO_RENAME:
+    for compiler_instance in host_compilers:
         for target_build_type in target_build_types:
             target_name = build_system.build_target.name.TargetBuildName(compiler_instance=compiler_instance, target_build_type=target_build_type)
             build_dir_names.append(str(target_name))
