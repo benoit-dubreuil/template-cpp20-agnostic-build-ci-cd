8e0dd91a828c9cc87b2c56197dcffcf17ff07b86
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-28 11:31:49 -0400

    WIP : Seems to work now

--- conf/script/src/utils/error/try_external_errors.py
+++ conf/script/src/utils/error/try_external_errors.py
@@ -1,8 +1,29 @@
-from typing import Callable
+from typing import Callable, Type, Union
+
+import utils.error.managed
+import utils.error.cls_def
+from utils.error.cls_def import UnsupportedError
 
 
-def try_manage_external_errors(func_to_try: Callable, external_errors_to_manage: [Exception] = [Exception]):
+def try_manage_external_errors(func_to_try: Callable,
+                               external_errors_to_manage: {tuple[Type[Exception], ...]: Type[utils.error.managed.ManagedErrorMixin]} = {
+                                   (Exception): utils.error.cls_def.UnsupportedError}):
     assert func_to_try is not None
     assert external_errors_to_manage is not None
     assert len(external_errors_to_manage) > 0
-    
+
+    try:
+        func_to_try()
+
+    except Exception as raised_error:
+
+        supported_external_error_types: tuple[Type[Exception]]
+        managed_error_type: Type[UnsupportedError]
+
+        for supported_external_error_types, managed_error_type in external_errors_to_manage.items():
+            for external_error_type in supported_external_error_types:
+                if isinstance(raised_error, external_error_type):
+                    managed_error = managed_error_type()
+                    managed_error.with_traceback(raised_error.__traceback__)
+
+                    raise managed_error
