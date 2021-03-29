408e289174cca6520d655154e0fa918b158dcc20
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-29 10:24:57 -0400

    Refactor param types

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
@@ -12,7 +12,7 @@ def _assure_build_dir_is_empty(build_dir):
         raise utils.error.cls_def.BuildDirNotEmptyError()
 
 
-def _generate_target_build_dir_names(supported_installed_compilers: list[build_system.compiler.installed_instance.CompilerInstance] = None):
+def _generate_target_build_dir_names(supported_installed_compilers: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None):
     import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_name_generation
 
     target_build_dir_names = build_system.cmd.hierarchy.create_target_build_dirs.target_dir_name_generation.generate_target_build_dir_names(
@@ -24,14 +24,16 @@ def _generate_target_build_dir_names(supported_installed_compilers: list[build_s
     return target_build_dir_names
 
 
-def _create_all_target_build_dirs(build_dir, supported_installed_compilers: list[build_system.compiler.installed_instance.CompilerInstance] = None):
+def _create_all_target_build_dirs(build_dir: Path,
+                                  supported_installed_compilers: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None):
     import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation
 
     target_build_dir_names = _generate_target_build_dir_names(supported_installed_compilers=supported_installed_compilers)
     return build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation.create_all_target_build_dirs(build_dir, target_build_dir_names)
 
 
-def create_target_build_dirs(build_dir: Optional[Path] = None, supported_installed_compilers: list[build_system.compiler.installed_instance.CompilerInstance] = None) -> list[Path]:
+def create_target_build_dirs(build_dir: Optional[Path] = None,
+                             supported_installed_compilers: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None) -> list[Path]:
     build_dir = build_system.cmd.hierarchy.assure_arg_integrity.assure_build_dir_exists(build_dir=build_dir)
     _assure_build_dir_is_empty(build_dir)
 
