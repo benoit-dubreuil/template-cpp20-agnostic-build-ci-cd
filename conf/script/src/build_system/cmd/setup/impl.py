0523cd7d5d54da52e112cb084ebe78aa3dcf0336
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-29 20:32:48 -0400

    Reorder and extracted _fetch_current_package_path func

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -9,16 +9,19 @@ import build_system.compiler.installed_instance.msvc
 import build_system.compiler.supported_installed_instances
 
 
-def _recreate_build_dir(root_dir: Optional[Path] = None) -> Path:
-    import build_system.cmd.hierarchy.clean_build_dir
-    import build_system.cmd.hierarchy.create_build_dir
-    import build_system.cmd.hierarchy.find_build_dir
+def setup_build_system(root_dir: Optional[Path] = None):
+    # TODO : Foreach compiler, foreach target build dir...
+    host_compilers: list[build_system.compiler.installed_instance.CompilerInstance] = build_system.compiler.supported_installed_instances.fetch_all()
+    target_build_dirs: dict[(build_system.compiler.installed_instance.CompilerInstance, list[Path])] = _create_target_build_dirs(root_dir=root_dir,
+                                                                                                                                 supported_installed_compilers=host_compilers)
 
-    build_dir = build_system.cmd.hierarchy.find_build_dir.get_build_dir_path_relative_to_root_dir(root_dir=root_dir)
-    build_system.cmd.hierarchy.clean_build_dir.clean_build_dir(build_dir=build_dir, ignore_errors=True)
-    build_dir = build_system.cmd.hierarchy.create_build_dir.create_build_dir(root_dir=root_dir)
+    # TODO : WIP
+    current_package_path = _fetch_current_package_path()
 
-    return build_dir
+    meson_launcher: str = str(current_package_path)
+    meson_cli_args: list[str] = ['-h']
+
+    mesonbuild.mesonmain.run(meson_cli_args, meson_launcher)
 
 
 def _create_target_build_dirs(root_dir: Optional[Path] = None,
@@ -34,10 +37,16 @@ def _create_target_build_dirs(root_dir: Optional[Path] = None,
     return target_build_dirs
 
 
-def _setup_host_compiler_target_build_dir(host_compiler: build_system.compiler.installed_instance.CompilerInstance,
-                                          target_build_dir: Path):
-    # TODO
-    ...
+def _recreate_build_dir(root_dir: Optional[Path] = None) -> Path:
+    import build_system.cmd.hierarchy.clean_build_dir
+    import build_system.cmd.hierarchy.create_build_dir
+    import build_system.cmd.hierarchy.find_build_dir
+
+    build_dir = build_system.cmd.hierarchy.find_build_dir.get_build_dir_path_relative_to_root_dir(root_dir=root_dir)
+    build_system.cmd.hierarchy.clean_build_dir.clean_build_dir(build_dir=build_dir, ignore_errors=True)
+    build_dir = build_system.cmd.hierarchy.create_build_dir.create_build_dir(root_dir=root_dir)
+
+    return build_dir
 
 
 def _setup_host_compiler_all_target_build_dirs(host_compiler: build_system.compiler.installed_instance.CompilerInstance,
@@ -52,18 +61,15 @@ def _setup_host_compiler_all_target_build_dirs(host_compiler: build_system.compi
         host_compiler.teardown_env_vars()
 
 
-def setup_build_system(root_dir: Optional[Path] = None):
-    # TODO : Foreach compiler, foreach target build dir...
-    host_compilers: list[build_system.compiler.installed_instance.CompilerInstance] = build_system.compiler.supported_installed_instances.fetch_all()
-    target_build_dirs: dict[(build_system.compiler.installed_instance.CompilerInstance, list[Path])] = _create_target_build_dirs(root_dir=root_dir,
-                                                                                                                                 supported_installed_compilers=host_compilers)
+def _setup_host_compiler_target_build_dir(host_compiler: build_system.compiler.installed_instance.CompilerInstance,
+                                          target_build_dir: Path):
+    # TODO
+    ...
 
-    # TODO : WIP
+
+def _fetch_current_package_path():
     current_package_path = Path(__file__).parent
     current_package_path.resolve(strict=True)
     current_package_path = current_package_path.absolute()
 
-    meson_launcher: str = str(current_package_path)
-    meson_cli_args: list[str] = ['-h']
-
-    mesonbuild.mesonmain.run(meson_cli_args, meson_launcher)
+    return current_package_path
