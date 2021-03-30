21d6bca94d183c808e055abd3a8a2c3bf770be86
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 09:35:17 -0400

    Impl BuildTarget into CompilerInstanceTargets

--- conf/script/src/build_system/build_target/compiler_instance_targets.py
+++ conf/script/src/build_system/build_target/compiler_instance_targets.py
@@ -1,25 +1,13 @@
 import dataclasses
 
-import build_system.build_target.name
+import build_system.build_target.build_target_cls
 import build_system.compiler.installed_instance
 
 
 @dataclasses.dataclass(order=True, frozen=True)
 class CompilerInstanceTargets:
     compiler_instance: build_system.compiler.installed_instance.CompilerInstance
-    targets: list[build_system.build_target.name.TargetBuildName]
-
-    def __init__(self,
-                 compiler_instance: build_system.compiler.installed_instance.CompilerInstance,
-                 targets: list[build_system.build_target.name.TargetBuildName] = None) -> None:
-        assert compiler_instance is not None
-
-        object.__setattr__(self, 'compiler_instance', compiler_instance)
-
-        if targets is None:
-            object.__setattr__(self, 'targets', [])
-        else:
-            object.__setattr__(self, 'targets', targets)
+    targets: list[build_system.build_target.build_target_cls.BuildTarget]
 
     def __iter__(self):
         return self.targets
