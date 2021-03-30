22a18a6680c44c3b4f8e9181cee5e67c29acda85
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 08:58:45 -0400

    Return TargetBuildType instead of str

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_name_generation.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_name_generation.py
@@ -9,22 +9,16 @@ def _assemble_target_build_types() -> list[build_system.build_target.build_type.
 
 
 def generate_target_build_dir_names(supported_installed_compilers: list[build_system.compiler.installed_instance.CompilerInstance] = None) \
-        -> dict[build_system.compiler.installed_instance.CompilerInstance, list[str]]:
+        -> dict[build_system.compiler.installed_instance.CompilerInstance, list[build_system.build_target.build_type.TargetBuildType]]:
     if supported_installed_compilers is None:
         host_compilers = build_system.compiler.supported_installed_instances.fetch_all()
     else:
         host_compilers = supported_installed_compilers
 
+    build_dir_names_by_compiler_instance: dict[build_system.compiler.installed_instance.CompilerInstance, list[build_system.build_target.build_type.TargetBuildType]] = {}
     target_build_types = _assemble_target_build_types()
 
-    build_dir_names_by_compiler_instance: dict[build_system.compiler.installed_instance.CompilerInstance, list[str]] = {}
     for compiler_instance in host_compilers:
-        target_build_type_for_compiler_instance: list[str] = []
-
-        for target_build_type in target_build_types:
-            target_name = build_system.build_target.name.TargetBuildName(compiler_instance=compiler_instance, target_build_type=target_build_type)
-            target_build_type_for_compiler_instance.append(str(target_name))
-
-        build_dir_names_by_compiler_instance[compiler_instance] = target_build_type_for_compiler_instance
+        build_dir_names_by_compiler_instance[compiler_instance] = target_build_types
 
     return build_dir_names_by_compiler_instance
