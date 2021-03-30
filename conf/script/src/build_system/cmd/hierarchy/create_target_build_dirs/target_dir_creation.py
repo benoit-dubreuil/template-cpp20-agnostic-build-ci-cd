82508c640d8dbb25621bea777cd089d931a26b21
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 10:17:13 -0400

    Strange : old rename did not persist

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_creation.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_creation.py
@@ -9,9 +9,9 @@ def create_all_compiler_instances_target_build_dirs(build_dir: Path,
                                                     all_compiler_instances_targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets]) -> None:
     for compiler_instance_targets in all_compiler_instances_targets:
         for build_target in compiler_instance_targets:
-            create_compiler_instance_target_build_dir(build_dir=build_dir, compiler_instance_target=build_target)
+            _create_compiler_instance_target_build_dir(build_dir=build_dir, compiler_instance_target=build_target)
 
 
-def create_compiler_instance_target_build_dir(build_dir: Path, compiler_instance_target: build_system.build_target.build_target_cls.BuildTarget) -> None:
+def _create_compiler_instance_target_build_dir(build_dir: Path, compiler_instance_target: build_system.build_target.build_target_cls.BuildTarget) -> None:
     compiler_instance_target.compute_target_build_dir(project_build_dir=build_dir)
     compiler_instance_target.dir.mkdir(mode=build_system.cmd.hierarchy.consts.BUILD_DIR_PERMISSIONS, exist_ok=True)
