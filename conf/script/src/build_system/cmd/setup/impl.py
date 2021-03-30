24b2de59b1dd0b700bdb05ed3aeddab0757e64ce
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 10:59:34 -0400

    Extracted setup_host_compiler_target_build_dir, fixed auto system exit from meson

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -1,31 +1,20 @@
 from pathlib import Path
 from typing import Final, Optional
 
-import mesonbuild.mesonmain
-
-import build_system.build_target.build_target_cls
 import build_system.build_target.compiler_instance_targets
 import build_system.compiler.installed_instance
-import build_system.compiler.installed_instance.msvc
 import build_system.compiler.supported_installed_instances
 from build_system.cmd.setup.create_targets_dirs import create_all_host_compilers_targets_build_dirs
-from build_system.cmd.setup.meson_utils import fetch_meson_launcher
+from build_system.cmd.setup.meson_utils import setup_host_compiler_target_build_dir
 
 
 def setup_build_system(root_dir: Optional[Path] = None):
-    # TODO : Foreach compiler, foreach target build dir...
     host_compilers = build_system.compiler.supported_installed_instances.fetch_all()
     all_host_compilers_targets = create_all_host_compilers_targets_build_dirs(root_dir=root_dir,
                                                                               supported_installed_compilers=host_compilers)
 
     for host_compiler_targets in all_host_compilers_targets:
         _setup_host_compiler_targets(host_compiler_targets=host_compiler_targets)
-
-    # TODO : WIP
-    meson_launcher: str = fetch_meson_launcher()
-    meson_cli_args: list[str] = ['-h']
-
-    mesonbuild.mesonmain.run(meson_cli_args, meson_launcher)
 
 
 def _setup_host_compiler_targets(host_compiler_targets: build_system.build_target.compiler_instance_targets.CompilerInstanceTargets):
@@ -35,13 +24,7 @@ def _setup_host_compiler_targets(host_compiler_targets: build_system.build_targe
         host_compiler.setup_env_vars()
 
     for target in host_compiler_targets.targets:
-        _setup_host_compiler_target_build_dir(host_compiler=host_compiler, target_build_dir=target)
+        setup_host_compiler_target_build_dir(host_compiler=host_compiler, target_build_dir=target)
 
     if host_compiler.requires_env_vars_setup():
         host_compiler.teardown_env_vars()
-
-
-def _setup_host_compiler_target_build_dir(host_compiler: build_system.compiler.installed_instance.CompilerInstance,
-                                          target_build_dir: build_system.build_target.build_target_cls.BuildTarget):
-    # TODO
-    ...
--- conf/script/src/build_system/cmd/setup/meson_utils.py
+++ conf/script/src/build_system/cmd/setup/meson_utils.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+import mesonbuild.mesonmain
+
+import build_system.build_target.build_target_cls
+import build_system.compiler.installed_instance
+
 
 def fetch_meson_launcher() -> str:
     current_package_path = _fetch_current_package_path()
@@ -12,3 +17,15 @@ def _fetch_current_package_path() -> Path:
     current_package_path = current_package_path.absolute()
 
     return current_package_path
+
+
+def setup_host_compiler_target_build_dir(host_compiler: build_system.compiler.installed_instance.CompilerInstance,
+                                         target_build_dir: build_system.build_target.build_target_cls.BuildTarget):
+    # TODO : WIP
+    meson_launcher: str = fetch_meson_launcher()
+    meson_cli_args: list[str] = ['-h']
+
+    try:
+        mesonbuild.mesonmain.run(meson_cli_args, meson_launcher)
+    except SystemExit:
+        pass
