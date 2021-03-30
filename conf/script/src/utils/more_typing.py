216d3158d5a04d00cf3ad8950237569f3b67e281
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 09:21:53 -0400

    Reformat

--- conf/script/src/utils/more_typing.py
+++ conf/script/src/utils/more_typing.py
@@ -1,6 +1,6 @@
+import os
 import pathlib
 import typing
-import os
 
 PathLike = typing.TypeVar("PathLike", pathlib.Path, os.PathLike, str, bytes)
 AnyPath = typing.TypeVar("AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
