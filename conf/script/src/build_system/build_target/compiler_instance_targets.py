2015e9edad02b68c3227886ec44cd5413e095dd2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 09:09:44 -0400

    Overrode init

--- conf/script/src/build_system/build_target/compiler_instance_targets.py
+++ conf/script/src/build_system/build_target/compiler_instance_targets.py
@@ -1,10 +1,22 @@
 import dataclasses
 
-import build_system.compiler.installed_instance
 import build_system.build_target.name
+import build_system.compiler.installed_instance
 
 
 @dataclasses.dataclass(order=True, frozen=True)
 class CompilerInstanceTargets:
     compiler_instance: build_system.compiler.installed_instance.CompilerInstance
     targets: list[build_system.build_target.name.TargetBuildName]
+
+    def __init__(self,
+                 compiler_instance: build_system.compiler.installed_instance.CompilerInstance,
+                 targets: list[build_system.build_target.name.TargetBuildName] = None) -> None:
+        assert compiler_instance is not None
+
+        object.__setattr__(self, 'compiler_instance', compiler_instance)
+
+        if targets is None:
+            object.__setattr__(self, 'targets', [])
+        else:
+            object.__setattr__(self, 'targets', targets)
