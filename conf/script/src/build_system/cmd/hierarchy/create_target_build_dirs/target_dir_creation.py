28dcacb1cefc2b47ea1076d6eea8073a7ca78cb0
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-29 20:09:59 -0400

    Converted create_all_target_build_dirs func to return dict

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_creation.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_creation.py
@@ -13,10 +13,15 @@ def create_target_build_dir(build_dir: Path, target_build_dir_name: str) -> Path
 
 def create_all_target_build_dirs(build_dir: Path, all_target_build_dir_names: dict[(build_system.compiler.installed_instance.CompilerInstance, list[str])]) \
         -> dict[(build_system.compiler.installed_instance.CompilerInstance, list[Path])]:
-    all_target_build_dirs: list[(Path, build_system.compiler.installed_instance.CompilerInstance)] = []
+    all_target_build_dirs: dict[(build_system.compiler.installed_instance.CompilerInstance, list[Path])] = {}
 
-    for compiler_instance, target_build_dir_name in all_target_build_dir_names:
-        target_build_dir = create_target_build_dir(build_dir, target_build_dir_name)
-        all_target_build_dirs.append((target_build_dir, compiler_instance))
+    for compiler_instance in all_target_build_dir_names:
+        target_build_dirs_for_compiler_instance = []
+
+        for target_build_dir_name in all_target_build_dir_names[compiler_instance]:
+            target_build_dir = create_target_build_dir(build_dir, target_build_dir_name)
+            target_build_dirs_for_compiler_instance.append(target_build_dir)
+
+        all_target_build_dirs[compiler_instance] = target_build_dirs_for_compiler_instance
 
     return all_target_build_dirs
