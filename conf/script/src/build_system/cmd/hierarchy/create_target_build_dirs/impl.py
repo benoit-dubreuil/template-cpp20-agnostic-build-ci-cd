9690593974243d6136f09835804640c4cd6b44ca
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 09:50:37 -0400

    Reorder

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/impl.py
@@ -7,9 +7,27 @@ import utils.error.cls_def
 import utils.more_path
 
 
+def create_target_build_dirs(build_dir: Optional[Path] = None,
+                             supported_installed_compilers: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None) \
+        -> dict[build_system.compiler.installed_instance.CompilerInstance, list[Path]]:
+    build_dir = build_system.cmd.hierarchy.assure_arg_integrity.assure_build_dir_exists(build_dir=build_dir)
+    _assure_build_dir_is_empty(build_dir)
+
+    return _create_all_target_build_dirs(build_dir, supported_installed_compilers=supported_installed_compilers)
+
+
 def _assure_build_dir_is_empty(build_dir):
     if not utils.more_path.is_dir_empty(build_dir):
         raise utils.error.cls_def.BuildDirNotEmptyError()
+
+
+def _create_all_target_build_dirs(build_dir: Path,
+                                  supported_installed_compilers: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None) \
+        -> dict[build_system.compiler.installed_instance.CompilerInstance, list[Path]]:
+    import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation
+
+    target_build_dir_names = _generate_target_build_dir_names(supported_installed_compilers=supported_installed_compilers)
+    return build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation.create_all_target_build_dirs(build_dir, target_build_dir_names)
 
 
 def _generate_target_build_dir_names(supported_installed_compilers: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None) \
@@ -23,21 +41,3 @@ def _generate_target_build_dir_names(supported_installed_compilers: Optional[lis
         raise utils.error.cls_def.NoSupportedCompilersAvailableError()
 
     return target_build_dir_names
-
-
-def _create_all_target_build_dirs(build_dir: Path,
-                                  supported_installed_compilers: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None) \
-        -> dict[build_system.compiler.installed_instance.CompilerInstance, list[Path]]:
-    import build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation
-
-    target_build_dir_names = _generate_target_build_dir_names(supported_installed_compilers=supported_installed_compilers)
-    return build_system.cmd.hierarchy.create_target_build_dirs.target_dir_creation.create_all_target_build_dirs(build_dir, target_build_dir_names)
-
-
-def create_target_build_dirs(build_dir: Optional[Path] = None,
-                             supported_installed_compilers: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None) \
-        -> dict[build_system.compiler.installed_instance.CompilerInstance, list[Path]]:
-    build_dir = build_system.cmd.hierarchy.assure_arg_integrity.assure_build_dir_exists(build_dir=build_dir)
-    _assure_build_dir_is_empty(build_dir)
-
-    return _create_all_target_build_dirs(build_dir, supported_installed_compilers=supported_installed_compilers)
