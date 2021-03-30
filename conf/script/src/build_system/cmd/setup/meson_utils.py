28802cec1803a69cc93ff24f50976d9608ecf9d9
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 11:00:17 -0400

    Reorder and made func private

--- conf/script/src/build_system/cmd/setup/meson_utils.py
+++ conf/script/src/build_system/cmd/setup/meson_utils.py
@@ -6,7 +6,19 @@ import build_system.build_target.build_target_cls
 import build_system.compiler.installed_instance
 
 
-def fetch_meson_launcher() -> str:
+def setup_host_compiler_target_build_dir(host_compiler: build_system.compiler.installed_instance.CompilerInstance,
+                                         target_build_dir: build_system.build_target.build_target_cls.BuildTarget):
+    # TODO : WIP
+    meson_launcher: str = _fetch_meson_launcher()
+    meson_cli_args: list[str] = ['-h']
+
+    try:
+        mesonbuild.mesonmain.run(meson_cli_args, meson_launcher)
+    except SystemExit:
+        pass
+
+
+def _fetch_meson_launcher() -> str:
     current_package_path = _fetch_current_package_path()
     return str(current_package_path)
 
@@ -17,15 +29,3 @@ def _fetch_current_package_path() -> Path:
     current_package_path = current_package_path.absolute()
 
     return current_package_path
-
-
-def setup_host_compiler_target_build_dir(host_compiler: build_system.compiler.installed_instance.CompilerInstance,
-                                         target_build_dir: build_system.build_target.build_target_cls.BuildTarget):
-    # TODO : WIP
-    meson_launcher: str = fetch_meson_launcher()
-    meson_cli_args: list[str] = ['-h']
-
-    try:
-        mesonbuild.mesonmain.run(meson_cli_args, meson_launcher)
-    except SystemExit:
-        pass
