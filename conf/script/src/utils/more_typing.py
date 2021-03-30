6d741ad6f0acab9d96f9faff8c5266e4ccbc8bbf
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 09:21:09 -0400

    Added PathLike type

--- conf/script/src/utils/more_typing.py
+++ conf/script/src/utils/more_typing.py
@@ -2,4 +2,5 @@ import pathlib
 import typing
 import os
 
+PathLike = typing.TypeVar("PathLike", pathlib.Path, os.PathLike, str, bytes)
 AnyPath = typing.TypeVar("AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
