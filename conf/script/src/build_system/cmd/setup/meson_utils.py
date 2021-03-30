304631cd630cb12530f94b0934d550a9a1259de7
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-30 16:26:01 -0400

    Added cli_mode to setup_host_compiler_target_build_dir func

--- conf/script/src/build_system/cmd/setup/meson_utils.py
+++ conf/script/src/build_system/cmd/setup/meson_utils.py
@@ -1,16 +1,19 @@
+import contextlib
 from pathlib import Path
 from typing import Final
 
-import colorama
 import mesonbuild.mesonmain
 
 import build_system.build_target.build_target_cls
 import build_system.compiler.installed_instance
+import utils.cli.hidden_prints
+from build_system.cmd.setup.cli_print_target_info import print_target_info
 
 
 def setup_host_compiler_target_build_dir(root_dir: Path,
                                          host_compiler: build_system.compiler.installed_instance.CompilerInstance,
-                                         target_build_dir: build_system.build_target.build_target_cls.BuildTarget):
+                                         target_build_dir: build_system.build_target.build_target_cls.BuildTarget,
+                                         cli_mode: bool = False):
     # TODO : WIP
     meson_launcher: str = _fetch_meson_launcher()
 
@@ -27,10 +30,12 @@ def setup_host_compiler_target_build_dir(root_dir: Path,
                                  meson_cli_arg_build_dir,
                                  meson_cli_arg_source_dir]
 
-    _print_target_info(host_compiler=host_compiler, target_build_dir=target_build_dir)
+    if cli_mode:
+        print_target_info(host_compiler=host_compiler, target_build_dir=target_build_dir)
 
     try:
-        mesonbuild.mesonmain.run(meson_cli_args, meson_launcher)
+        with contextlib.nullcontext() if cli_mode else utils.cli.hidden_prints.HiddenPrints():
+            mesonbuild.mesonmain.run(meson_cli_args, meson_launcher)
     except SystemExit:
         pass
 
@@ -38,81 +43,6 @@ def setup_host_compiler_target_build_dir(root_dir: Path,
 def _fetch_meson_launcher() -> str:
     current_package_path = _fetch_current_package_path()
     return str(current_package_path)
-
-
-def _print_target_info(host_compiler: build_system.compiler.installed_instance.CompilerInstance,
-                       target_build_dir: build_system.build_target.build_target_cls.BuildTarget) -> None:
-    def print_indented_label_and_info(pre_label_indent: str = str(),
-                                      post_label_indent: str = str(),
-                                      label: str = str(),
-                                      info: str = str(),
-                                      color_label: bool = True,
-                                      color_info: bool = False) -> None:
-        if color_label:
-            label_colored = colorama.Fore.CYAN + label + colorama.Style.RESET_ALL + ':'
-        else:
-            label_colored = label
-
-        if color_info:
-            info_colored = colorama.Fore.LIGHTBLACK_EX + info + colorama.Style.RESET_ALL
-        else:
-            info_colored = info
-
-        label_formatted = pre_label_indent + label_colored + post_label_indent
-        line = label_formatted + info_colored
-
-        print(line)
-
-    def print_post_header_labels_and_info(pre_label_indent: str = str(),
-                                          post_label_indent: str = str()):
-        label_compiler_family_label = r'Compiler family'
-        label_compiler_family_info = host_compiler.compiler_family.value
-
-        label_compiler_version_label = r'Compiler version'
-        label_compiler_version_info = str(host_compiler.version)
-
-        label_compiler_installation_path_label = r'Compiler installation path'
-        label_compiler_installation_path_info = str(host_compiler.installation_dir)
-
-        label_build_type_label = r'Build type'
-        label_build_type_info = target_build_dir.get_build_type().value
-
-        print_indented_label_and_info(pre_label_indent=pre_label_indent,
-                                      post_label_indent=post_label_indent,
-                                      label=label_compiler_family_label,
-                                      info=label_compiler_family_info)
-
-        print_indented_label_and_info(pre_label_indent=pre_label_indent,
-                                      post_label_indent=post_label_indent,
-                                      label=label_compiler_version_label,
-                                      info=label_compiler_version_info)
-
-        print_indented_label_and_info(pre_label_indent=pre_label_indent,
-                                      post_label_indent=post_label_indent,
-                                      label=label_compiler_installation_path_label,
-                                      info=label_compiler_installation_path_info,
-                                      color_info=True)
-
-        print_indented_label_and_info(pre_label_indent=pre_label_indent,
-                                      post_label_indent=post_label_indent,
-                                      label=label_build_type_label,
-                                      info=label_build_type_info)
-
-    white_space: Final[str] = ' '
-
-    header_label = r'Target'
-    header_colored_label = colorama.Fore.LIGHTCYAN_EX + header_label + colorama.Style.RESET_ALL
-    post_header_indent = white_space * 6
-    header_total_indent = (white_space * len(header_label)) + post_header_indent
-
-    post_label_indent = white_space * 3
-
-    print_indented_label_and_info(post_label_indent=post_header_indent,
-                                  label=header_colored_label,
-                                  color_label=False)
-
-    print_post_header_labels_and_info(pre_label_indent=header_total_indent,
-                                      post_label_indent=post_label_indent)
 
 
 def _fetch_current_package_path() -> Path:
