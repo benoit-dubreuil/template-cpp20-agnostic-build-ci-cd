d559c9659064fd1a6f36387b0eac4247d477cb7a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-31 10:12:50 -0400

    Added COMPILER_REQS_NOT_FOUND error status

--- conf/script/src/utils/error/status.py
+++ conf/script/src/utils/error/status.py
@@ -17,6 +17,7 @@ class ErrorStatus(enum.IntEnum):
     BUILD_DIR_NOT_EMPTY = enum.auto()
     CONF_DIR_NOT_FOUND = enum.auto()
     CONF_BUILD_SYSTEM_DIR_NOT_FOUND = enum.auto()
+    COMPILER_REQS_NOT_FOUND = enum.auto()
     COMPILER_NOT_FOUND = enum.auto()
     NO_SUPPORTED_COMPILERS_AVAILABLE = enum.auto()
     MSVC_COMPILER_VCVARS_DIR_NOT_FOUND = enum.auto()
