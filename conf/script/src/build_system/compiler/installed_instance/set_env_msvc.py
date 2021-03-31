3387197e8cd3dc47153c6188894b6211d453a199
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-31 10:44:56 -0400

    Added __enter__ and __exit__ funcs

--- conf/script/src/build_system/compiler/installed_instance/set_env_msvc.py
+++ conf/script/src/build_system/compiler/installed_instance/set_env_msvc.py
@@ -9,3 +9,17 @@ class EnvMSVC(contextlib.AbstractContextManager):
     def __init__(self, compiler: build_system.compiler.installed_instance.msvc.MSVCCompilerInstance) -> None:
         super().__init__()
         self.compiler = compiler
+
+    def __enter__(self):
+        self.__setup_env_vars()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.__teardown_env_vars()
+        return False
+
+    def __setup_env_vars(self) -> None:
+        ...
+
+    def __teardown_env_vars(self) -> None:
+        ...
