09bf071fff6524f3aaa8451d9e2695e3dfa64fcd
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-03-31 10:43:27 -0400

    Added init

--- conf/script/src/build_system/compiler/installed_instance/set_env_msvc.py
+++ conf/script/src/build_system/compiler/installed_instance/set_env_msvc.py
@@ -5,4 +5,7 @@ class EnvMSVC(contextlib.AbstractContextManager):
     import build_system.compiler.installed_instance.msvc
 
     compiler: build_system.compiler.installed_instance.msvc.MSVCCompilerInstance
-    ...
+
+    def __init__(self, compiler: build_system.compiler.installed_instance.msvc.MSVCCompilerInstance) -> None:
+        super().__init__()
+        self.compiler = compiler
