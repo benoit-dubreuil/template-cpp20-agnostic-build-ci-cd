be951ae692b7cc6ca7500a250a4a9f339fb7cf9b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-03 11:40:23 -0400

    Import create_targets_build_dirs

--- conf/script/src/build_system/cmd/setup/setup_steps/__init__.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/__init__.py
@@ -1,2 +1,3 @@
+from build_system.cmd.setup.setup_steps.step_create_targets_build_dirs import create_targets_build_dirs
 from build_system.cmd.setup.setup_steps.step_fetch_host_compilers import fetch_host_compilers
 from build_system.cmd.setup.setup_steps.step_get_root_dir import get_verified_root_dir
