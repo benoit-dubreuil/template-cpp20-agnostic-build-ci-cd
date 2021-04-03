5efa6bb01fe030bd75489cbc031e2fc55eb59f4b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-03 11:22:15 -0400

    Refactor

--- conf/script/src/build_system/cmd/hierarchy/assure_arg_integrity.py
+++ conf/script/src/build_system/cmd/hierarchy/assure_arg_integrity.py
@@ -5,10 +5,10 @@ import utils.error.cls_def
 
 
 def get_verified_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
-    import build_system.cmd.hierarchy.find_root_dir
+    from build_system.cmd.hierarchy.find_root_dir import find_root_dir
 
     if unverified_root_dir is None:
-        unverified_root_dir = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
+        unverified_root_dir = find_root_dir()
     else:
         if not unverified_root_dir.exists():
             raise utils.error.cls_def.RootDirNotFoundError()
@@ -17,10 +17,10 @@ def get_verified_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
 
 
 def get_verified_build_dir(unverified_build_dir: Optional[Path] = None) -> Path:
-    import build_system.cmd.hierarchy.create_build_dir
+    from build_system.cmd.hierarchy.create_build_dir import create_build_dir
 
     if unverified_build_dir is None:
-        unverified_build_dir = build_system.cmd.hierarchy.create_build_dir.create_build_dir()
+        unverified_build_dir = create_build_dir()
     else:
         if not unverified_build_dir.exists():
             raise utils.error.cls_def.BuildDirNotFoundError()
