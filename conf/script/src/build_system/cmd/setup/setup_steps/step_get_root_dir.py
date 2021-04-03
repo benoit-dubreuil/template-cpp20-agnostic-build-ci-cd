b4dc51dc031171d13e571f96344c5fc95b984d6d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-03 11:32:17 -0400

    Added step_fetch_host_compilers

--- conf/script/src/build_system/cmd/setup/setup_steps/step_get_root_dir.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_get_root_dir.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import Optional
 
 
-def get_verified_root_dir(unverified_root_dir: Optional[Path] = None):
+def get_verified_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
     import build_system.cmd.hierarchy.assure_arg_integrity
 
     verified_root_dir = build_system.cmd.hierarchy.assure_arg_integrity.get_verified_root_dir(unverified_root_dir=unverified_root_dir)
