f4e51f9ac1397a88be3febef518570eb9533d81f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-04 10:20:33 -0400

    Refactor

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -13,10 +13,7 @@ def setup_build_system(root_dir: Optional[Path] = None, cli_mode: bool = False):
     root_dir = setup_steps.get_verified_root_dir(unverified_root_dir=root_dir)
     host_compilers = setup_steps.fetch_host_compilers()
     targets = setup_steps.create_targets_build_dirs(root_dir=root_dir, host_compilers=host_compilers)
-
-    _setup_targets(root_dir=root_dir,
-                   host_compilers_targets=targets,
-                   cli_mode=cli_mode)
+    _setup_targets(root_dir=root_dir, host_compilers_targets=targets, cli_mode=cli_mode)
 
 
 def _setup_targets(root_dir: Path,
