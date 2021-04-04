695c1946cee037e6af019288f415ba2b8769c850
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-04 10:14:45 -0400

    Renamed func

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -23,14 +23,14 @@ def _setup_targets(root_dir: Path,
                    all_host_compilers_targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets],
                    cli_mode: bool):
     for host_compiler_targets in all_host_compilers_targets:
-        _setup_host_compiler_all_targets(root_dir=root_dir,
-                                         host_compiler_targets=host_compiler_targets,
-                                         cli_mode=cli_mode)
+        _setup_host_compiler_targets(root_dir=root_dir,
+                                     host_compiler_targets=host_compiler_targets,
+                                     cli_mode=cli_mode)
 
 
-def _setup_host_compiler_all_targets(root_dir: Path,
-                                     host_compiler_targets: build_system.build_target.compiler_instance_targets.CompilerInstanceTargets,
-                                     cli_mode: bool):
+def _setup_host_compiler_targets(root_dir: Path,
+                                 host_compiler_targets: build_system.build_target.compiler_instance_targets.CompilerInstanceTargets,
+                                 cli_mode: bool):
     from build_system.cmd.setup.meson_utils import setup_host_compiler_target as _setup_host_compiler_target
 
     host_compiler: Final[build_system.compiler.installed_instance.CompilerInstance] = host_compiler_targets.compiler_instance
