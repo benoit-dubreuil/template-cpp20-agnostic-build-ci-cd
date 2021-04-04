7a98ad05c747d287d96e78b4ac4f480f444fa6f3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-04 10:32:34 -0400

    Extracted create_targets_dirs

--- conf/script/src/build_system/cmd/setup/setup_steps/step_create_targets_build_dirs.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_create_targets_build_dirs.py
@@ -7,9 +7,21 @@ import build_system.compiler.installed_instance
 def create_targets_build_dirs(root_dir: Path,
                               host_compilers: list[build_system.compiler.installed_instance.CompilerInstance]) \
         -> list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets]:
-    from build_system.cmd.setup.create_targets_dirs import create_host_compilers_targets_build_dirs
+    from build_system.cmd.hierarchy.create_target_build_dirs import create_target_build_dirs
 
-    host_compilers_targets = create_host_compilers_targets_build_dirs(root_dir=root_dir,
-                                                                      supported_installed_compilers=host_compilers)
+    build_dir = _recreate_build_dir(root_dir)
+    target_build_dirs = create_target_build_dirs(build_dir=build_dir, supported_installed_compilers=host_compilers)
 
-    return host_compilers_targets
+    return target_build_dirs
+
+
+def _recreate_build_dir(root_dir: Path) -> Path:
+    from build_system.cmd.hierarchy.clean_build_dir import clean_build_dir
+    from build_system.cmd.hierarchy.create_build_dir import create_build_dir
+    from build_system.cmd.hierarchy.find_build_dir import get_build_dir_path_relative_to_root_dir
+
+    build_dir = get_build_dir_path_relative_to_root_dir(root_dir=root_dir)
+    clean_build_dir(build_dir=build_dir, ignore_errors=True)
+    build_dir = create_build_dir(root_dir=root_dir)
+
+    return build_dir
