47a3cf772d6aa60d9308c2e5e7f1e519e0051bf4
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-04 10:24:44 -0400

    Extracted the setup targets step

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -1,10 +1,5 @@
 from pathlib import Path
-from typing import Final, Optional
-
-import build_system.build_target.build_target_cls
-import build_system.build_target.compiler_instance_targets
-import build_system.compiler.installed_instance
-import build_system.compiler.supported_installed_instances
+from typing import Optional
 
 
 def setup_build_system(root_dir: Optional[Path] = None, cli_mode: bool = False):
@@ -13,29 +8,4 @@ def setup_build_system(root_dir: Optional[Path] = None, cli_mode: bool = False):
     root_dir = setup_steps.get_verified_root_dir(unverified_root_dir=root_dir)
     host_compilers = setup_steps.fetch_host_compilers()
     targets = setup_steps.create_targets_build_dirs(root_dir=root_dir, host_compilers=host_compilers)
-    _setup_targets(root_dir=root_dir, host_compilers_targets=targets, cli_mode=cli_mode)
-
-
-def _setup_targets(root_dir: Path,
-                   host_compilers_targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets],
-                   cli_mode: bool):
-    for host_compiler_targets in host_compilers_targets:
-        _setup_host_compiler_targets(root_dir=root_dir,
-                                     host_compiler_targets=host_compiler_targets,
-                                     cli_mode=cli_mode)
-
-
-def _setup_host_compiler_targets(root_dir: Path,
-                                 host_compiler_targets: build_system.build_target.compiler_instance_targets.CompilerInstanceTargets,
-                                 cli_mode: bool):
-    from build_system.cmd.setup.meson_utils import setup_host_compiler_target as _setup_host_compiler_target
-
-    host_compiler: Final[build_system.compiler.installed_instance.CompilerInstance] = host_compiler_targets.compiler_instance
-
-    with host_compiler.create_env_vars_context_manager() as compiler_env_vars_manager:
-        for target in host_compiler_targets.targets:
-            _setup_host_compiler_target(root_dir=root_dir,
-                                        host_compiler=host_compiler,
-                                        compiler_env_vars_manager=compiler_env_vars_manager,
-                                        target_build_dir=target,
-                                        cli_mode=cli_mode)
+    setup_steps.setup_targets(root_dir=root_dir, host_compilers_targets=targets, cli_mode=cli_mode)
--- conf/script/src/build_system/cmd/setup/setup_steps/__init__.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/__init__.py
@@ -1,3 +1,4 @@
 from build_system.cmd.setup.setup_steps.step_create_targets_build_dirs import create_targets_build_dirs
 from build_system.cmd.setup.setup_steps.step_fetch_host_compilers import fetch_host_compilers
 from build_system.cmd.setup.setup_steps.step_get_root_dir import get_verified_root_dir
+from build_system.cmd.setup.setup_steps.step_setup_targets import setup_targets
