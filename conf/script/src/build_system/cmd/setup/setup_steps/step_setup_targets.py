8b8e48224a4941f0c6f073be05604b70865a1d32
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-04 10:33:58 -0400

    Renamed func

--- conf/script/src/build_system/cmd/setup/setup_steps/step_setup_targets.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_setup_targets.py
@@ -15,14 +15,14 @@ def setup_targets(root_dir: Path,
 def _setup_host_compiler_targets(root_dir: Path,
                                  host_compiler_targets: build_system.build_target.compiler_instance_targets.CompilerInstanceTargets,
                                  cli_mode: bool):
-    from build_system.cmd.setup.meson_utils import setup_host_compiler_target
+    from build_system.cmd.setup.meson_utils import setup_target
 
     host_compiler: Final[build_system.compiler.installed_instance.CompilerInstance] = host_compiler_targets.compiler_instance
 
     with host_compiler.create_env_vars_context_manager() as compiler_env_vars_manager:
         for target in host_compiler_targets.targets:
-            setup_host_compiler_target(root_dir=root_dir,
-                                       host_compiler=host_compiler,
-                                       compiler_env_vars_manager=compiler_env_vars_manager,
-                                       target_build_dir=target,
-                                       cli_mode=cli_mode)
+            setup_target(root_dir=root_dir,
+                         host_compiler=host_compiler,
+                         compiler_env_vars_manager=compiler_env_vars_manager,
+                         target_build_dir=target,
+                         cli_mode=cli_mode)
