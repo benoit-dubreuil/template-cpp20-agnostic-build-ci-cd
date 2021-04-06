ed9e93b3ef2342a7fca880eb7982864402ec1154
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-06 11:27:46 -0400

    Revert "Switched to from imports within the class"
    This reverts commit 6296f5494a99bcfe3b45c781238df9d91ba77f8f.

--- conf/script/src/build_system/compiler/installed_instance/gnu.py
+++ conf/script/src/build_system/compiler/installed_instance/gnu.py
@@ -2,6 +2,7 @@ import contextlib
 from dataclasses import dataclass
 from pathlib import Path
 
+import build_system.compiler.family
 import build_system.compiler.installed_instance.compiler_instance
 import utils.cmd_integrity
 import utils.error.cls_def
@@ -11,7 +12,6 @@ import utils.error.try_external_errors
 @dataclass(order=True, frozen=True)
 class GNUCompilerInstance(build_system.compiler.installed_instance.compiler_instance.CompilerInstance):
     from build_system.compiler.build_option.sanitizer import CompilerSanitizer
-    from build_system.compiler.family import CompilerFamily
 
     executable_file: Path
 
@@ -35,7 +35,7 @@ class GNUCompilerInstance(build_system.compiler.installed_instance.compiler_inst
         return build_system.compiler.installed_instance.set_env_default_compiler.EnvDefaultCompiler(compiler=self)
 
     @classmethod
-    def _find_installation_dir_by_compiler_family(cls, compiler_family: CompilerFamily) -> Path:
+    def _find_installation_dir_by_compiler_family(cls, compiler_family: build_system.compiler.family.CompilerFamily) -> Path:
         cls._assert_compiler_family(compiler_family=compiler_family)
 
         compiler_location, compiler_instance_exists = utils.cmd_integrity.get_cmd_path(cmd=compiler_family.value)
@@ -48,9 +48,9 @@ class GNUCompilerInstance(build_system.compiler.installed_instance.compiler_inst
         return compiler_installation_dir
 
     @staticmethod
-    def get_supported_compiler_families() -> list[CompilerFamily]:
-        return [CompilerFamily.GCC,
-                CompilerFamily.CLANG]
+    def get_supported_compiler_families() -> list[build_system.compiler.family.CompilerFamily]:
+        return [build_system.compiler.family.CompilerFamily.GCC,
+                build_system.compiler.family.CompilerFamily.CLANG]
 
     @staticmethod
     def get_supported_sanitizers() -> list[CompilerSanitizer]:
