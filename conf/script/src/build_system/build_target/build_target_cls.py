e890fdb91b98a132fb9a94624e0d3f6c836f07b5
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-06 10:39:50 -0400

    Extracted __str__

--- conf/script/src/build_system/build_target/build_target_cls.py
+++ conf/script/src/build_system/build_target/build_target_cls.py
@@ -2,7 +2,6 @@ import dataclasses
 import pathlib
 from typing import Final, Optional
 
-import build_system.build_target.name
 import build_system.build_target.build_type
 import build_system.compiler.host.architecture
 import build_system.compiler.installed_instance
@@ -20,9 +19,6 @@ class BuildTarget:
         self.name = build_name
         self.dir = None
 
-    def __str__(self) -> str:
-        return self.form_name()
-
     def form_name(self):
         return str(self.name)
 
@@ -35,3 +31,14 @@ class BuildTarget:
     @staticmethod
     def get_name_separator() -> str:
         return '-'
+
+    def __str__(self) -> str:
+        sep: Final[str] = self.get_name_separator()
+
+        os_family_name = self.compiler_instance.os_family.value
+        arch_bit_name = self.compiler_instance.arch.arch_to_bit_name()
+        compiler_name = self.compiler_instance.compiler_family.value
+        compiler_version_name = str(self.compiler_instance.version)
+        target_build_type_name = self.target_build_type.value
+
+        return os_family_name + sep + arch_bit_name + sep + compiler_name + sep + compiler_version_name + sep + target_build_type_name
