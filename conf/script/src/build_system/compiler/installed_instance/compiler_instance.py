ea9d898682156cf0133166b22363b5dd55bb61be
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-06 11:19:35 -0400

    Revert "Switched to from imports within the class"
    This reverts commit 1341c48eee92e31526e4b7fa55fe91000545ec30.

--- conf/script/src/build_system/compiler/installed_instance/compiler_instance.py
+++ conf/script/src/build_system/compiler/installed_instance/compiler_instance.py
@@ -4,6 +4,11 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional, Type, final
 
+import build_system.compiler.build_option.sanitizer
+import build_system.compiler.family
+import build_system.compiler.host.architecture
+import build_system.compiler.host.os_family
+import build_system.compiler.version
 import utils.error.cls_def
 import utils.error.format
 import utils.error.try_external_errors
@@ -11,23 +16,17 @@ import utils.error.try_external_errors
 
 @dataclass(order=True, frozen=True)
 class CompilerInstance(metaclass=abc.ABCMeta):
-    from build_system.compiler.build_option.sanitizer import CompilerSanitizer
-    from build_system.compiler.family import CompilerFamily
-    from build_system.compiler.host.architecture import Architecture
-    from build_system.compiler.host.os_family import OSFamily
-    from build_system.compiler.version import CompilerVersion
-
-    compiler_family: CompilerFamily
-    os_family: OSFamily
-    arch: Architecture
-    version: CompilerVersion
+    compiler_family: build_system.compiler.family.CompilerFamily
+    os_family: build_system.compiler.host.os_family.OSFamily
+    arch: build_system.compiler.host.architecture.Architecture
+    version: build_system.compiler.version.CompilerVersion
     installation_dir: Path
 
     def __init__(self,
-                 compiler_family: CompilerFamily,
-                 os_family: OSFamily,
-                 arch: Architecture,
-                 version: CompilerVersion,
+                 compiler_family: build_system.compiler.family.CompilerFamily,
+                 os_family: build_system.compiler.host.os_family.OSFamily,
+                 arch: build_system.compiler.host.architecture.Architecture,
+                 version: build_system.compiler.version.CompilerVersion,
                  installation_dir: Path
                  ):
         self._assert_compiler_family(compiler_family=compiler_family)
@@ -41,9 +40,9 @@ class CompilerInstance(metaclass=abc.ABCMeta):
     @classmethod
     @final
     def create_from_installed_compiler(cls,
-                                       compiler_family: CompilerFamily,
-                                       os_family: OSFamily,
-                                       arch: Architecture,
+                                       compiler_family: build_system.compiler.family.CompilerFamily,
+                                       os_family: build_system.compiler.host.os_family.OSFamily,
+                                       arch: build_system.compiler.host.architecture.Architecture,
                                        installation_dir: Optional[Path] = None) -> 'CompilerInstance':
         import build_system.cmd.compiler.host.get_info.version.fetch_by_criteria
 
@@ -60,7 +59,7 @@ class CompilerInstance(metaclass=abc.ABCMeta):
         return sub_cls_matching_compiler_family(compiler_family=compiler_family, os_family=os_family, arch=arch, version=version, installation_dir=installation_dir)
 
     @classmethod
-    def __search_first_sub_cls_matching_compiler_family(cls, compiler_family: CompilerFamily) -> Optional[Type['CompilerInstance']]:
+    def __search_first_sub_cls_matching_compiler_family(cls, compiler_family: build_system.compiler.family.CompilerFamily) -> Optional[Type['CompilerInstance']]:
         # noinspection PyUnresolvedReferences
         import build_system.compiler.installed_instance.import_all_concrete_instances
 
@@ -82,7 +81,7 @@ class CompilerInstance(metaclass=abc.ABCMeta):
         return sublcass_matching_compiler_family
 
     @classmethod
-    def __checked_search_first_sub_cls_matching_compiler_family(cls, compiler_family: CompilerFamily) -> Type['CompilerInstance']:
+    def __checked_search_first_sub_cls_matching_compiler_family(cls, compiler_family: build_system.compiler.family.CompilerFamily) -> Type['CompilerInstance']:
         sublcass_matching_compiler_family: Optional[Type[CompilerInstance]] = cls.__search_first_sub_cls_matching_compiler_family(compiler_family=compiler_family)
 
         if sublcass_matching_compiler_family is None:
@@ -93,12 +92,12 @@ class CompilerInstance(metaclass=abc.ABCMeta):
 
     @staticmethod
     @abc.abstractmethod
-    def get_supported_compiler_families() -> list[CompilerFamily]:
+    def get_supported_compiler_families() -> list[build_system.compiler.family.CompilerFamily]:
         raise NotImplementedError()
 
     @staticmethod
-    def get_supported_sanitizers() -> list[CompilerSanitizer]:
-        return [CompilerSanitizer.NONE]
+    def get_supported_sanitizers() -> list[build_system.compiler.build_option.sanitizer.CompilerSanitizer]:
+        return [build_system.compiler.build_option.sanitizer.CompilerSanitizer.NONE]
 
     def create_env_vars_context_manager(self) -> contextlib.AbstractContextManager:
         return contextlib.nullcontext()
@@ -115,10 +114,10 @@ class CompilerInstance(metaclass=abc.ABCMeta):
 
     @classmethod
     @abc.abstractmethod
-    def _find_installation_dir_by_compiler_family(cls, compiler_family: CompilerFamily) -> Path:
+    def _find_installation_dir_by_compiler_family(cls, compiler_family: build_system.compiler.family.CompilerFamily) -> Path:
         raise NotImplementedError()
 
     @classmethod
     @final
-    def _assert_compiler_family(cls, compiler_family: CompilerFamily):
+    def _assert_compiler_family(cls, compiler_family: build_system.compiler.family.CompilerFamily):
         assert compiler_family in cls.get_supported_compiler_families()
