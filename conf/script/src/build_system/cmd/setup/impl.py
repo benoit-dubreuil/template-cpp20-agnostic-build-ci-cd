6d3ab2331bdae3f361faa3ae9859654f5dabe1ad
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-07 14:59:11 -0400

    Renamed params

--- conf/script/src/build_system/cmd/setup/impl.py
+++ conf/script/src/build_system/cmd/setup/impl.py
@@ -7,5 +7,5 @@ def setup_build_system(root_dir: Optional[Path] = None, cli_mode: bool = False):
 
     root_dir = setup_steps.get_verified_root_dir(unverified_root_dir=root_dir)
     host_compilers = setup_steps.fetch_host_compilers()
-    targets = setup_steps.create_targets_build_dirs(root_dir=root_dir, host_compilers=host_compilers)
+    targets = setup_steps.create_targets_build_dirs(root_dir=root_dir, compiler_instances=host_compilers)
     setup_steps.setup_targets(root_dir=root_dir, targets=targets, cli_mode=cli_mode)
