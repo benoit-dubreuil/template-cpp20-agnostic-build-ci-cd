603e87966d38960018b21e2421428ec2b9370c30
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-07 10:39:17 -0400

    Renamed vars

--- conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_creation.py
+++ conf/script/src/build_system/cmd/hierarchy/create_target_build_dirs/target_dir_creation.py
@@ -7,11 +7,11 @@ import build_system.cmd.hierarchy.consts
 
 def create_all_compiler_instances_target_build_dirs(build_dir: Path,
                                                     targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets]) -> None:
-    for compiler_instance_targets in targets:
-        for build_target in compiler_instance_targets:
-            _create_compiler_instance_target_build_dir(build_dir=build_dir, compiler_instance_target=build_target)
+    for targets_of_compiler_instance in targets:
+        for build_target in targets_of_compiler_instance:
+            _create_compiler_instance_target_build_dir(build_dir=build_dir, target_of_compiler_instance=build_target)
 
 
-def _create_compiler_instance_target_build_dir(build_dir: Path, compiler_instance_target: build_system.build_target.build_target.BuildTarget) -> None:
-    compiler_instance_target.compute_target_build_dir(project_build_dir=build_dir)
-    compiler_instance_target.dir.mkdir(mode=build_system.cmd.hierarchy.consts.BUILD_DIR_PERMISSIONS, exist_ok=True)
+def _create_compiler_instance_target_build_dir(build_dir: Path, target_of_compiler_instance: build_system.build_target.build_target.BuildTarget) -> None:
+    target_of_compiler_instance.compute_target_build_dir(project_build_dir=build_dir)
+    target_of_compiler_instance.dir.mkdir(mode=build_system.cmd.hierarchy.consts.BUILD_DIR_PERMISSIONS, exist_ok=True)
