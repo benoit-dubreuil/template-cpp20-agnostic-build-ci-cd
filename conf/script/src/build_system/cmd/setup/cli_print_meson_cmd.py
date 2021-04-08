68765636803d2b85f111b8e78d25c164d94b8649
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:54:28 -0400

    Added the meson cmd to the args for printing

--- conf/script/src/build_system/cmd/setup/cli_print_meson_cmd.py
+++ conf/script/src/build_system/cmd/setup/cli_print_meson_cmd.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from build_system.cmd.hierarchy.consts import BUILD_SYSTEM_NAME
+
 
 def print_meson_main_file(meson_main_file: Path) -> None:
     from build_system.cmd.setup.cli_color import colorize_path
@@ -11,7 +13,8 @@ def print_meson_main_file(meson_main_file: Path) -> None:
 def print_meson_cmd(meson_cli_args: list[str]) -> None:
     from build_system.cmd.setup.cli_color import colorize_path
 
-    meson_cmd = ' '.join(meson_cli_args)
-    colored_meson_cmd = colorize_path(path_info=meson_cmd)
+    meson_cli_cmd_and_args = [BUILD_SYSTEM_NAME, *meson_cli_args]
+    meson_cli_full_cmd = ' '.join(meson_cli_cmd_and_args)
 
+    colored_meson_cmd = colorize_path(path_info=meson_cli_full_cmd)
     print(colored_meson_cmd)
