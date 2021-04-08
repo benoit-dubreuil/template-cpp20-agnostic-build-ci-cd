da1d9d040d5515d292e319285f2f4780128b66c3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:48:57 -0400

    Added print_meson_main_file func and integrated colorization

--- conf/script/src/build_system/cmd/setup/cli_print_meson_cmd.py
+++ conf/script/src/build_system/cmd/setup/cli_print_meson_cmd.py
@@ -1,8 +1,17 @@
-import colorama
+from pathlib import Path
+
+
+def print_meson_main_file(meson_main_file: Path) -> None:
+    from build_system.cmd.setup.cli_color import colorize_path
+
+    colorized_meson_main_file = colorize_path(path_info=meson_main_file)
+    print(colorized_meson_main_file)
 
 
 def print_meson_cmd(meson_cli_args: list[str]) -> None:
+    from build_system.cmd.setup.cli_color import colorize_path
+
     meson_cmd = ' '.join(meson_cli_args)
-    colored_meson_cmd = colorama.Fore.LIGHTBLACK_EX + meson_cmd + colorama.Style.RESET_ALL
+    colored_meson_cmd = colorize_path(path_info=meson_cmd)
 
     print(colored_meson_cmd)
