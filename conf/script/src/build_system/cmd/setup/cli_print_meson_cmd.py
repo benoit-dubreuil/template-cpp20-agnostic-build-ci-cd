b5776a568e027d67c41288703becf004bc39afd8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:58:55 -0400

    Print meson cmd label

--- conf/script/src/build_system/cmd/setup/cli_print_meson_cmd.py
+++ conf/script/src/build_system/cmd/setup/cli_print_meson_cmd.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Final
 
 from build_system.cmd.hierarchy.consts import BUILD_SYSTEM_NAME
 
@@ -11,10 +12,13 @@ def print_meson_main_file(meson_main_file: Path) -> None:
 
 
 def print_meson_cmd(meson_cli_args: list[str]) -> None:
-    from build_system.cmd.setup.cli_color import colorize_path
+    from build_system.cmd.setup.cli_color import colorize_path, colorize_label
+
+    meson_cmd_label: Final[str] = colorize_label(label=f'{BUILD_SYSTEM_NAME.capitalize()} cmd') + ': '
 
     meson_cli_cmd_and_args = [BUILD_SYSTEM_NAME, *meson_cli_args]
     meson_cli_full_cmd = ' '.join(meson_cli_cmd_and_args)
-
     colored_meson_cmd = colorize_path(path_info=meson_cli_full_cmd)
+
+    print(meson_cmd_label, end=str())
     print(colored_meson_cmd)
