eb0029b374d2d7ebf30e4018c1131e962c0df7f3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:42:25 -0400

    Fixed typing for colors

--- conf/script/src/build_system/cmd/setup/cli_color.py
+++ conf/script/src/build_system/cmd/setup/cli_color.py
@@ -3,9 +3,9 @@ import colorama
 from utils.more_typing import PathLike
 
 
-def colorize_label(label: str) -> None:
+def colorize_label(label: str) -> str:
     return colorama.Fore.CYAN + label + colorama.Style.RESET_ALL
 
 
-def colorize_path(path_info: PathLike) -> None:
-    return colorama.Fore.LIGHTBLACK_EX + path_info + colorama.Style.RESET_ALL
+def colorize_path(path_info: PathLike) -> str:
+    return colorama.Fore.LIGHTBLACK_EX + str(path_info) + colorama.Style.RESET_ALL
