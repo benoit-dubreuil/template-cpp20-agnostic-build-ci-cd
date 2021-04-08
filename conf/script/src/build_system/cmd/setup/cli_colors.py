a961ba89fc5b30e3c387b2c2d5e8aef95e0903e3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:39:14 -0400

    Added colorize_path func

--- conf/script/src/build_system/cmd/setup/cli_colors.py
+++ conf/script/src/build_system/cmd/setup/cli_colors.py
@@ -1,5 +1,11 @@
 import colorama
 
+from utils.more_typing import PathLike
+
 
 def colorize_label(label: str) -> None:
     return colorama.Fore.CYAN + label + colorama.Style.RESET_ALL
+
+
+def colorize_path(path_info: PathLike) -> None:
+    return colorama.Fore.LIGHTBLACK_EX + path_info + colorama.Style.RESET_ALL
