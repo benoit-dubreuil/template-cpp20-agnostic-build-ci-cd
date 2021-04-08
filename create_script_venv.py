91a83c08ae99f35af401ca613f93a7f8cab618db
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 10:57:50 -0400

    Added many constants

--- create_script_venv.py
+++ create_script_venv.py
@@ -4,12 +4,16 @@ import venv
 from pathlib import Path
 from typing import Final
 
-SCRIPT_DIR: Final[Path] = Path().absolute()
+ROOT_DIR: Final[Path] = Path().absolute()
+ROOT_DIR.resolve(strict=True)
+
+SCRIPT_DIR: Final[Path] = ROOT_DIR / Path('conf') / 'script'
 SCRIPT_DIR.resolve(strict=True)
 
-VENV_RELATIVE_DIR: Final[Path] = Path('conf') / 'script' / 'venv'
+REQS_FILE: Final[Path] = SCRIPT_DIR / 'requirements.txt'
+REQS_FILE.resolve(strict=True)
 
-VENV_DIR: Final[Path] = SCRIPT_DIR / VENV_RELATIVE_DIR
+VENV_DIR: Final[Path] = ROOT_DIR / 'venv'
 VENV_PIP_DIR: Final[Path] = VENV_DIR / 'Lib' / 'site-packages' / 'pip'
 
 venv.create(env_dir=VENV_DIR,
@@ -18,6 +22,7 @@ venv.create(env_dir=VENV_DIR,
             symlinks=False,  # See Windows warning https://docs.python.org/3/library/venv.html#creating-virtual-environments
             with_pip=True,
             upgrade_deps=True)
+
 
 def pip_install_reqs() -> None:
     ...
