e4b8d60ac66d0370cab222851b221743d74f8232
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 10:43:45 -0400

    Added constants

--- create_script_venv.py
+++ create_script_venv.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 import venv
+from pathlib import Path
+from typing import Final
+
+SCRIPT_DIR: Final[Path] = Path().absolute()
+SCRIPT_DIR.resolve(strict=True)
+
+VENV_RELATIVE_DIR: Final[Path] = Path('conf') / 'script' / 'venv'
+
+VENV_DIR: Final[Path] = SCRIPT_DIR / VENV_RELATIVE_DIR
 
 if __name__ == '__main__':
     print('WIP')
