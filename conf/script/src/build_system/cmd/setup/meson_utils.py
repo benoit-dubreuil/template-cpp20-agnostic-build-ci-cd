a5230ea07ca68d1813214b20433afab1225d1577
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:30:31 -0400

    Renamed func

--- conf/script/src/build_system/cmd/setup/meson_utils.py
+++ conf/script/src/build_system/cmd/setup/meson_utils.py
@@ -59,7 +59,7 @@ def _generate_meson_setup_cli_args(root_dir: Path,
 
 
 def _run_meson(cli_mode, meson_cli_args):
-    meson_launcher: str = _fetch_meson_launcher()
+    meson_launcher: str = _find_meson_launcher()
 
     try:
         with contextlib.nullcontext() if cli_mode else utils.cli.hidden_prints.HiddenPrints():
@@ -69,7 +69,7 @@ def _run_meson(cli_mode, meson_cli_args):
         pass
 
 
-def _fetch_meson_launcher() -> str:
+def _find_meson_launcher() -> str:
     venv_interpreter = Path(sys.executable)
     venv_scripts_dir = venv_interpreter.parent
 
