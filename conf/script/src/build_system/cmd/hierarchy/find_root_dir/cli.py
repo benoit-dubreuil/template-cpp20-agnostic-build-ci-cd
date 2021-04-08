2a3e7e0fbc3f3dc3222ec479e31d0e932954386d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:16:41 -0400

    Replaced local const

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
@@ -9,7 +9,7 @@ import utils.cli.try_cmd
 def find_root_dir():
     arg_parser = argparse.ArgumentParser(
         description="Finds the project's root folder, where the "
-                    f"'{colorama.Fore.LIGHTBLACK_EX}{build_system.cmd.hierarchy.find_root_dir.BUILD_SYSTEM_CONF_FILENAME}{colorama.Style.RESET_ALL}' folder is. "
+                    f"'{colorama.Fore.LIGHTBLACK_EX}{build_system.cmd.hierarchy.find_root_dir.BUILD_SYSTEM_CONF_FILE_NAME}{colorama.Style.RESET_ALL}' folder is. "
                     'It searches recursively parent folders upwards.')
 
     arg_parser.parse_args()
