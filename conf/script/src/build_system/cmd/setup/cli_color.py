d07db54d137f2bfcd7737e0ad1604f5773afd2b4
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:44:34 -0400

    Added colorize_header

--- conf/script/src/build_system/cmd/setup/cli_color.py
+++ conf/script/src/build_system/cmd/setup/cli_color.py
@@ -3,6 +3,10 @@ import colorama
 from utils.more_typing import PathLike
 
 
+def colorize_header(header: str) -> str:
+    return colorama.Fore.LIGHTCYAN_EX + header + colorama.Style.RESET_ALL
+
+
 def colorize_label(label: str) -> str:
     return colorama.Fore.CYAN + label + colorama.Style.RESET_ALL
 
