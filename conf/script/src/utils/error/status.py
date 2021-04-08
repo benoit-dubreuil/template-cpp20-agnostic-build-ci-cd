6b00bb45e828d093cf6141fac2a211079b81cf34
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-08 16:24:47 -0400

    Reformat

--- conf/script/src/utils/error/status.py
+++ conf/script/src/utils/error/status.py
@@ -8,20 +8,27 @@ import utils.error.meta
 class ErrorStatus(enum.IntEnum):
     SUCCESS = 0
     UNSUPPORTED = 1
+    
     ARG_PARSER = 2
+
     UNKNOWN_PARSED_ARG = enum.auto()
     EMPTY_PARSED_ARG = enum.auto()
+
     ROOT_DIR_NOT_FOUND = enum.auto()
     BUILD_DIR_NOT_FOUND = enum.auto()
     BUILD_DIR_NOT_DIR = enum.auto()
     BUILD_DIR_NOT_EMPTY = enum.auto()
+
     CONF_DIR_NOT_FOUND = enum.auto()
     CONF_BUILD_SYSTEM_DIR_NOT_FOUND = enum.auto()
+
     MESON_MAIN_FILE_NOT_FOUND = enum.auto()
     MESON_MACHINE_FILES_DIR_NOT_FOUND = enum.auto()
+
     COMPILER_REQS_NOT_FOUND = enum.auto()
     COMPILER_NOT_FOUND = enum.auto()
     NO_SUPPORTED_COMPILERS_AVAILABLE = enum.auto()
+
     MSVC_COMPILER_VCVARS_DIR_NOT_FOUND = enum.auto()
     MSVC_COMPILER_VCVARS_BATCH_FILE_NOT_FOUND = enum.auto()
 
