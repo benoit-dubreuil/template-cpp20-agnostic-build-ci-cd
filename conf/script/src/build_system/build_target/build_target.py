973bb630d4fe2271e2b848f9345bdd4b95e0d5d1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-10 10:20:17 -0400

    Added script_dir member var

--- conf/script/src/build_system/build_target/build_target.py
+++ conf/script/src/build_system/build_target/build_target.py
@@ -14,6 +14,7 @@ class BuildTarget:
     sanitizer: Final[build_system.compiler.build_option.sanitizer.CompilerSanitizer]
 
     dir: Optional[pathlib.Path]
+    script_dir: Optional[pathlib.Path]
 
     def __init__(self,
                  compiler_instance: build_system.compiler.installed_instance.CompilerInstance,
