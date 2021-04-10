308942e299814a0702f8ad1964c87ca6f2ea26bf
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-10 11:06:39 -0400

    Remove CLI mode for func

--- conf/script/src/build_system/cmd/setup/setup_steps/step_create_symlimks_to_compilers_export_shell_env_vars_script.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_create_symlimks_to_compilers_export_shell_env_vars_script.py
@@ -5,16 +5,14 @@ import build_system.build_target.compiler_instance_targets
 from build_system.cmd.hierarchy.consts import TARGET_SCRIPT_EXPORT_SHELL_ENV_VARS_NAME
 
 
-def create_symlinks_to_compilers_export_shell_env_vars_script(targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets],
-                                                              cli_mode: bool):
+def create_symlinks_to_compilers_export_shell_env_vars_script(targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets]):
     for compiler_instance_targets in targets:
         if compiler_instance_targets.compiler_instance.has_export_shell_env_vars_script():
             for target in compiler_instance_targets:
-                _create_symlinks_to_compilers_export_shell_env_vars__script(target=target, cli_mode=cli_mode)
+                _create_symlinks_to_compilers_export_shell_env_vars__script(target=target)
 
 
-def _create_symlinks_to_compilers_export_shell_env_vars__script(target: build_system.build_target.build_target.BuildTarget,
-                                                                cli_mode: bool):
+def _create_symlinks_to_compilers_export_shell_env_vars__script(target: build_system.build_target.build_target.BuildTarget):
     export_shell_env_vars_script: Path = target.compiler_instance.get_export_shell_env_vars_script()
 
     symlink = target.script_dir / TARGET_SCRIPT_EXPORT_SHELL_ENV_VARS_NAME
