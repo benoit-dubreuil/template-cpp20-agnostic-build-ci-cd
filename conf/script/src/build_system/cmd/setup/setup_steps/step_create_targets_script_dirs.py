37a48fb2820c14eee10cd3c22e851a30a9b5f5e8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-10 10:04:45 -0400

    Added the WIP step_create_targets_script_dirs script

---
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_create_targets_script_dirs.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+import build_system.build_target.compiler_instance_targets
+
+
+def create_targets_script_dirs() -> list[Path]:
+    ...
+
+
+def _create_target_script_dir() -> Path:
+    ...
