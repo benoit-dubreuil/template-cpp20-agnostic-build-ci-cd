7d404ad4157f2b75d42610c6770e6191e0d9c5a8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-12 09:33:16 -0400

    Renamed param

--- conf/script/src/build_system/cmd/setup/meson_machine_file_args.py
+++ conf/script/src/build_system/cmd/setup/meson_machine_file_args.py
@@ -18,10 +18,10 @@ def generate_meson_machine_files_args(compiler_instance: build_system.compiler.i
                                             meson_machine_files_dir=meson_machine_files_dir,
                                             native_machine_files_dir=native_machine_files_dir)
 
-    machine_files_cli_args: list[str] = _machine_files_to_args(machine_files=machine_files)
-    _insert_setup_cli_arg_native_file(machine_files_cli_args=machine_files_cli_args)
+    machine_files_args: list[str] = _machine_files_to_args(machine_files=machine_files)
+    _insert_setup_cli_arg_native_file(machine_files_args=machine_files_args)
 
-    return machine_files_cli_args
+    return machine_files_args
 
 
 def _assemble_machine_files(compiler_instance: build_system.compiler.installed_instance.CompilerInstance,
@@ -40,9 +40,9 @@ def _machine_files_to_args(machine_files: list[Path]) -> list[str]:
     return [str(machine_file) for machine_file in machine_files]
 
 
-def _insert_setup_cli_arg_native_file(machine_files_cli_args: list[str]) -> None:
+def _insert_setup_cli_arg_native_file(machine_files_args: list[str]) -> None:
     setup_cli_arg_native_file: Final[str] = r'--native-file'
     step: Final[int] = 2
 
-    for i in range(0, len(machine_files_cli_args) * step, step):
-        machine_files_cli_args.insert(i, setup_cli_arg_native_file)
+    for i in range(0, len(machine_files_args) * step, step):
+        machine_files_args.insert(i, setup_cli_arg_native_file)
