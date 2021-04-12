1db604e31d1693d369cb1e5b80566fbe740bcf3f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-12 10:32:17 -0400

    Added UTF_8 const

--- conf/script/src/build_system/cmd/hierarchy/consts.py
+++ conf/script/src/build_system/cmd/hierarchy/consts.py
@@ -1,3 +1,4 @@
+import encodings
 from typing import Final
 
 import utils.cli.arg
@@ -6,17 +7,19 @@ DIR_STR: Final[str] = 'dir'
 ROOT_STR: Final[str] = 'root'
 BUILD_SYSTEM_NAME: Final[str] = 'meson'
 
+BUILD_SYSTEM_CONF_FILE_NAME: Final[str] = f'{BUILD_SYSTEM_NAME}.build'
+
 BUILD_DIR_NAME: Final[str] = 'build'
-CONF_DIR_NAME: Final[str] = 'conf'
-CONF_BUILD_SYSTEM_DIR_NAME: Final[str] = f'{BUILD_DIR_NAME}-system'
-MESON_MACHINE_FILES_DIR_NAME: Final[str] = 'machine'
 TARGET_SCRIPT_DIR_NAME = 'script'
 TARGET_SCRIPT_EXPORT_SHELL_ENV_NAME = 'export_shell_env'
 TARGET_SCRIPT_COMPILER_ENV_NAME = 'compiler.env'
+
+CONF_DIR_NAME: Final[str] = 'conf'
+CONF_BUILD_SYSTEM_DIR_NAME: Final[str] = f'{BUILD_DIR_NAME}-system'
+MESON_MACHINE_FILES_DIR_NAME: Final[str] = 'machine'
 
 ROOT_DIR_ARG: Final[utils.cli.arg.CLIArg] = utils.cli.arg.CLIArg(f'{ROOT_STR}{DIR_STR}')
 BUILD_DIR_ARG: Final[utils.cli.arg.CLIArg] = utils.cli.arg.CLIArg(f'{BUILD_DIR_NAME}{DIR_STR}')
 
 BUILD_DIR_PERMISSIONS: Final[int] = 0o770
-
-BUILD_SYSTEM_CONF_FILE_NAME: Final[str] = f'{BUILD_SYSTEM_NAME}.build'
+UTF_8: Final[str] = encodings.utf_8.getregentry().name
