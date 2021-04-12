359d62cddf20dccecb68f5ffedd2527303189a62
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-12 11:27:58 -0400

    Encode the compiler env

--- conf/script/src/build_system/cmd/setup/setup_steps/step_save_compiler_env.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_save_compiler_env.py
@@ -23,6 +23,8 @@ def _save_compiler_target_env(target: build_system.build_target.build_target.Bui
 
     assert target.compiler_instance.has_cached_compiler_env()
     target_compiler_env: dict[str, str] = _multi_line_compiler_env_to_single_line(compiler_env=target.compiler_instance.cached_compiler_env)
+    encoded_env: str = javaproperties.dumps(props=target_compiler_env, timestamp=False, ensure_ascii=False)
+
     ...
 
 
