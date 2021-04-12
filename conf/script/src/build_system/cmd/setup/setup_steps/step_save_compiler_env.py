b3c3c74ea66f09b103241fbc3a739c79aafdb970
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-12 09:47:30 -0400

    Finished structure of API save_compiler_instances_targets_env func

--- conf/script/src/build_system/cmd/setup/setup_steps/step_save_compiler_env.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_save_compiler_env.py
@@ -4,14 +4,14 @@ import build_system.build_target.build_target
 import build_system.build_target.compiler_instance_targets
 
 
-def save_compiler_env(targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets],
-                      cli_mode: bool) -> None:
+def save_compiler_instances_targets_env(targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets],
+                                        cli_mode: bool) -> None:
     for compiler_instance_targets in targets:
         if compiler_instance_targets.compiler_instance.has_export_shell_env_script():
             for target in compiler_instance_targets:
-                ...
+                _save_target_env(target=target, cli_mode=cli_mode)
 
 
-def _create_symlinks_to_compilers_export_shell_env__script(target: build_system.build_target.build_target.BuildTarget,
-                                                           cli_mode: bool)-> None:
+def _save_target_env(target: build_system.build_target.build_target.BuildTarget,
+                     cli_mode: bool) -> None:
     ...
