feae2a06f171f1166ec8597f65b03627e906d953
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-12 09:45:53 -0400

    Renamed func

--- conf/script/src/build_system/cmd/setup/setup_steps/step_create_symlimks_to_compilers_export_shell_env_scripts.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_create_symlimks_to_compilers_export_shell_env_scripts.py
@@ -10,11 +10,11 @@ def create_symlinks_to_compilers_export_shell_env_scripts(targets: list[build_sy
     for compiler_instance_targets in targets:
         if compiler_instance_targets.compiler_instance.has_export_shell_env_script():
             for target in compiler_instance_targets:
-                _create_symlinks_to_compilers_export_shell_env_script(target=target, cli_mode=cli_mode)
+                _create_symlink_to_compiler_export_shell_env_script(target=target, cli_mode=cli_mode)
 
 
-def _create_symlinks_to_compilers_export_shell_env_script(target: build_system.build_target.build_target.BuildTarget,
-                                                          cli_mode: bool) -> None:
+def _create_symlink_to_compiler_export_shell_env_script(target: build_system.build_target.build_target.BuildTarget,
+                                                        cli_mode: bool) -> None:
     from build_system.cmd.setup.cli.compiler_shell_env import print_symlink_to_compiler_export_shell_env_script
 
     export_shell_env_script: Path = target.compiler_instance.get_export_shell_env_script()
