173dab91b4e1549c14f6c7050c082344c6d02ec2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-12 10:28:33 -0400

    Create the target_compiler_env file

--- conf/script/src/build_system/cmd/setup/setup_steps/step_save_compiler_env.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/step_save_compiler_env.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import build_system.build_target.build_target
 import build_system.build_target.compiler_instance_targets
+from build_system.cmd.hierarchy.consts import TARGET_SCRIPT_COMPILER_ENV_NAME, BUILD_DIR_PERMISSIONS
 
 
 def save_compiler_instances_targets_env(targets: list[build_system.build_target.compiler_instance_targets.CompilerInstanceTargets],
@@ -14,4 +15,6 @@ def save_compiler_instances_targets_env(targets: list[build_system.build_target.
 
 def _save_compiler_target_env(target: build_system.build_target.build_target.BuildTarget,
                               cli_mode: bool) -> None:
+    target_compiler_env: Path = target.script_dir / TARGET_SCRIPT_COMPILER_ENV_NAME
+    target_compiler_env.touch(mode=BUILD_DIR_PERMISSIONS, exist_ok=True)
     ...
