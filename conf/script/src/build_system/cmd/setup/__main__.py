9f1a38c88fb05ad6846bab64d3e201223c089ecf
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-12 09:23:18 -0400

    Renamed file

--- conf/script/src/build_system/cmd/setup/__main__.py
+++ conf/script/src/build_system/cmd/setup/__main__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import build_system.cmd.setup.cli
+import build_system.cmd.setup.cli_impl
 import utils.cli.main
 
 
 def main():
-    build_system.cmd.setup.cli.setup()
+    build_system.cmd.setup.cli_impl.setup()
 
 
 utils.cli.main.wrap_main(main)
