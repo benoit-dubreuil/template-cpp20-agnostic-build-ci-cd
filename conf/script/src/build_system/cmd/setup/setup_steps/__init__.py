ca10e9d29a2dec3f165a5fe99bd4dc69427a6503
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-12 11:41:41 -0400

    Import save_compiler_instances_targets_env

--- conf/script/src/build_system/cmd/setup/setup_steps/__init__.py
+++ conf/script/src/build_system/cmd/setup/setup_steps/__init__.py
@@ -2,4 +2,5 @@ from build_system.cmd.setup.setup_steps.step_create_symlimks_to_compilers_export
 from build_system.cmd.setup.setup_steps.step_create_targets_build_dirs import create_targets_build_dirs
 from build_system.cmd.setup.setup_steps.step_fetch_host_compilers import fetch_host_compilers
 from build_system.cmd.setup.setup_steps.step_get_root_dir import get_verified_root_dir
+from build_system.cmd.setup.setup_steps.step_save_compiler_env import save_compiler_instances_targets_env
 from build_system.cmd.setup.setup_steps.step_setup_targets import setup_targets
