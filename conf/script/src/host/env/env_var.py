1168b2a792829f506e8be4ad8d5ca8ed73946c09
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-13 10:18:35 -0400

    Defined the structure of EnvVar

--- conf/script/src/host/env/env_var.py
+++ conf/script/src/host/env/env_var.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 
+from utils.more_typing import PathLike
+
 
 @dataclass(init=False, order=True)
 class EnvVar:
-    ...
+    key_name: PathLike
+    values: list[PathLike]
