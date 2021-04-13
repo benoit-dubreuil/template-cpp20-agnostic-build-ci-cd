d13d2a3d2a0343c0daf526985c129b43311bf5d5
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-13 16:07:28 -0400

    Bug fix : more paths were generic types instead of concrete types

--- conf/script/src/utils/more_typing.py
+++ conf/script/src/utils/more_typing.py
@@ -2,5 +2,8 @@ import os
 import pathlib
 import typing
 
-PathLike = typing.TypeVar("PathLike", pathlib.Path, os.PathLike, str, bytes)
-AnyPath = typing.TypeVar("AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
+PathLike = typing.Union[pathlib.Path, os.PathLike, str, bytes]
+AnyPath = typing.Union[pathlib.Path, os.PathLike, str, bytes, type(None)]
+
+T_PathLike = typing.TypeVar("T_PathLike", pathlib.Path, os.PathLike, str, bytes)
+T_AnyPath = typing.TypeVar("T_AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
