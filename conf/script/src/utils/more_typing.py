afe768ebc9845cc73a507a0af6265fd3e222d2cd
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-13 16:55:28 -0400

    Added constraints

--- conf/script/src/utils/more_typing.py
+++ conf/script/src/utils/more_typing.py
@@ -2,8 +2,11 @@ import os
 import pathlib
 import typing
 
-PathLike = typing.Union[pathlib.Path, os.PathLike, str, bytes]
-AnyPath = typing.Union[pathlib.Path, os.PathLike, str, bytes, type(None)]
+T_PathLike_Constraints = (pathlib.Path, os.PathLike, str, bytes)
+T_AnyPath_Constraints: typing.Final = (pathlib.Path, os.PathLike, str, bytes, type(None))
 
-T_PathLike = typing.TypeVar("T_PathLike", pathlib.Path, os.PathLike, str, bytes)
-T_AnyPath = typing.TypeVar("T_AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
+PathLike: typing.Final[type] = typing.Union[*T_PathLike_Constraints, ...]
+AnyPath: typing.Final[type] = typing.Union[*T_AnyPath_Constraints, ...]
+
+T_PathLike: typing.Final[type] = typing.TypeVar("T_PathLike", *T_PathLike_Constraints, ...)
+T_AnyPath: typing.Final[type] = typing.TypeVar("T_AnyPath", *T_AnyPath_Constraints, ...)
