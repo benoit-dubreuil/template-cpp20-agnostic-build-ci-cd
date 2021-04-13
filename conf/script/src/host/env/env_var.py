96ddfa757fb4273b972a9c72262d7fa34f8990ff
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-13 10:24:35 -0400

    Added constructor

--- conf/script/src/host/env/env_var.py
+++ conf/script/src/host/env/env_var.py
@@ -7,3 +7,7 @@ from utils.more_typing import PathLike
 class EnvVar:
     key_name: PathLike
     values: list[PathLike]
+
+    def __init__(self, key_name=None, values=None) -> None:
+        self.key_name = key_name if key_name is not None else str()
+        self.values = values if values is not None else []
