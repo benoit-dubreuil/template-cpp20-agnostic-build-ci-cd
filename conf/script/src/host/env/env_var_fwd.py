456cd1b5de5786e9161f7a42644ca4d27799b32d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 10:45:12 -0400

    Removed alias

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -7,6 +7,4 @@ TAlias_Default_AnyStr: typing.Final[type] = str
 T_EnvVar = typing.TypeVar('T_EnvVar', bound='EnvVar')
 
 T_Key: typing.Final[type] = typing.AnyStr
-
 T_Single_Value: typing.Final[type] = utils.more_typing.T_PathLike
-TAlias_Values: typing.Final[type] = list[T_Single_Value]
