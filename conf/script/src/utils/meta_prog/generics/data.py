edd32b015a8aad59c95cd615ff91489427209520
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 18:24:10 -0400

    Decorated with ClassVar

--- conf/script/src/utils/meta_prog/generics/data.py
+++ conf/script/src/utils/meta_prog/generics/data.py
@@ -6,7 +6,7 @@ import typing
 class GenericsData:
     TAlias_Generics_By_TypeVars = dict[typing.TypeVar, typing.Optional[type]]
 
-    generics_by_type_vars: typing.Final[TAlias_Generics_By_TypeVars]
+    generics_by_type_vars: typing.Final[typing.ClassVar[TAlias_Generics_By_TypeVars]]
 
     def __init__(self, *args,
                  generics_by_type_vars: TAlias_Generics_By_TypeVars = None,
