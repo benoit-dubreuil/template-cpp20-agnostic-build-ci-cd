0ebf3c874e981067d36520f8b1e2a7f8ac55cd9b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 16:10:13 -0400

    Detect type vars

--- conf/script/src/utils/meta_prog/generics/proxy.py
+++ conf/script/src/utils/meta_prog/generics/proxy.py
@@ -7,9 +7,11 @@ class GenericClassProxy(GenericClassMixin):
     _TAlias_generic_cls = type[GenericClassMixin, Generic]
 
     wrapped_generic_cls: _TAlias_generic_cls
+    type_vars: list[TypeVar]
 
     def __init__(self, generic_cls: _TAlias_generic_cls, *args, **kwargs) -> None:
         self.wrapped_generic_cls = generic_cls
+        self.type_vars = self.__detect_type_vars(generic_cls=generic_cls)
         super().__init__(*args, **kwargs)
 
     def __call__(self, *args, **kwargs):
@@ -18,3 +20,22 @@ class GenericClassProxy(GenericClassMixin):
     @property
     def __class__(self) -> _TAlias_generic_cls:
         return self.wrapped_generic_cls
+
+    @classmethod
+    def __detect_type_vars(cls, generic_cls: _TAlias_generic_cls) -> list[TypeVar]:
+        from typing import TypeVar, Generic, get_args, get_origin
+
+        type_vars: list[TypeVar] = []
+
+        for orig_base in generic_cls.__orig_bases__:
+            if get_origin(orig_base) is Generic:
+                base_type_args = get_args(orig_base)
+
+                if len(base_type_args) > 0:
+                    for type_arg in base_type_args:
+                        if isinstance(type_arg, TypeVar):
+                            type_vars.append(type_arg)
+                else:
+                    type_vars += cls.__detect_type_vars(generic_cls=orig_base)
+
+        return type_vars
