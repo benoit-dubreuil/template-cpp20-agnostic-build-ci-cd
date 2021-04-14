dbf9228caf6c613f9d1394514ff1f6e871be278b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 14:59:44 -0400

    Renamed param

--- conf/script/src/utils/meta_prog/generics/aware_cls_proxy.py
+++ conf/script/src/utils/meta_prog/generics/aware_cls_proxy.py
@@ -4,8 +4,8 @@ from utils.meta_prog.generics.cls_mixin import GenericClassMixin
 class GenericAwareClassProxy(GenericClassMixin):
     wrapped_generic_cls: type[GenericClassMixin]
 
-    def __init__(self, wrapped_generic_cls: type[GenericClassMixin], *args, **kwargs) -> None:
-        self.wrapped_generic_cls = wrapped_generic_cls
+    def __init__(self, generic_cls: type[GenericClassMixin], *args, **kwargs) -> None:
+        self.wrapped_generic_cls = generic_cls
         super().__init__(*args, **kwargs)
 
     def __call__(self):
