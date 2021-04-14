49dd5f85b894b4443ef0605c11238921cf239e08
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 19:15:13 -0400

    Fixed type hints for proxy injector

--- conf/script/src/utils/meta_prog/generics/cls_proxy_injector.py
+++ conf/script/src/utils/meta_prog/generics/cls_proxy_injector.py
@@ -1,10 +1,12 @@
 import utils.meta_prog.generics.cls_mixin
 import utils.meta_prog.generics.cls_proxy
+import typing
 
 
 class GenericClassProxyInjectorMixin(utils.meta_prog.generics.cls_mixin.GenericClassMixin):
+    __TAlias_Generics_Subscript_Op = typing.Optional[typing.Union[tuple, type]]
 
     @classmethod
-    def __class_getitem__(cls, item: tuple):
+    def __class_getitem__(cls, item: __TAlias_Generics_Subscript_Op):
         generics = item if isinstance(item, tuple) else (item,)
         return utils.meta_prog.generics.cls_proxy.GenericClassProxy(generic_cls=cls, generics=generics)
