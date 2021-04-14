04820ab96a79a32f24ea72a73a3f6f5c39f467bd
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 16:52:25 -0400

    Made the data hold generics_by_type_vars

--- conf/script/src/utils/meta_prog/generics/data.py
+++ conf/script/src/utils/meta_prog/generics/data.py
@@ -4,8 +4,10 @@ import typing
 
 @dataclasses.dataclass()
 class GenericsData:
-    generics: typing.Final[tuple[type]]
+    generics_by_type_vars: typing.Final[dict[typing.TypeVar, type]]
 
-    def __init__(self, *args, generics: tuple[type] = tuple(), **kwargs) -> None:
-        self.generics = generics
+    def __init__(self, *args,
+                 generics_by_type_vars: dict[typing.TypeVar, type] = None,
+                 **kwargs) -> None:
+        self.generics_by_type_vars = generics_by_type_vars if generics_by_type_vars is not None else {}
         super().__init__(*args, **kwargs)
