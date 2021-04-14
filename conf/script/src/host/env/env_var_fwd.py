a9001a5e32e144a83822b5d27521b44edd84e833
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 09:18:25 -0400

    Do not forward classes

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -1,5 +1,3 @@
-import abc
-import collections.abc
 import typing
 
 import utils.more_typing
@@ -7,18 +5,3 @@ import utils.more_typing
 T_Key: typing.Final[type] = utils.more_typing.T_PathLike
 T_Single_Value: typing.Final[type] = utils.more_typing.T_PathLike
 T_Values: typing.Final[type] = list[T_Single_Value]
-
-
-# Forward declaration
-class EnvVarBaseIt(collections.abc.Iterator[utils.more_typing.T_PathLike], metaclass=abc.ABCMeta):
-    pass
-
-
-# Forward declaration
-class EnvVarKeyIt(EnvVarBaseIt, metaclass=abc.ABCMeta):
-    pass
-
-
-# Forward declaration
-class EnvVarJoinedValues(collections.abc.Iterator[T_Single_Value], metaclass=abc.ABCMeta):
-    pass
