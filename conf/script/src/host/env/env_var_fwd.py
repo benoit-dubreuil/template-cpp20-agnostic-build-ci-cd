816bff8016d939881d4e806f6b203a710543fbd2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 10:33:10 -0400

    Fixed fwd

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -2,12 +2,11 @@ import typing
 
 import utils.more_typing
 
+TAlias_Default_AnyStr: typing.Final[type] = str
+
 T_EnvVar = typing.TypeVar('T_EnvVar', bound='EnvVar')
 
 T_Key: typing.Final[type] = typing.AnyStr
 
 T_Single_Value: typing.Final[type] = utils.more_typing.T_PathLike
-T_Values_Collection: typing.Final[type[typing.Generic]] = list
-
-T_Values: typing.Final[type] = T_Values_Collection[T_Single_Value]
-T_Default_AnyStr: typing.Final[type] = str
+TAlias_Values: typing.Final[type] = list[T_Single_Value]
