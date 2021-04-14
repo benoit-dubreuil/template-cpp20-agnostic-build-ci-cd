fa8a3999b98b5ca9bb39faebb0971961005cb028
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 16:52:37 -0400

    Simplified the mixin

--- conf/script/src/utils/meta_prog/generics/mixin.py
+++ conf/script/src/utils/meta_prog/generics/mixin.py
@@ -2,13 +2,6 @@ from utils.meta_prog.generics.data import GenericsData
 
 
 class GenericClassMixin(GenericsData):
-    from typing import Final, TypeVar
-
-    type_vars: Final[tuple[TypeVar]]
-
-    def __init__(self, *args, type_vars: tuple[TypeVar] = tuple(), **kwargs):
-        self.type_vars = type_vars
-        super().__init__(*args, **kwargs)
 
     def has_generics(self) -> bool:
-        return len(self.generics) > 0
+        return len(self.generics_by_type_vars) > 0
