71b9e85ea8f9c55308f9711eef4529c6c6ca859a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 09:59:45 -0400

    Added T_Joined_Values

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -8,3 +8,4 @@ T_Single_Value: typing.Final[type] = utils.more_typing.T_PathLike
 T_Values_Collection: typing.Final[type[typing.Generic]] = list
 
 T_Values: typing.Final[type] = T_Values_Collection[T_Single_Value]
+T_Joined_Values: typing.Final[type] = typing.AnyStr
