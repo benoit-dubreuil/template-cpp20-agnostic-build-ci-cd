124439891eea2fc59eb60fc36cec902bf20a5b78
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 18:23:56 -0400

    Added alias

--- conf/script/src/utils/meta_prog/generics/data.py
+++ conf/script/src/utils/meta_prog/generics/data.py
@@ -4,10 +4,12 @@ import typing
 
 @dataclasses.dataclass()
 class GenericsData:
-    generics_by_type_vars: typing.Final[dict[typing.TypeVar, type]]
+    TAlias_Generics_By_TypeVars = dict[typing.TypeVar, typing.Optional[type]]
+
+    generics_by_type_vars: typing.Final[TAlias_Generics_By_TypeVars]
 
     def __init__(self, *args,
-                 generics_by_type_vars: dict[typing.TypeVar, type] = None,
+                 generics_by_type_vars: TAlias_Generics_By_TypeVars = None,
                  **kwargs) -> None:
         self.generics_by_type_vars = generics_by_type_vars if generics_by_type_vars is not None else {}
         super().__init__(*args, **kwargs)
