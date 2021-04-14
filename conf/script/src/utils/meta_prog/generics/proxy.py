cd0a3169447c989850e13bcf6460699cfeeb0303
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 16:10:05 -0400

    Added alias

--- conf/script/src/utils/meta_prog/generics/proxy.py
+++ conf/script/src/utils/meta_prog/generics/proxy.py
@@ -2,9 +2,13 @@ from utils.meta_prog.generics.mixin import GenericClassMixin
 
 
 class GenericClassProxy(GenericClassMixin):
-    wrapped_generic_cls: type[GenericClassMixin]
+    from typing import TypeVar, Generic
 
-    def __init__(self, generic_cls: type[GenericClassMixin], *args, **kwargs) -> None:
+    _TAlias_generic_cls = type[GenericClassMixin, Generic]
+
+    wrapped_generic_cls: _TAlias_generic_cls
+
+    def __init__(self, generic_cls: _TAlias_generic_cls, *args, **kwargs) -> None:
         self.wrapped_generic_cls = generic_cls
         super().__init__(*args, **kwargs)
 
@@ -12,5 +16,5 @@ class GenericClassProxy(GenericClassMixin):
         return self.wrapped_generic_cls(*args, generics=self.generics, **kwargs)
 
     @property
-    def __class__(self) -> type[GenericClassMixin]:
+    def __class__(self) -> _TAlias_generic_cls:
         return self.wrapped_generic_cls
