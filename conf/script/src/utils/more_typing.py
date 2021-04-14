4daaa16ead95b8f9a8dd3ad325842f125f7b531a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 14:57:44 -0400

    Bug fix : type hints caused fuck ups

--- conf/script/src/utils/more_typing.py
+++ conf/script/src/utils/more_typing.py
@@ -2,11 +2,11 @@ import os
 import pathlib
 import typing
 
-T_PathLike_Constraints: typing.Final = (pathlib.Path, os.PathLike, str, bytes)
-T_AnyPath_Constraints: typing.Final = (pathlib.Path, os.PathLike, str, bytes, type(None))
+T_PathLike_Constraints = (pathlib.Path, os.PathLike, str, bytes)
+T_AnyPath_Constraints = (pathlib.Path, os.PathLike, str, bytes, type(None))
 
-PathLike: typing.Final[type] = typing.Union[pathlib.Path, os.PathLike, str, bytes]
-AnyPath: typing.Final[type] = typing.Union[pathlib.Path, os.PathLike, str, bytes, type(None)]
+PathLike = typing.Union[pathlib.Path, os.PathLike, str, bytes]
+AnyPath = typing.Union[pathlib.Path, os.PathLike, str, bytes, type(None)]
 
-T_PathLike: typing.Final[type] = typing.TypeVar("T_PathLike", pathlib.Path, os.PathLike, str, bytes)
-T_AnyPath: typing.Final[type] = typing.TypeVar("T_AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
+T_PathLike = typing.TypeVar("T_PathLike", pathlib.Path, os.PathLike, str, bytes)
+T_AnyPath = typing.TypeVar("T_AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
