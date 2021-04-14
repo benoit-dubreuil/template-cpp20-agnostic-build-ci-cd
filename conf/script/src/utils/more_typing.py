84d08ec400fae9c5b560d6d731b6cb03d1f91570
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 10:28:41 -0400

    Fixed more_typing

--- conf/script/src/utils/more_typing.py
+++ conf/script/src/utils/more_typing.py
@@ -5,8 +5,8 @@ import typing
 T_PathLike_Constraints: typing.Final = (pathlib.Path, os.PathLike, str, bytes)
 T_AnyPath_Constraints: typing.Final = (pathlib.Path, os.PathLike, str, bytes, type(None))
 
-PathLike: typing.Final[type] = typing.Union[*T_PathLike_Constraints, ...]
-AnyPath: typing.Final[type] = typing.Union[*T_AnyPath_Constraints, ...]
+PathLike: typing.Final[type] = typing.Union[pathlib.Path, os.PathLike, str, bytes]
+AnyPath: typing.Final[type] = typing.Union[pathlib.Path, os.PathLike, str, bytes, type(None)]
 
-T_PathLike: typing.Final[type] = typing.TypeVar("T_PathLike", *T_PathLike_Constraints, ...)
-T_AnyPath: typing.Final[type] = typing.TypeVar("T_AnyPath", *T_AnyPath_Constraints, ...)
+T_PathLike: typing.Final[type] = typing.TypeVar("T_PathLike", pathlib.Path, os.PathLike, str, bytes)
+T_AnyPath: typing.Final[type] = typing.TypeVar("T_AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
