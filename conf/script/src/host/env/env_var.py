05f43d79583391d377fb29167a5c00d7d4d7b795
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 09:21:22 -0400

    Added type hints

--- conf/script/src/host/env/env_var.py
+++ conf/script/src/host/env/env_var.py
@@ -15,7 +15,7 @@ class EnvVar(collections.abc.Mapping[_fwd.T_Key, _fwd.T_Values]):
 
     __ENV_VAR_ITEM_COUNT: Final[int] = 1
 
-    def __init__(self, key_name=None, values=None) -> None:
+    def __init__(self, key_name: _fwd.T_Key = None, values: _fwd.T_Values = None) -> None:
         self.__env_key = key_name if key_name is not None else str()
         self.__env_values = values if values is not None else _fwd.T_Values()
 
