387e7770c21e01bb777a66ef8d7702ea66bc91d9
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 16:30:17 -0400

    Integrated generics data into mixin

--- conf/script/src/utils/meta_prog/generics/mixin.py
+++ conf/script/src/utils/meta_prog/generics/mixin.py
@@ -1,11 +1,12 @@
-class GenericClassMixin:
+from utils.meta_prog.generics.data import GenericsData
+
+
+class GenericClassMixin(GenericsData):
     from typing import Final, TypeVar
 
-    generics: Final[tuple[type]]
     type_vars: Final[tuple[TypeVar]]
 
-    def __init__(self, *args, type_vars: tuple[TypeVar] = None, generics: tuple[type] = None, **kwargs):
-        self.generics = generics if generics is not None else tuple()
+    def __init__(self, *args, type_vars: tuple[TypeVar] = None, **kwargs):
         self.type_vars = type_vars if type_vars is not None else tuple()
 
         super().__init__(*args, **kwargs)
