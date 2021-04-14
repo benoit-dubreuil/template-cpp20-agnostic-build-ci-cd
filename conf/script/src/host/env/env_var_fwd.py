7ab67ed9d3b676a87d296ba07f32a2e07327f81a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 14:58:16 -0400

    Removed type hints for fwd

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -2,9 +2,9 @@ import typing
 
 import utils.more_typing
 
-TAlias_Default_AnyStr: typing.Final[type] = str
+TAlias_Default_AnyStr = str
 
 T_EnvVar = typing.TypeVar('T_EnvVar', bound='EnvVar')
 
-T_Key: typing.Final[type] = typing.AnyStr
-T_Single_Value: typing.Final[type] = utils.more_typing.T_PathLike
+T_Key = typing.AnyStr
+T_Single_Value = utils.more_typing.T_PathLike
