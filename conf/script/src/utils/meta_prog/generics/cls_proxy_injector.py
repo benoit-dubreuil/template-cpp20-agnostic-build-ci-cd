628b6902a39615adb19d8eec14e425ee6c66b5c2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 19:12:16 -0400

    Bug fix : Damn tuples and their... non-iterable-ness-ish

--- conf/script/src/utils/meta_prog/generics/cls_proxy_injector.py
+++ conf/script/src/utils/meta_prog/generics/cls_proxy_injector.py
@@ -6,4 +6,5 @@ class GenericClassProxyInjectorMixin(utils.meta_prog.generics.cls_mixin.GenericC
 
     @classmethod
     def __class_getitem__(cls, item: tuple):
-        return utils.meta_prog.generics.cls_proxy.GenericClassProxy(generic_cls=cls, generics=item)
+        generics = item if isinstance(item, tuple) else (item,)
+        return utils.meta_prog.generics.cls_proxy.GenericClassProxy(generic_cls=cls, generics=generics)
