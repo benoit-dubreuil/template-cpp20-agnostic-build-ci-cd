bdde7c0c2cb16ae856fbdb01b72165974906af89
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 16:27:56 -0400

    Added type_vars member

--- conf/script/src/utils/meta_prog/generics/mixin.py
+++ conf/script/src/utils/meta_prog/generics/mixin.py
@@ -1,10 +1,13 @@
 class GenericClassMixin:
-    from typing import Final
+    from typing import Final, TypeVar
 
     generics: Final[tuple[type]]
+    type_vars: Final[tuple[TypeVar]]
 
-    def __init__(self, *args, generics: tuple[type] = None, **kwargs):
+    def __init__(self, *args, type_vars: tuple[TypeVar] = None, generics: tuple[type] = None, **kwargs):
         self.generics = generics if generics is not None else tuple()
+        self.type_vars = type_vars if type_vars is not None else tuple()
+
         super().__init__(*args, **kwargs)
 
     def has_generics(self) -> bool:
