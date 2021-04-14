74b0f6b2bffe218cc6934bd338c9a115df6603ae
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 18:54:14 -0400

    Fixed imports

--- conf/script/src/utils/meta_prog/generics/cls_proxy_injector.py
+++ conf/script/src/utils/meta_prog/generics/cls_proxy_injector.py
@@ -1,9 +1,9 @@
-from utils.meta_prog.generics.cls_mixin import GenericClassMixin
-from utils.meta_prog.generics.cls_proxy import GenericClassProxy
+import utils.meta_prog.generics.cls_mixin
+import utils.meta_prog.generics.cls_proxy
 
 
-class GenericClassProxyInjectorMixin(GenericClassMixin):
+class GenericClassProxyInjectorMixin(utils.meta_prog.generics.cls_mixin.GenericClassMixin):
 
     @classmethod
     def __class_getitem__(cls, item: tuple):
-        return GenericClassProxy(generic_cls=cls, generics=item)
+        return utils.meta_prog.generics.cls_proxy.GenericClassProxy(generic_cls=cls, generics=item)
