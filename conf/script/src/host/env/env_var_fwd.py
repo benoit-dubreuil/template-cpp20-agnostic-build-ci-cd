5dcb52949da26542b9ebbd088855f4f27598a52e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 09:54:19 -0400

    Added T_Values_Collection

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -3,5 +3,8 @@ import typing
 import utils.more_typing
 
 T_Key: typing.Final[type] = typing.AnyStr
+
 T_Single_Value: typing.Final[type] = utils.more_typing.T_PathLike
-T_Values: typing.Final[type] = list[T_Single_Value]
+T_Values_Collection: typing.Final[type[typing.Generic]] = list
+
+T_Values: typing.Final[type] = T_Values_Collection[T_Single_Value]
