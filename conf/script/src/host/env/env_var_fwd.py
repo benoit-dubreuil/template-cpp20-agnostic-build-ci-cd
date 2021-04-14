56bb75455d764e650943477dc4511bd8757b4b15
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-14 09:24:46 -0400

    Switched T_Key type to AnyStr

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -2,6 +2,6 @@ import typing
 
 import utils.more_typing
 
-T_Key: typing.Final[type] = utils.more_typing.T_PathLike
+T_Key: typing.Final[type] = typing.AnyStr
 T_Single_Value: typing.Final[type] = utils.more_typing.T_PathLike
 T_Values: typing.Final[type] = list[T_Single_Value]
