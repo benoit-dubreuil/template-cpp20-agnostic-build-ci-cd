cc662b413762fc8554722b64c331e4f548a918e7
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-15 10:43:00 -0400

    Create generics_by_type_vars if missing

--- conf/script/src/utils/meta_prog/generics/cls_proxy_injector.py
+++ conf/script/src/utils/meta_prog/generics/cls_proxy_injector.py
@@ -7,8 +7,18 @@ import utils.meta_prog.generics.cls_proxy
 class GenericClassProxyInjectorMixin(utils.meta_prog.generics.cls_mixin.GenericClassMixin):
     __TAlias_Generics_Subscript_Op = typing.Optional[typing.Union[tuple, type]]
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self,
+                 *args,
+                 generics_by_type_vars: utils.meta_prog.generics.cls_mixin.GenericClassMixin.TAlias_Generics_By_TypeVars = None,
+                 **kwargs) -> None:
+        if generics_by_type_vars is None:
+            generic_cls = type(self)
+            generics = tuple()
+            cls_proxy = utils.meta_prog.generics.cls_proxy.GenericClassProxy(generic_cls=generic_cls, generics=generics)
+
+            generics_by_type_vars = cls_proxy.generics_by_type_vars
+
+        super().__init__(*args, generics_by_type_vars=generics_by_type_vars, **kwargs)
 
     @classmethod
     def __class_getitem__(cls, item: __TAlias_Generics_Subscript_Op):
