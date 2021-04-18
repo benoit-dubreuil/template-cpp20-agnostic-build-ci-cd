eeadc70bcf1ff5b57d5fb9b18ec49f0e2fd9a291
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-18 10:43:48 -0400

    Added constant

--- conf/script/src/utils/meta_prog/encapsulation/export.py
+++ conf/script/src/utils/meta_prog/encapsulation/export.py
@@ -1,11 +1,15 @@
 import inspect
+import typing
 
 
 # https://stackoverflow.com/a/35710527/2924010
 def export(func):
+    attribute_name_all: typing.Final[str] = '__all__'
+
     module = inspect.getmodule(func)
-    if hasattr(module, '__all__'):
-        module.__all__.append(func.__name__)
+
+    if hasattr(module, attribute_name_all):
+        module.__all__.append(func.__qualname__)
     else:
         module.__all__ = [func.__name__]
     return fn
