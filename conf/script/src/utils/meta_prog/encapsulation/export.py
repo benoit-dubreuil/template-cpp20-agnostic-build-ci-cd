c0302122d96d810061e9a83955df0250f2db6d16
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-18 10:27:41 -0400

    Renamed var

--- conf/script/src/utils/meta_prog/encapsulation/export.py
+++ conf/script/src/utils/meta_prog/encapsulation/export.py
@@ -3,11 +3,11 @@ import inspect
 
 # https://stackoverflow.com/a/35710527/2924010
 def export(fn):
-    mod = inspect.getmodule(fn)
-    if hasattr(mod, '__all__'):
-        mod.__all__.append(fn.__name__)
+    module = inspect.getmodule(fn)
+    if hasattr(module, '__all__'):
+        module.__all__.append(fn.__name__)
     else:
-        mod.__all__ = [fn.__name__]
+        module.__all__ = [fn.__name__]
     return fn
 
 
