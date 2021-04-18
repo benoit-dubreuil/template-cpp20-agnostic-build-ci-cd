c4948d5b60d15ef50be6a593dcda6b64b70eefdf
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-18 10:49:53 -0400

    Use variables instead of hidden attributes

--- conf/script/src/utils/meta_prog/encapsulation/export.py
+++ conf/script/src/utils/meta_prog/encapsulation/export.py
@@ -7,12 +7,12 @@ def export(func):
     attribute_name_all: typing.Final[str] = '__all__'
 
     module = inspect.getmodule(func)
-    func_api = func.__qualname__
 
-    if hasattr(module, attribute_name_all):
-        module.__all__.append(func_api)
-    else:
-        module.__all__ = [func_api]
+    func_api = func.__qualname__
+    module_api: list[str] = getattr(module, attribute_name_all, [])
+
+    module_api.append(func_api)
+    setattr(module, attribute_name_all, module_api)
 
     print(func)
     print(module)
