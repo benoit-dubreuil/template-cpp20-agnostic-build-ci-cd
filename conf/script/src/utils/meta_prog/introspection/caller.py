78ed4e216fb927a2d64c827a09a11a3b2463fb7a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-18 14:14:42 -0400

    Added get_nth_caller func

---
+++ conf/script/src/utils/meta_prog/introspection/caller.py
@@ -0,0 +1,17 @@
+import inspect
+from types import FrameType
+from typing import Final
+
+CALLER_MIN_N: Final[int] = 1
+
+
+def get_nth_caller(n: int = CALLER_MIN_N) -> FrameType:
+    assert n >= CALLER_MIN_N
+
+    stack_indices = range(CALLER_MIN_N, n)
+    caller_frame: FrameType = inspect.currentframe()
+
+    for frame_index in stack_indices:
+        caller_frame = caller_frame.f_back
+
+    return caller_frame
