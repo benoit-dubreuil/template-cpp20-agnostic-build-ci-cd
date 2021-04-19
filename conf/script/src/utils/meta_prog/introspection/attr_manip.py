4a0483f55662b81b875f5fa919173979e296dda5
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 10:17:38 -0400

    Added get_or_create_attr func

--- conf/script/src/utils/meta_prog/introspection/attr_manip.py
+++ conf/script/src/utils/meta_prog/introspection/attr_manip.py
@@ -4,6 +4,27 @@ from macro import *
 T_Attr_Val = TypeVar('T_Attr_Val')
 
 
+def get_or_create_attr(obj, attr: str, default_val: Optional[T_Attr_Val] = None) -> T_Attr_Val:
+    attr_val: T_Attr_Val
+
+    assert hasattr(obj, Macro.DICT)
+
+    if not hasattr(obj, attr):
+        non_none_default_val: T_Attr_Val
+
+        if default_val is None:
+            non_none_default_val = T_Attr_Val()
+        else:
+            non_none_default_val = default_val
+
+        attr_val = non_none_default_val
+        setattr(obj, attr, attr_val)
+    else:
+        attr_val = getattr(obj, attr)
+
+    return attr_val
+
+
 def get_or_create_optional_attr(obj, attr: str, default_val: Optional[T_Attr_Val] = None) -> Optional[T_Attr_Val]:
     attr_val: Optional[T_Attr_Val]
 
