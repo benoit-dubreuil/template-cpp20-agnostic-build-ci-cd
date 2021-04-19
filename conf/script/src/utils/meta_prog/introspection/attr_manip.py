b9e7ffd5c345f549970b8679dc73ce219c0197ec
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 10:13:33 -0400

    Added assert

--- conf/script/src/utils/meta_prog/introspection/attr_manip.py
+++ conf/script/src/utils/meta_prog/introspection/attr_manip.py
@@ -1,4 +1,5 @@
 from typing import *
+from macro import *
 
 T_Attr_Val = TypeVar('T_Attr_Val')
 
@@ -6,9 +7,11 @@ T_Attr_Val = TypeVar('T_Attr_Val')
 def get_or_create_optional_attr(obj, attr: str, default_val: Optional[T_Attr_Val] = None) -> Optional[T_Attr_Val]:
     attr_val: Optional[T_Attr_Val]
 
+    assert hasattr(obj, Macro.DICT)
+
     if not hasattr(obj, attr):
         attr_val = default_val
-        setattr(obj, attr, default_val)
+        setattr(obj, attr, attr_val)
     else:
         attr_val = getattr(obj, attr)
 
