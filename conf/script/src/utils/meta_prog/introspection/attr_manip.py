2d8ce2ac1dd682af0332ef86b1f83abbdfe11d80
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 09:34:12 -0400

    Added generics to get_or_create_attr

--- conf/script/src/utils/meta_prog/introspection/attr_manip.py
+++ conf/script/src/utils/meta_prog/introspection/attr_manip.py
@@ -1,7 +1,11 @@
-from typing import Optional
+from typing import *
+
+T_Attr_Val = TypeVar('T_Attr_Val')
 
 
-def get_or_create_attr(obj, attr: str, default_val: Optional = None):
+def get_or_create_attr(obj, attr: str, default_val: Optional[T_Attr_Val] = None) -> Optional[T_Attr_Val]:
+    attr_val: Optional[T_Attr_Val]
+
     if not hasattr(obj, attr):
         attr_val = default_val
         setattr(obj, attr, default_val)
