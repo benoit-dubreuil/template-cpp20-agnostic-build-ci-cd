64a2c2d7ad73b84c8ad492d252c12baaa6616caa
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 12:15:32 -0400

    Export main

--- conf/script/src/utils/cli/main.py
+++ conf/script/src/utils/cli/main.py
@@ -2,15 +2,20 @@ from typing import Callable
 
 import colorama
 
+from ..meta_prog.encapsulation import *
 
+
+@export
 def init():
     colorama.init()
 
 
+@export
 def deinit():
     colorama.deinit()
 
 
+@export
 def wrap_main(main_func: Callable):
     init()
     main_func()
