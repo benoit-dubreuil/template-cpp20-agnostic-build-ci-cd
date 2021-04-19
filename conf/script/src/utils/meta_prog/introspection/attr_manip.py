31b104e046b4d0942cdc12d9e92f22f5d9ee23a7
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 09:36:01 -0400

    Renamed func

--- conf/script/src/utils/meta_prog/introspection/attr_manip.py
+++ conf/script/src/utils/meta_prog/introspection/attr_manip.py
@@ -3,7 +3,7 @@ from typing import *
 T_Attr_Val = TypeVar('T_Attr_Val')
 
 
-def get_or_create_attr(obj, attr: str, default_val: Optional[T_Attr_Val] = None) -> Optional[T_Attr_Val]:
+def get_or_create_optional_attr(obj, attr: str, default_val: Optional[T_Attr_Val] = None) -> Optional[T_Attr_Val]:
     attr_val: Optional[T_Attr_Val]
 
     if not hasattr(obj, attr):
