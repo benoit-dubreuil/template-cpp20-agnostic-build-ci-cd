f50c85e0d55007102d1c5b31d8558f4467af4b1a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 12:23:04 -0400

    Fixed imports

--- conf/script/src/utils/more_typing.py
+++ conf/script/src/utils/more_typing.py
@@ -1,6 +1,6 @@
 import os
-import pathlib
-import typing
+from pathlib import Path
+from typing import TypeVar, Union
 
 from .meta_prog.introspection import *
 
@@ -8,11 +8,11 @@ __all__: TAlias_Macro_All = ['TConstraints_PathLike', 'TConstraints_AnyPath',
                              'PathLike', 'AnyPath',
                              'T_PathLike', 'T_AnyPath']
 
-TConstraints_PathLike = (pathlib.Path, os.PathLike, str, bytes)
-TConstraints_AnyPath = (pathlib.Path, os.PathLike, str, bytes, type(None))
+TConstraints_PathLike = (Path, os.PathLike, str, bytes)
+TConstraints_AnyPath = (Path, os.PathLike, str, bytes, type(None))
 
-PathLike = typing.Union[pathlib.Path, os.PathLike, str, bytes]
-AnyPath = typing.Union[pathlib.Path, os.PathLike, str, bytes, type(None)]
+PathLike = Union[Path, os.PathLike, str, bytes]
+AnyPath = Union[Path, os.PathLike, str, bytes, type(None)]
 
-T_PathLike = typing.TypeVar("T_PathLike", pathlib.Path, os.PathLike, str, bytes)
-T_AnyPath = typing.TypeVar("T_AnyPath", pathlib.Path, os.PathLike, str, bytes, type(None))
+T_PathLike = TypeVar("T_PathLike", Path, os.PathLike, str, bytes)
+T_AnyPath = TypeVar("T_AnyPath", Path, os.PathLike, str, bytes, type(None))
