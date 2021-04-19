ddcdb6dbd534ed01c205b5a4214ed0d199b09b30
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 12:17:55 -0400

    Reformat imports

--- conf/script/src/utils/cli/arg_parsing.py
+++ conf/script/src/utils/cli/arg_parsing.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 from ..meta_prog.encapsulation import *
 
-import utils.more_typing
+from utils.more_typing import *
 from .arg import *
 from ..error import *
 
 
 @export
 def add_optional_path_arg(arg_parser: argparse.ArgumentParser, path_arg: CLIArg = DEFAULT_PATH_ARG,
-                          path_arg_default_value: utils.more_typing.AnyPath = None, path_arg_help: Optional[AnyStr] = None):
+                          path_arg_default_value: AnyPath = None, path_arg_help: Optional[AnyStr] = None):
     arg_parser.add_argument(path_arg.prefixed_name, type=Path, nargs=argparse.OPTIONAL, const=path_arg_default_value, default=path_arg_default_value, help=path_arg_help)
 
 
@@ -19,18 +19,18 @@ def _assure_no_unknown_parsed_args(arg_parser: argparse.ArgumentParser, unknown_
         error.raise_or_exit_cli(arg_parser, print_usage=True)
 
 
-def _assure_nonempty_parsed_path(arg_parser: argparse.ArgumentParser, path_arg_name: str, parsed_path: utils.more_typing.AnyPath):
+def _assure_nonempty_parsed_path(arg_parser: argparse.ArgumentParser, path_arg_name: str, parsed_path: AnyPath):
     if str(parsed_path) == str():
         error = EmptyParsedArgError(path_arg_name)
         error.raise_or_exit_cli(arg_parser, print_usage=True)
 
 
 @export
-def parse_optional_path_arg(arg_parser: argparse.ArgumentParser, path_arg: CLIArg = DEFAULT_PATH_ARG) -> utils.more_typing.AnyPath:
+def parse_optional_path_arg(arg_parser: argparse.ArgumentParser, path_arg: CLIArg = DEFAULT_PATH_ARG) -> AnyPath:
     parsed_args, unknown_parsed_args = arg_parser.parse_known_args([path_arg.prefixed_name])
 
     _assure_no_unknown_parsed_args(arg_parser, unknown_parsed_args)
-    parsed_path: utils.more_typing.AnyPath = getattr(parsed_args, path_arg.name) if path_arg.name in parsed_args else None
+    parsed_path: AnyPath = getattr(parsed_args, path_arg.name) if path_arg.name in parsed_args else None
     _assure_nonempty_parsed_path(arg_parser, path_arg.name, parsed_path)
 
     return parsed_path
