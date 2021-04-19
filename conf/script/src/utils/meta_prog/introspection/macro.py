c71b89fbbd0d16bff7a312766df0a320ef1a1146
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 10:07:19 -0400

    Fixed macro inheritance

--- conf/script/src/utils/meta_prog/introspection/macro.py
+++ conf/script/src/utils/meta_prog/introspection/macro.py
@@ -6,13 +6,11 @@ TAlias_Macro_All = list[str]
 __all__: TAlias_Macro_All = ['Macro']
 
 
-@unique
 class AutoMacroFromName(str, Enum):
-    __AFFIX: Final[str] = '__'
 
     def _generate_next_value_(name: str, start, count, last_values) -> str:
-        affixed_name = AutoMacroFromName.__AFFIX + name + AutoMacroFromName.__AFFIX
-        return affixed_name
+        __AFFIX: Final[str] = '__'
+        return __AFFIX + name + __AFFIX
 
 
 @unique
