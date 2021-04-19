705acbf02840732f4c0544d82380d081631cd928
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 14:42:47 -0400

    Export CLI API

--- conf/script/src/build_system/cmd/compiler/host/get_info/location/msvc/cli.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/location/msvc/cli.py
@@ -2,20 +2,23 @@ from pathlib import Path
 from typing import NoReturn, Optional, Union
 
 import build_system.cmd.compiler.host.get_info.cli
-import build_system.compiler.core.family
-import ext.error.core.cls_def
+from build_system.compiler import *
+from ext.error import *
+
+from ext.meta_prog.encapsulation import *
 
 
 def _find_no_arg(compiler_installation_path: Optional[Path] = None) -> Union[Path, NoReturn]:
     compiler_installation_path: Optional[Path] = build_system.cmd.compiler.host.get_info.location.msvc.find_location(compiler_installation_path)
 
     if compiler_installation_path is None:
-        raise ext.error.core.cls_def.CompilerNotFoundError()
+        raise CompilerNotFoundError()
 
     return compiler_installation_path
 
 
+@export
 def find() -> None:
-    build_system.cmd.compiler.host.get_info.cli.fetch_compiler_info(build_system.compiler.core.family.CompilerFamily.MSVC,
+    build_system.cmd.compiler.host.get_info.cli.fetch_compiler_info(CompilerFamily.MSVC,
                                                                     _find_no_arg, desc_compiler_info='location',
                                                                     help_path_meaning='installation')
