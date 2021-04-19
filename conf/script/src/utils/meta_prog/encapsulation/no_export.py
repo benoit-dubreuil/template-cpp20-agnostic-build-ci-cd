56b67a6b0b483be676b407f6f6e71e5b974cfb49
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 10:55:35 -0400

    Added TODO

--- conf/script/src/utils/meta_prog/encapsulation/no_export.py
+++ conf/script/src/utils/meta_prog/encapsulation/no_export.py
@@ -5,6 +5,7 @@ from utils.meta_prog.introspection import *
 __all__: TAlias_Macro_All = ['no_export']
 
 
+# TODO : Do not empty __all__. Create an empty if non-existant, else assure that func is absent
 def no_export(func):
     module = inspect.getmodule(func)
     module_api = TAlias_Macro_All()
