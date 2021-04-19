5d82a7297f220498e634ac275ab620dfec5a893e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-19 10:28:31 -0400

    Removed now useless func

--- conf/script/src/utils/meta_prog/introspection/attr_manip.py
+++ conf/script/src/utils/meta_prog/introspection/attr_manip.py
@@ -2,39 +2,21 @@ from typing import *
 
 from macro import *
 
-__all__: TAlias_Macro_All = ['get_or_create_attr', 'get_or_create_optional_attr']
+__all__: TAlias_Macro_All = ['get_or_create_attr']
 
 T_Attr_Val = TypeVar('T_Attr_Val')
+TAlias_Attr_Val_Generator = Callable[[], T_Attr_Val]
 
 
-def get_or_create_attr(obj, attr: str, default_val: Optional[T_Attr_Val] = None) -> T_Attr_Val:
+def get_or_create_attr(obj,
+                       attr: str,
+                       default_val_generator: TAlias_Attr_Val_Generator = lambda: T_Attr_Val()) -> T_Attr_Val:
     attr_val: T_Attr_Val
 
     assert hasattr(obj, Macro.DICT)
 
     if not hasattr(obj, attr):
-        non_none_default_val: T_Attr_Val
-
-        if default_val is None:
-            non_none_default_val = T_Attr_Val()
-        else:
-            non_none_default_val = default_val
-
-        attr_val = non_none_default_val
-        setattr(obj, attr, attr_val)
-    else:
-        attr_val = getattr(obj, attr)
-
-    return attr_val
-
-
-def get_or_create_optional_attr(obj, attr: str, default_val: Optional[T_Attr_Val] = None) -> Optional[T_Attr_Val]:
-    attr_val: Optional[T_Attr_Val]
-
-    assert hasattr(obj, Macro.DICT)
-
-    if not hasattr(obj, attr):
-        attr_val = default_val
+        attr_val = default_val_generator()
         setattr(obj, attr, attr_val)
     else:
         attr_val = getattr(obj, attr)
