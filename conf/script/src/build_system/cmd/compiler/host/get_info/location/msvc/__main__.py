842bfc9a37e4794f1481565b3449b5a39169ec25
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-20 10:15:45 -0400

    Fixed imports

--- conf/script/src/build_system/cmd/compiler/host/get_info/location/msvc/__main__.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/location/msvc/__main__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import build_system.cmd.compiler.host.get_info.location.msvc.cli
-import ext.cli.main
+from . import *
+from ext.cli import *
 
 
 def main():
-    build_system.cmd.compiler.host.get_info.location.msvc.cli.cli_find_msvc_location()
+    cli_find_msvc_location()
 
 
-ext.cli.main.wrap_main(main)
+wrap_main(main)
