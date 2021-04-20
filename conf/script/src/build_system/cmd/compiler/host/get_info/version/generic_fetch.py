1e0e3b763333546cb1fb7a1e3773d7375586e8f7
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-20 10:23:37 -0400

    Integrated imports

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/generic_fetch.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/generic_fetch.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from typing import AnyStr, Callable
 
-import build_system.compiler.core.version
-import ext.cmd_integrity
-import ext.error.core.cls_def
+from build_system.compiler import *
+from ext.cmd_integrity import *
+from ext.error import *
 
 from ext.meta_prog.encapsulation import *
 
@@ -15,15 +15,15 @@ def assure_path_integrity(compiler_path: Path) -> None:
     :param compiler_path: The path to the compiler executable file. It must not be a directory.
     """
     if not ext.cmd_integrity.cmd_exists(str(compiler_path)):
-        raise ext.error.core.cls_def.CompilerNotFoundError()
+        raise CompilerNotFoundError()
 
 
 @export
-def interpret_fetched_version(compiler_version_str: AnyStr) -> build_system.compiler.core.version.CompilerVersion:
-    return build_system.compiler.core.version.CompilerVersion.create_from_str(compiler_version_str.strip())
+def interpret_fetched_version(compiler_version_str: AnyStr) -> CompilerVersion:
+    return CompilerVersion.create_from_str(compiler_version_str.strip())
 
 
 @export
-def fetch(compiler: Path, fetch_compiler_version_func: Callable[[Path], AnyStr]) -> build_system.compiler.core.version.CompilerVersion:
+def fetch(compiler: Path, fetch_compiler_version_func: Callable[[Path], AnyStr]) -> CompilerVersion:
     compiler_version_str: AnyStr = fetch_compiler_version_func(compiler)
     return interpret_fetched_version(compiler_version_str)
