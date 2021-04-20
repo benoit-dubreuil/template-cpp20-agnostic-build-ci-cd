a97a69fa21d786d85aba6463a43f43825a969d65
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-20 10:38:26 -0400

    Export gcc cli

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/gcc/cli.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/gcc/cli.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
-import build_system.cmd.compiler.host.get_info.version.gnu.cli
-import build_system.compiler.core.family
+from ..gnu import *
+from build_system.compiler import *
+
+from ext.meta_prog.encapsulation import *
 
 
+@export
 def cli_fetch_gcc_version() -> None:
-    build_system.cmd.compiler.host.get_info.version.gnu.cli.cli_fetch_gnu_version(compiler_family=build_system.compiler.core.family.CompilerFamily.GCC)
+    cli_fetch_gnu_version(compiler_family=CompilerFamily.GCC)
