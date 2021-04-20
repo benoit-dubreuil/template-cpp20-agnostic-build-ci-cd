e3bfbf90e9ab4c428dc5e54fe1210e6cb89db977
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-20 10:27:19 -0400

    Export gnu version impl

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/gnu/impl.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/gnu/impl.py
@@ -2,12 +2,14 @@ import subprocess
 from pathlib import Path
 from typing import AnyStr
 
-import build_system.cmd.compiler.host.get_info.version.generic_fetch
-import build_system.compiler.core.version
+from ..generic_fetch import *
+from build_system.compiler import *
+
+from ext.meta_prog.encapsulation import *
 
 
 def _fetch_raw(compiler: Path) -> AnyStr:
-    build_system.cmd.compiler.host.get_info.version.generic_fetch.verify_compiler_path(compiler)
+    verify_compiler_path(compiler)
 
     result: subprocess.CompletedProcess = subprocess.run(
         [compiler, '-dumpversion'], capture_output=True, text=True, check=True
@@ -16,5 +18,6 @@ def _fetch_raw(compiler: Path) -> AnyStr:
     return result.stdout
 
 
-def fetch_version(compiler: Path) -> build_system.compiler.core.version.CompilerVersion:
-    return build_system.cmd.compiler.host.get_info.version.generic_fetch.fetch_compiler_version(compiler, _fetch_raw)
+@export
+def fetch_version(compiler: Path) -> CompilerVersion:
+    return fetch_compiler_version(compiler, _fetch_raw)
