eb0e6d18313237e8202d791546b6dc22a88f531b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-20 10:21:27 -0400

    Export generic fetch

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/generic_fetch.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/generic_fetch.py
@@ -5,7 +5,10 @@ import build_system.compiler.core.version
 import ext.cmd_integrity
 import ext.error.core.cls_def
 
+from ext.meta_prog.encapsulation import *
 
+
+@export
 def assure_path_integrity(compiler_path: Path) -> None:
     """Assures the integrity of the supplied :param:`compiler_path`
 
@@ -15,10 +18,12 @@ def assure_path_integrity(compiler_path: Path) -> None:
         raise ext.error.core.cls_def.CompilerNotFoundError()
 
 
+@export
 def interpret_fetched_version(compiler_version_str: AnyStr) -> build_system.compiler.core.version.CompilerVersion:
     return build_system.compiler.core.version.CompilerVersion.create_from_str(compiler_version_str.strip())
 
 
+@export
 def fetch(compiler: Path, fetch_compiler_version_func: Callable[[Path], AnyStr]) -> build_system.compiler.core.version.CompilerVersion:
     compiler_version_str: AnyStr = fetch_compiler_version_func(compiler)
     return interpret_fetched_version(compiler_version_str)
