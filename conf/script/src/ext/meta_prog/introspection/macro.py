fe69005ed977f11fbb35bed2029036fbdba81302
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-23 18:11:09 -0400

    Decorated public interfaces with `@final`

--- conf/script/src/ext/meta_prog/introspection/macro.py
+++ conf/script/src/ext/meta_prog/introspection/macro.py
@@ -1,18 +1,20 @@
 __all__ = ['Macro', 'TAlias_Macro_All']
 
 from enum import Enum, auto, unique
-from typing import Final
+from typing import Final, final
 
 TAlias_Macro_All = list[str]
 
 
 class AutoMacroFromName(str, Enum):
 
+    @final
     def _generate_next_value_(name: str, start, count, last_values) -> str:
         __AFFIX: Final[str] = '__'
         return __AFFIX + name.lower() + __AFFIX
 
 
+@final
 @unique
 class Macro(AutoMacroFromName):
     ALL = auto()
