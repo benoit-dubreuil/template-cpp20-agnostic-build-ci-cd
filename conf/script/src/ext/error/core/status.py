6df23b67466e500ed0b72a8fcf491a115a6e2cd1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 11:08:27 -0400

    Removed `@export` usage for module

--- conf/script/src/ext/error/core/status.py
+++ conf/script/src/ext/error/core/status.py
@@ -1,47 +1,47 @@
-import abc
-import enum
+__all__ = ['ErrorStatus',
+           'EncodedErrorMixin']
+
+from abc import abstractmethod
+from enum import IntEnum, auto, unique
 
 from .meta import *
-from ...meta_prog.encapsulation import *
 
 
-@enum.unique
-@export
-class ErrorStatus(enum.IntEnum):
+@unique
+class ErrorStatus(IntEnum):
     SUCCESS = 0
     UNSUPPORTED = 1
 
     ARG_PARSER = 2
 
-    UNKNOWN_PARSED_ARG = enum.auto()
-    EMPTY_PARSED_ARG = enum.auto()
+    UNKNOWN_PARSED_ARG = auto()
+    EMPTY_PARSED_ARG = auto()
 
-    ROOT_DIR_NOT_FOUND = enum.auto()
-    BUILD_DIR_NOT_FOUND = enum.auto()
-    BUILD_DIR_NOT_DIR = enum.auto()
-    BUILD_DIR_NOT_EMPTY = enum.auto()
+    ROOT_DIR_NOT_FOUND = auto()
+    BUILD_DIR_NOT_FOUND = auto()
+    BUILD_DIR_NOT_DIR = auto()
+    BUILD_DIR_NOT_EMPTY = auto()
 
-    CONF_DIR_NOT_FOUND = enum.auto()
-    CONF_BUILD_SYSTEM_DIR_NOT_FOUND = enum.auto()
+    CONF_DIR_NOT_FOUND = auto()
+    CONF_BUILD_SYSTEM_DIR_NOT_FOUND = auto()
 
-    MESON_MAIN_FILE_NOT_FOUND = enum.auto()
-    MESON_MACHINE_FILES_DIR_NOT_FOUND = enum.auto()
+    MESON_MAIN_FILE_NOT_FOUND = auto()
+    MESON_MACHINE_FILES_DIR_NOT_FOUND = auto()
 
-    COMPILER_REQS_NOT_FOUND = enum.auto()
-    COMPILER_NOT_FOUND = enum.auto()
-    NO_SUPPORTED_COMPILERS_AVAILABLE = enum.auto()
+    COMPILER_REQS_NOT_FOUND = auto()
+    COMPILER_NOT_FOUND = auto()
+    NO_SUPPORTED_COMPILERS_AVAILABLE = auto()
 
-    MSVC_COMPILER_VCVARS_DIR_NOT_FOUND = enum.auto()
-    MSVC_COMPILER_VCVARS_BATCH_FILE_NOT_FOUND = enum.auto()
+    MSVC_COMPILER_VCVARS_DIR_NOT_FOUND = auto()
+    MSVC_COMPILER_VCVARS_BATCH_FILE_NOT_FOUND = auto()
 
 
-@export
 class EncodedErrorMixin(Exception, metaclass=ErrorMeta):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     @staticmethod
-    @abc.abstractmethod
+    @abstractmethod
     def get_error_status() -> ErrorStatus:
         raise NotImplementedError()
