121c6f12ae0da75abff92a2b952bc9140cd0e2c3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 16:41:42 -0400

    Removed `@export` usage for module

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/generic_fetch.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/generic_fetch.py
@@ -1,3 +1,7 @@
+__all__ = ['verify_compiler_path',
+           'interpret_fetched_compiler_version',
+           'fetch_compiler_version']
+
 from pathlib import Path
 from typing import AnyStr, Callable
 
@@ -5,25 +9,20 @@ from build_system.compiler import *
 from ext.cmd_integrity import *
 from ext.error import *
 
-from ext.meta_prog.encapsulation import *
 
-
-@export
 def verify_compiler_path(compiler_path: Path) -> None:
     """Assures the integrity of the supplied :param:`compiler_path`
 
     :param compiler_path: The path to the compiler executable file. It must not be a directory.
     """
-    if not ext.cmd_integrity.cmd_exists(str(compiler_path)):
+    if not cmd_exists(str(compiler_path)):
         raise CompilerNotFoundError()
 
 
-@export
 def interpret_fetched_compiler_version(compiler_version_str: AnyStr) -> CompilerVersion:
     return CompilerVersion.create_from_str(compiler_version_str.strip())
 
 
-@export
 def fetch_compiler_version(compiler: Path, fetch_compiler_version_func: Callable[[Path], AnyStr]) -> CompilerVersion:
     compiler_version_str: AnyStr = fetch_compiler_version_func(compiler)
     return interpret_fetched_compiler_version(compiler_version_str)
