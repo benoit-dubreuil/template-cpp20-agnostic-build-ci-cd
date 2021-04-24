80e4a4b44a135604ac5fdf1f44443271daacdba1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 17:27:47 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/hierarchy/create_targets_build_dirs/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/create_targets_build_dirs/impl.py
@@ -1,33 +1,35 @@
+__all__ = ['create_targets_build_dirs']
+
 from pathlib import Path
 from typing import Optional
 
 from build_system.build_target import *
-import build_system.cmd.hierarchy.assure_arg_integrity
-import build_system.compiler.installed_instance
-import ext.error.core.cls_def
-import ext.more_path
+from build_system.compiler import *
+from ext.error import *
+from ext.more_path import *
+from ..assure_arg_integrity import *
 
 
 def create_targets_build_dirs(build_dir: Optional[Path] = None,
-                              compiler_instances: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None) \
+                              compiler_instances: Optional[list[CompilerInstance]] = None) \
         -> list[CompilerInstanceTargets]:
-    build_dir = build_system.cmd.hierarchy.assure_arg_integrity.get_verified_build_dir(unverified_build_dir=build_dir)
+    build_dir = get_verified_build_dir(unverified_build_dir=build_dir)
     _assure_build_dir_is_empty(build_dir)
 
     return _unchecked_create_targets_build_dirs(build_dir, compiler_instances=compiler_instances)
 
 
 def _assure_build_dir_is_empty(build_dir):
-    if not ext.more_path.is_dir_empty(build_dir):
-        raise ext.error.core.cls_def.BuildDirNotEmptyError()
+    if not is_dir_empty(build_dir):
+        raise BuildDirNotEmptyError()
 
 
 def _unchecked_create_targets_build_dirs(build_dir: Path,
-                                         compiler_instances: Optional[list[build_system.compiler.installed_instance.CompilerInstance]] = None) \
+                                         compiler_instances: Optional[list[CompilerInstance]] = None) \
         -> list[CompilerInstanceTargets]:
-    from build_system.cmd.hierarchy.create_targets_build_dirs.target_dir_name_generation import checked_generate_targets
-    from build_system.cmd.hierarchy.create_targets_build_dirs.target_dir_creation import create_targets_build_dirs
-    from build_system.cmd.hierarchy.create_targets_build_dirs.target_script_dir_creation import create_targets_script_dirs
+    from .target_dir_name_generation import checked_generate_targets
+    from .target_dir_creation import create_targets_build_dirs
+    from .target_script_dir_creation import create_targets_script_dirs
 
     targets = checked_generate_targets(compiler_instances=compiler_instances)
 
