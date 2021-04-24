b6af4212f95c3c7d3e5e479fdbf4b57ba460bf03
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 16:59:31 -0400

    Optimized imports

--- conf/script/src/build_system/cmd/hierarchy/clean_build_dir/__main__.py
+++ conf/script/src/build_system/cmd/hierarchy/clean_build_dir/__main__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import build_system.cmd.hierarchy.clean_build_dir.cli
-import ext.cli.main
+from ext.cli import *
+from . import *
 
 
 def main():
-    build_system.cmd.hierarchy.clean_build_dir.cli.cli_clean_build_dir()
+    cli_clean_build_dir()
 
 
-ext.cli.main.wrap_main(main)
+wrap_main(main)
