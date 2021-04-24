c3ad7c279027858a9e92cc21e879df5579b8c0f9
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 16:43:17 -0400

    Removed `@export` usage for module

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/fetch_by_criteria.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/fetch_by_criteria.py
@@ -1,17 +1,12 @@
-__all__ = ['fetch_func_by_compiler_family']
+__all__ = ['fetch_by_compiler_family']
 
 from . import *
 from build_system.compiler import *
 
-fetch_func_by_compiler_family = {build_system.compiler.family.CompilerFamily.MSVC: build_system.cmd.compiler.host.get_info.version.msvc.fetch_version,
-                                 build_system.compiler.family.CompilerFamily.CLANG: build_system.cmd.compiler.host.get_info.version.clang.fetch_version,
-                                 build_system.compiler.family.CompilerFamily.GCC: build_system.cmd.compiler.host.get_info.version.gcc.fetch_version}
-
-fetch_func_by_compiler_family = {CompilerFamily.MSVC: fetch_msvc_version,
-                                 CompilerFamily.CLANG: fetch_clang_version,
-                                 CompilerFamily.GCC: fetch_gcc_version}
+_fetch_func_by_compiler_family = {CompilerFamily.MSVC: fetch_msvc_version,
+                                  CompilerFamily.CLANG: fetch_clang_version,
+                                  CompilerFamily.GCC: fetch_gcc_version}
 
 
-@export
 def fetch_by_compiler_family(compiler_family: CompilerFamily) -> CompilerVersion:
-    return fetch_func_by_compiler_family[compiler_family]()
+    return _fetch_func_by_compiler_family[compiler_family]()
