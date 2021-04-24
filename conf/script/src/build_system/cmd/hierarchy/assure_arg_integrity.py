b150803bdbdd7a68677d5757222e87e3efc794c9
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 18:14:34 -0400

    Optimized imports

--- conf/script/src/build_system/cmd/hierarchy/assure_arg_integrity.py
+++ conf/script/src/build_system/cmd/hierarchy/assure_arg_integrity.py
@@ -5,11 +5,11 @@ from pathlib import Path
 from typing import Optional
 
 from ext.error import *
+from .create_build_dir import *
+from .find_root_dir import *
 
 
 def get_verified_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
-    from build_system.cmd.hierarchy.find_root_dir import find_root_dir
-
     if unverified_root_dir is None:
         unverified_root_dir = find_root_dir()
     else:
@@ -20,8 +20,6 @@ def get_verified_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
 
 
 def get_verified_build_dir(unverified_build_dir: Optional[Path] = None) -> Path:
-    from build_system.cmd.hierarchy.create_build_dir import create_build_dir
-
     if unverified_build_dir is None:
         unverified_build_dir = create_build_dir()
     else:
