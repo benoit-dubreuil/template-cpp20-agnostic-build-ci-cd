d6a331715f2e2b0565c717b43749a29f6bbd445d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 16:44:13 -0400

    Renamed func

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/fetch_by_criteria.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/fetch_by_criteria.py
@@ -1,12 +1,12 @@
 __all__ = ['fetch_compiler_version_by_family']
 
-from . import *
 from build_system.compiler import *
+from . import *
 
-_fetch_func_by_compiler_family = {CompilerFamily.MSVC: fetch_msvc_version,
-                                  CompilerFamily.CLANG: fetch_clang_version,
-                                  CompilerFamily.GCC: fetch_gcc_version}
+_fetch_func = {CompilerFamily.MSVC: fetch_msvc_version,
+               CompilerFamily.CLANG: fetch_clang_version,
+               CompilerFamily.GCC: fetch_gcc_version}
 
 
 def fetch_compiler_version_by_family(compiler_family: CompilerFamily) -> CompilerVersion:
-    return _fetch_func_by_compiler_family[compiler_family]()
+    return _fetch_func[compiler_family]()
