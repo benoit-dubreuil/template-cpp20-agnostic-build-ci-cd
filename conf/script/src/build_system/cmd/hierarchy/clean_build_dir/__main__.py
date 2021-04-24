71b6e116b540d58c093d570f8362ce45042c1011
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 16:57:29 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/hierarchy/clean_build_dir/__main__.py
+++ conf/script/src/build_system/cmd/hierarchy/clean_build_dir/__main__.py
@@ -5,7 +5,7 @@ import ext.cli.main
 
 
 def main():
-    build_system.cmd.hierarchy.clean_build_dir.cli.clean_build_dir()
+    build_system.cmd.hierarchy.clean_build_dir.cli.cli_clean_build_dir()
 
 
 ext.cli.main.wrap_main(main)
--- conf/script/src/build_system/cmd/hierarchy/clean_build_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/clean_build_dir/cli.py
@@ -1,24 +1,25 @@
+__all__ = ['cli_clean_build_dir']
+
 import argparse
 
 import colorama
 
-import build_system.cmd.hierarchy.consts
-import build_system.cmd.hierarchy.find_build_dir
-import ext.cli.arg_parsing
-import ext.cli.try_cmd
+from build_system.cmd.hierarchy.consts import *
+from ext.cli import *
 from ext.more_typing import AnyPath
+from .impl import *
 
 
-def clean_build_dir():
+def cli_clean_build_dir():
     arg_parser = argparse.ArgumentParser(
-        description=f"Cleans the project's {colorama.Fore.LIGHTBLACK_EX}{build_system.cmd.hierarchy.consts.BUILD_DIR_NAME}{colorama.Style.RESET_ALL} "
+        description=f"Cleans the project's {colorama.Fore.LIGHTBLACK_EX}{BUILD_DIR_NAME}{colorama.Style.RESET_ALL} "
                     "folder, where the build system organizes into subfolders specific builds.")
-    ext.cli.arg_parsing.add_optional_path_arg(arg_parser, build_system.cmd.hierarchy.consts.ROOT_DIR_ARG, path_arg_help="The project's root directory")
+    add_optional_path_arg(arg_parser, ROOT_DIR_ARG, path_arg_help="The project's root directory")
 
-    root_dir: AnyPath = ext.cli.arg_parsing.parse_optional_path_arg(arg_parser, build_system.cmd.hierarchy.consts.ROOT_DIR_ARG)
+    root_dir: AnyPath = parse_optional_path_arg(arg_parser, ROOT_DIR_ARG)
     arg_parser.parse_args()
 
     def cli_cmd():
-        build_system.cmd.hierarchy.clean_build_dir.clean_build_dir(build_dir=root_dir)
+        clean_build_dir(build_dir=root_dir)
 
-    ext.cli.try_cmd.try_cmd_except_managed_errors(cli_cmd, arg_parser)
+    try_cmd_except_managed_errors(cli_cmd, arg_parser)
