1ed48ecaf63fbc193eec3de7dbf4c865f67b019b
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-23 20:26:59 -0400

    WIP : pass attr as arg

--- conf/script/src/ext/meta_prog/introspection/macro.py
+++ conf/script/src/ext/meta_prog/introspection/macro.py
@@ -1,7 +1,7 @@
 __all__ = ['Macro', 'TAlias_Macro_All']
 
 from enum import Enum, auto, unique
-from typing import Final, final
+from typing import Any, Final, final
 
 TAlias_Macro_All = list[str]
 
@@ -9,16 +9,24 @@ TAlias_Macro_All = list[str]
 class _AutoMacroFromName(str, Enum):
 
     @final
-    def _generate_next_value_(name: str, start, count, last_values) -> str:
+    def _generate_next_value_(name: str, start, count, last_values, *args) -> (str, ...):
         __AFFIX: Final[str] = '__'
-        return __AFFIX + name.lower() + __AFFIX
+        return __AFFIX + name.lower() + __AFFIX, *args
 
 
 @final
 @unique
 class Macro(_AutoMacroFromName):
-    ALL = auto()
+    ALL = auto(('TAlias_Macro_All', list[str]))
     DICT = auto()
     MAIN = auto()
     NAME = auto()
     QUALNAME = auto()
+
+    def __new__(cls, value, *args: tuple[str, Any]):
+        macro = str().__new__(cls, value)
+
+        for attr_name, attr_val in args:
+            setattr(macro, attr_name, attr_val)
+
+        return macro
