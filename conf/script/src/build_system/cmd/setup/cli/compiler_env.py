15f13005db8b9c3705a0ca447f88fd874e38f817
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 18:28:30 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/setup/cli/compiler_env.py
+++ conf/script/src/build_system/cmd/setup/cli/compiler_env.py
@@ -1,13 +1,13 @@
+__all__ = ['print_compiler_env_file']
+
 from typing import Final
 
 from build_system.build_target import *
+from .colorize import *
 
 
 def print_compiler_env_file(target: BuildTarget) -> None:
-    from build_system.cmd.setup.cli.colorize import colorize_label, colorize_path
-
     label: Final[str] = colorize_label(label='Compiler env') + ': '
-
     colorized_symlink = colorize_path(path_info=target.compiler_env_file)
 
     print(label, end=str())
