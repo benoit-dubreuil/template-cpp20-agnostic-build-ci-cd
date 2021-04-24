570595ae999554308969fdc105e8e12df29e6a8e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 18:30:26 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/setup/cli/meson.py
+++ conf/script/src/build_system/cmd/setup/cli/meson.py
@@ -1,14 +1,15 @@
+__all__ = ['print_meson_main_file',
+           'print_meson_cmd']
+
 from pathlib import Path
 from typing import Final
 
-from build_system.cmd.hierarchy.consts import BUILD_SYSTEM_NAME
+from .colorize import *
+from ...hierarchy.consts import *
 
 
 def print_meson_main_file(meson_main_file: Path) -> None:
-    from build_system.cmd.setup.cli.colorize import colorize_label, colorize_path
-
     meson_cmd_label: Final[str] = colorize_label(label=f'{BUILD_SYSTEM_NAME.capitalize()} main file') + ': '
-
     colorized_meson_main_file = colorize_path(path_info=meson_main_file)
 
     print(meson_cmd_label, end=str())
@@ -16,8 +17,6 @@ def print_meson_main_file(meson_main_file: Path) -> None:
 
 
 def print_meson_cmd(meson_cli_args: list[str]) -> None:
-    from build_system.cmd.setup.cli.colorize import colorize_label, colorize_path
-
     meson_cmd_label: Final[str] = colorize_label(label=f'{BUILD_SYSTEM_NAME.capitalize()} cmd') + ': '
 
     meson_cli_cmd_and_args = [BUILD_SYSTEM_NAME, *meson_cli_args]
