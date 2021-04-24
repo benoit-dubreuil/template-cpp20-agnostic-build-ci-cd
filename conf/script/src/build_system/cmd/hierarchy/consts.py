0f41db453968e57f70e0cc0d8a5d1777fa859c3e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 16:50:41 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/hierarchy/consts.py
+++ conf/script/src/build_system/cmd/hierarchy/consts.py
@@ -1,10 +1,26 @@
-import encodings
-import typing
+__all__ = ['DIR_STR',
+           'ROOT_STR',
+           'BUILD_SYSTEM_NAME',
+           'BUILD_SYSTEM_CONF_FILE_NAME',
+           'BUILD_DIR_NAME',
+           'TARGET_SCRIPT_DIR_NAME',
+           'TARGET_SCRIPT_EXPORT_SHELL_ENV_NAME',
+           'TARGET_SCRIPT_COMPILER_ENV_NAME',
+           'CONF_DIR_NAME',
+           'CONF_BUILD_SYSTEM_DIR_NAME',
+           'MESON_MACHINE_FILES_DIR_NAME',
+           'ROOT_DIR_ARG',
+           'BUILD_DIR_ARG',
+           'BUILD_DIR_PERMISSIONS',
+           'UTF_8']
 
-import ext.cli.arg
+from encodings import utf_8
+from typing import Final
 
-_TAlias_Const = typing.Final[str]
-_TAlias_CLI_Const = typing.Final[ext.cli.arg.CLIArg]
+from ext.cli import *
+
+_TAlias_Const = Final[str]
+_TAlias_CLI_Const = Final[CLIArg]
 
 DIR_STR: _TAlias_Const = 'dir'
 ROOT_STR: _TAlias_Const = 'root'
@@ -21,8 +37,8 @@ CONF_DIR_NAME: _TAlias_Const = 'conf'
 CONF_BUILD_SYSTEM_DIR_NAME: _TAlias_Const = f'{BUILD_DIR_NAME}-system'
 MESON_MACHINE_FILES_DIR_NAME: _TAlias_Const = 'machine'
 
-ROOT_DIR_ARG: _TAlias_CLI_Const = ext.cli.arg.CLIArg(f'{ROOT_STR}{DIR_STR}')
-BUILD_DIR_ARG: _TAlias_CLI_Const = ext.cli.arg.CLIArg(f'{BUILD_DIR_NAME}{DIR_STR}')
+ROOT_DIR_ARG: _TAlias_CLI_Const = CLIArg(f'{ROOT_STR}{DIR_STR}')
+BUILD_DIR_ARG: _TAlias_CLI_Const = CLIArg(f'{BUILD_DIR_NAME}{DIR_STR}')
 
-BUILD_DIR_PERMISSIONS: typing.Final[int] = 0o770
-UTF_8: _TAlias_Const = encodings.utf_8.getregentry().name
+BUILD_DIR_PERMISSIONS: Final[int] = 0o770
+UTF_8: _TAlias_Const = utf_8.getregentry().name
