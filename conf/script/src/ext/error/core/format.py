fe654947e3c1df0ae2e374c75619124617945507
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 11:04:17 -0400

    Removed `@export` usage for module

--- conf/script/src/ext/error/core/format.py
+++ conf/script/src/ext/error/core/format.py
@@ -1,23 +1,25 @@
-import abc
+__all__ = ['format_error_msg',
+           'format_success_msg',
+           'BaseFormattedErrorMixin',
+           'FormattedErrorMixin',
+           'FormattedSuccessMixin']
+
+from abc import abstractmethod
 from typing import AnyStr
 
 import colorama
 
 from .meta import *
-from ...meta_prog.encapsulation import *
 
 
-@export
 def format_error_msg(message: AnyStr) -> AnyStr:
     return colorama.Style.BRIGHT + colorama.Fore.RED + message + colorama.Style.RESET_ALL
 
 
-@export
 def format_success_msg(message: AnyStr) -> AnyStr:
     return colorama.Style.BRIGHT + colorama.Fore.GREEN + message + colorama.Style.RESET_ALL
 
 
-@export
 class BaseFormattedErrorMixin(Exception, metaclass=ErrorMeta):
 
     def __init__(self, *args, **kwargs):
@@ -28,12 +30,11 @@ class BaseFormattedErrorMixin(Exception, metaclass=ErrorMeta):
         return self._format_msg(error_msg)
 
     @staticmethod
-    @abc.abstractmethod
+    @abstractmethod
     def _format_msg(message: str) -> str:
         raise NotImplementedError()
 
 
-@export
 class FormattedErrorMixin(BaseFormattedErrorMixin):
 
     def __init__(self, *args, **kwargs):
@@ -44,7 +45,6 @@ class FormattedErrorMixin(BaseFormattedErrorMixin):
         return format_error_msg(message)
 
 
-@export
 class FormattedSuccessMixin(BaseFormattedErrorMixin):
 
     def __init__(self, *args, **kwargs):
