5784ba3cf73389ace8820e740c4539cfb5099db9
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 10:55:32 -0400

    Removed `@export` usage for module

--- conf/script/src/ext/cli/main.py
+++ conf/script/src/ext/cli/main.py
@@ -1,21 +1,20 @@
+__all__ = ['init',
+           'deinit',
+           'wrap_main']
+
 from typing import Callable
 
 import colorama
 
-from ..meta_prog.encapsulation import *
 
-
-@export
 def init():
     colorama.init()
 
 
-@export
 def deinit():
     colorama.deinit()
 
 
-@export
 def wrap_main(main_func: Callable):
     init()
     main_func()
