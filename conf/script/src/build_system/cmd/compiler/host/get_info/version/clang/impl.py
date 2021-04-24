e8122c1b3e51e9443fb4d4889fa943d8ce31479c
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 15:42:59 -0400

    Removed `@export` usage for module

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/clang/impl.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/clang/impl.py
@@ -1,9 +1,8 @@
-from ..gnu import *
+__all__ = ['fetch_clang_version']
+
 from build_system.compiler import *
+from ..gnu import *
 
-from ext.meta_prog.encapsulation import *
 
-
-@export
 def fetch_clang_version() -> CompilerVersion:
     return fetch_gnu_version(CompilerFamily.CLANG.value)
