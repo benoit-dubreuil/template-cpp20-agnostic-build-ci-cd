cd6ce87c532b08c52e576f56b82e80e738109d1a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 18:12:13 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/find_root_dir/cli.py
@@ -1,21 +1,24 @@
+__all__ = ['cli_find_root_dir']
+
 import argparse
 
 import colorama
 
-import build_system.cmd.hierarchy.find_root_dir
-import ext.cli.try_cmd
+from ext.cli import *
+from .impl import *
+from ..consts import *
 
 
 def cli_find_root_dir():
     arg_parser = argparse.ArgumentParser(
         description="Finds the project's root folder, where the "
-                    f"'{colorama.Fore.LIGHTBLACK_EX}{build_system.cmd.hierarchy.find_root_dir.BUILD_SYSTEM_CONF_FILE_NAME}{colorama.Style.RESET_ALL}' folder is. "
+                    f"'{colorama.Fore.LIGHTBLACK_EX}{BUILD_SYSTEM_CONF_FILE_NAME}{colorama.Style.RESET_ALL}' folder is. "
                     'It searches recursively parent folders upwards.')
 
     arg_parser.parse_args()
 
     def cli_cmd():
-        project_root = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
+        project_root = find_root_dir()
         print(project_root, end=str())
 
-    ext.cli.try_cmd.try_cmd_except_managed_errors(cli_cmd, arg_parser)
+    try_cmd_except_managed_errors(cli_cmd, arg_parser)
