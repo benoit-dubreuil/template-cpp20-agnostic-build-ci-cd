41fce80b8da77877c7b20cf39b9af3c964193cee
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 17:30:02 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/hierarchy/create_targets_build_dirs/cli.py
+++ conf/script/src/build_system/cmd/hierarchy/create_targets_build_dirs/cli.py
@@ -1,26 +1,27 @@
+__all__ = ['cli_create_target_build_dirs']
+
 import argparse
 
 import colorama
 
-import build_system.cmd.hierarchy.consts
-import build_system.cmd.hierarchy.create_targets_build_dirs
-import ext.cli.arg_parsing
-import ext.cli.try_cmd
-from ext.more_typing import AnyPath
+from ext.cli import *
+from ext.more_typing import *
+from .impl import *
+from ..consts import *
 
 
 def cli_create_target_build_dirs():
     arg_parser = argparse.ArgumentParser(description="Creates the project's target build directories inside the "
-                                                     f"'{colorama.Fore.LIGHTBLACK_EX}{build_system.cmd.hierarchy.consts.BUILD_DIR_NAME}{colorama.Style.RESET_ALL}' folder.")
+                                                     f"'{colorama.Fore.LIGHTBLACK_EX}{BUILD_DIR_NAME}{colorama.Style.RESET_ALL}' folder.")
 
-    ext.cli.arg_parsing.add_optional_path_arg(arg_parser=arg_parser,
-                                              path_arg=build_system.cmd.hierarchy.consts.BUILD_DIR_ARG,
-                                              path_arg_help=f"The project's {build_system.cmd.hierarchy.consts.BUILD_DIR_NAME} directory")
+    add_optional_path_arg(arg_parser=arg_parser,
+                          path_arg=BUILD_DIR_ARG,
+                          path_arg_help=f"The project's {BUILD_DIR_NAME} directory")
 
-    build_dir: AnyPath = ext.cli.arg_parsing.parse_optional_path_arg(arg_parser, build_system.cmd.hierarchy.consts.BUILD_DIR_ARG)
+    build_dir: AnyPath = parse_optional_path_arg(arg_parser, BUILD_DIR_ARG)
     arg_parser.parse_args()
 
     def cli_cmd():
-        build_system.cmd.hierarchy.create_targets_build_dirs.create_targets_build_dirs(build_dir)
+        create_targets_build_dirs(build_dir)
 
-    ext.cli.try_cmd.try_cmd_except_managed_errors(cli_cmd, arg_parser)
+    try_cmd_except_managed_errors(cli_cmd, arg_parser)
