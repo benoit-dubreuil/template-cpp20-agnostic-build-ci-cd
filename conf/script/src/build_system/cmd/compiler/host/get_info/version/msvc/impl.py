cc8c49af1708d65434190929385c929bec87cce8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 16:40:04 -0400

    Removed `@export` usage for module

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/msvc/impl.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/msvc/impl.py
@@ -1,23 +1,22 @@
+__all__ = ['fetch_msvc_version']
+
 from pathlib import Path
 from typing import Final, Optional
 
 import vswhere
 
-from ...location.msvc import *
 from build_system.compiler import *
+from ...location.msvc import *
 
-from ext.meta_prog.encapsulation import *
-
-PROP_VERSION: Final[str] = 'installationVersion'
+_PROP_VERSION: Final[str] = 'installationVersion'
 
 
-@export
 def fetch_msvc_version(compiler_installation_path: Optional[Path] = None) -> Optional[CompilerVersion]:
     interpreted_compiler_version: Optional[CompilerVersion] = None
     compiler_installation_path = find_msvc_location(compiler_installation_path)
 
     if compiler_installation_path is not None:
-        fetched_compiler_version: Optional[str] = vswhere.find_first(prop=PROP_VERSION, path=compiler_installation_path)
+        fetched_compiler_version: Optional[str] = vswhere.find_first(prop=_PROP_VERSION, path=compiler_installation_path)
 
         if fetched_compiler_version is not None:
             fetched_compiler_version = fetched_compiler_version.strip()
