84c25c7a2ba45078e1fd8a1aeb4797fa55bfd682
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 17:59:37 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/hierarchy/find_conf_dir/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/find_conf_dir/impl.py
@@ -1,29 +1,30 @@
-from pathlib import Path
+__all__ = ['get_conf_dir_path_relative_to_root_dir',
+           'find_conf_dir']
+
 from typing import Optional
 
-import build_system.cmd.hierarchy.consts
-import ext.error.core.cls_def
-import ext.error.utils.try_external_errors
+from ext.error import *
+from ext.error.utils import *
+from ..consts import *
+from ..find_root_dir import *
 
 
 def get_conf_dir_path_relative_to_root_dir(root_dir: Optional[Path] = None) -> Path:
-    import build_system.cmd.hierarchy.find_root_dir
-
     if root_dir is None:
-        root_dir = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
+        root_dir = find_root_dir()
 
-    return root_dir / build_system.cmd.hierarchy.consts.CONF_DIR_NAME
+    return root_dir / CONF_DIR_NAME
 
 
 def find_conf_dir(root_dir: Optional[Path] = None) -> Path:
     conf_dir = get_conf_dir_path_relative_to_root_dir(root_dir=root_dir)
 
-    ext.error.utils.try_external_errors.try_manage_strict_path_resolving(path_to_resolve=conf_dir,
-                                                                         external_errors_to_manage={(Exception,): ext.error.core.cls_def.ConfDirNotFoundError})
+    try_manage_strict_path_resolving(path_to_resolve=conf_dir,
+                                     external_errors_to_manage={(Exception,): ConfDirNotFoundError})
 
     conf_dir = conf_dir.absolute()
 
     if not conf_dir.is_dir():
-        raise ext.error.core.cls_def.ConfDirNotFoundError()
+        raise ConfDirNotFoundError()
 
     return conf_dir
