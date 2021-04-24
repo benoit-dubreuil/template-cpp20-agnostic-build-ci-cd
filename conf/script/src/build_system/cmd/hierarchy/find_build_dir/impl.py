2a8b85be9cf0eea8e958ad1e4b2226bb49f55dd6
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 17:53:40 -0400

    Export public API through `__all__`

--- conf/script/src/build_system/cmd/hierarchy/find_build_dir/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/find_build_dir/impl.py
@@ -1,29 +1,31 @@
+__all__ = ['get_build_dir_path_relative_to_root_dir',
+           'find_build_dir']
+
 from pathlib import Path
 from typing import Optional
 
-import build_system.cmd.hierarchy.consts
-import ext.error.core.cls_def
-import ext.error.utils.try_external_errors
+from ext.error import *
+from ext.error.utils import *
+from ..consts import *
+from ..find_root_dir import *
 
 
 def get_build_dir_path_relative_to_root_dir(root_dir: Optional[Path] = None) -> Path:
-    import build_system.cmd.hierarchy.find_root_dir
-
     if root_dir is None:
-        root_dir = build_system.cmd.hierarchy.find_root_dir.find_root_dir()
+        root_dir = find_root_dir()
 
-    return root_dir / build_system.cmd.hierarchy.consts.BUILD_DIR_NAME
+    return root_dir / BUILD_DIR_NAME
 
 
 def find_build_dir(root_dir: Optional[Path] = None) -> Path:
     build_dir = get_build_dir_path_relative_to_root_dir(root_dir=root_dir)
 
-    ext.error.utils.try_external_errors.try_manage_strict_path_resolving(path_to_resolve=build_dir,
-                                                                         external_errors_to_manage={(Exception,): ext.error.core.cls_def.BuildDirNotFoundError})
+    try_manage_strict_path_resolving(path_to_resolve=build_dir,
+                                     external_errors_to_manage={(Exception,): BuildDirNotFoundError})
 
     build_dir = build_dir.absolute()
 
     if not build_dir.is_dir():
-        raise ext.error.core.cls_def.BuildDirNotFoundError()
+        raise BuildDirNotFoundError()
 
     return build_dir
