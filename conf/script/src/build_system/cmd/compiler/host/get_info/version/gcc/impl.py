c34071d63d8eb68a8f1476cf4af5bf2d826ca235
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 15:44:04 -0400

    Removed `@export` usage for module

--- conf/script/src/build_system/cmd/compiler/host/get_info/version/gcc/impl.py
+++ conf/script/src/build_system/cmd/compiler/host/get_info/version/gcc/impl.py
@@ -1,9 +1,8 @@
+__all__ = ['fetch_gcc_version']
+
 from build_system.compiler import *
 from ..gnu import *
 
-from ext.meta_prog.encapsulation import *
 
-
-@export
 def fetch_gcc_version() -> CompilerVersion:
     return fetch_gnu_version(CompilerFamily.GCC.value)
