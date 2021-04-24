7197083e6cb6048f876489863cc78ac1fc64246e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-24 11:09:53 -0400

    Removed `@export` usage for module

--- conf/script/src/ext/cmd_integrity.py
+++ conf/script/src/ext/cmd_integrity.py
@@ -1,16 +1,15 @@
+__all__ = ['cmd_exists',
+           'get_cmd_path']
+
 import shutil
 from pathlib import Path
 
-from .meta_prog.encapsulation import *
-
 
 # From https://stackoverflow.com/a/28909933/2924010
-@export
 def cmd_exists(cmd) -> bool:
     return shutil.which(cmd) is not None
 
 
-@export
 def get_cmd_path(cmd, dir_path=None) -> (Path, bool):
     cmd_path_str = shutil.which(cmd=cmd, path=dir_path)
     cmd_path = Path(cmd_path_str)
