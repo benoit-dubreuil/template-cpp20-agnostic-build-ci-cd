f90ba142446a2b97bb46b1e3b9461c0fa90d8e85
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-25 12:04:36 -0400

    Func `find_or_verify_build_dir` does not create the build dir anymore

--- conf/script/src/build_system/cmd/hierarchy/assure_arg_integrity.py
+++ conf/script/src/build_system/cmd/hierarchy/assure_arg_integrity.py
@@ -5,8 +5,8 @@ from pathlib import Path
 from typing import Optional
 
 from ext.error import *
-from .create_build_dir import *
 from .find_root_dir import *
+from .find_build_dir import *
 
 
 def find_or_verify_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
@@ -21,7 +21,7 @@ def find_or_verify_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
 
 def find_or_verify_build_dir(unverified_build_dir: Optional[Path] = None) -> Path:
     if unverified_build_dir is None:
-        unverified_build_dir = create_build_dir()
+        unverified_build_dir = find_build_dir()
     else:
         if not unverified_build_dir.exists():
             raise BuildDirNotFoundError()
