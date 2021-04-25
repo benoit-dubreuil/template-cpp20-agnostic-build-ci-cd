29de3f439bd297ac92995b7e28cbbb82009fe5ec
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-25 11:58:35 -0400

    Renamed func

--- conf/script/src/build_system/cmd/setup/_priv/setup_steps/step_get_root_dir.py
+++ conf/script/src/build_system/cmd/setup/_priv/setup_steps/step_get_root_dir.py
@@ -6,5 +6,5 @@ from typing import Optional
 from ....hierarchy import *
 
 
-def get_verified_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
+def get_root_dir(unverified_root_dir: Optional[Path] = None) -> Path:
     return find_or_verify_root_dir(unverified_root_dir=unverified_root_dir)
