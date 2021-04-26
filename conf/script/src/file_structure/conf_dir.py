f7ed9e295dec63e57fa8cb671f0ab70ebb521c47
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-26 11:53:46 -0400

    Extracted conf constants

--- conf/script/src/file_structure/conf_dir.py
+++ conf/script/src/file_structure/conf_dir.py
@@ -1,1 +1,11 @@
-__all__ = []
+__all__ = ['CONF_DIR_NAME',
+           'CONF_BUILD_SYSTEM_DIR_NAME',
+           'CONF_MESON_MACHINE_FILES_DIR_NAME']
+
+from ._type_alias import *
+from .build_dir import *
+from .build_system import *
+
+CONF_DIR_NAME: TAlias_Name = 'conf'
+CONF_BUILD_SYSTEM_DIR_NAME: TAlias_Name = f'{BUILD_DIR_NAME}-{SYSTEM_STR}'
+CONF_MESON_MACHINE_FILES_DIR_NAME: TAlias_Name = 'machine'
