cefd43e400f179c9b6ee70caac734bc631226167
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-26 11:53:14 -0400

    Add convivial constants

--- conf/script/src/file_structure/build_system.py
+++ conf/script/src/file_structure/build_system.py
@@ -1,10 +1,13 @@
-__all__ = ['BUILD_SYSTEM_NAME',
+__all__ = ['SYSTEM_STR',
+           'BUILD_SYSTEM_NAME',
            'BUILD_SYSTEM_CONF_FILE_EXTENSION',
            'BUILD_SYSTEM_CONF_FILE_NAME']
 
 from ._type_alias import *
+from .build_dir import *
 
+SYSTEM_STR: TAlias_Name = 'system'
 BUILD_SYSTEM_NAME: TAlias_Name = 'meson'
 
-BUILD_SYSTEM_CONF_FILE_EXTENSION: TAlias_Name = 'build'
+BUILD_SYSTEM_CONF_FILE_EXTENSION: TAlias_Name = BUILD_STR
 BUILD_SYSTEM_CONF_FILE_NAME: TAlias_Name = f'{BUILD_SYSTEM_NAME}.{BUILD_SYSTEM_CONF_FILE_EXTENSION}'
