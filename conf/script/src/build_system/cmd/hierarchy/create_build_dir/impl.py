5ad7f7aa4e17037b7be5caa1b42cc14e964ba7b8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-26 10:38:36 -0400

    WIP : Better usage of `find_build_dir` cmd inside `create_build_dir` cmd

--- conf/script/src/build_system/cmd/hierarchy/create_build_dir/impl.py
+++ conf/script/src/build_system/cmd/hierarchy/create_build_dir/impl.py
@@ -10,13 +10,10 @@ from ..find_build_dir import *
 
 
 def create_build_dir(root_dir: Optional[Path] = None) -> Path:
-    root_dir = find_or_verify_root_dir(unverified_root_dir=root_dir)
-    build_dir = get_build_dir_path(root_dir=root_dir)
-
-    if build_dir.exists():
-        if not build_dir.is_dir():
-            raise BuildDirNotDirError()
-    else:
+    try:
+        build_dir = find_build_dir(root_dir=root_dir)
+    except BuildDirNotFoundError:
+        # PASS INFO TO EXCEPTION!!!!!!
         build_dir.mkdir(mode=BUILD_DIR_PERMISSIONS, parents=True)
 
     return build_dir
