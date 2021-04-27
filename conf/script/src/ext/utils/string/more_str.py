ba17a32745548f42c359cad054411987967a21f8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-27 16:54:11 -0400

    Optimized `cast_any_str` func for *Sanic* speed

--- conf/script/src/ext/utils/string/more_str.py
+++ conf/script/src/ext/utils/string/more_str.py
@@ -1,19 +1,13 @@
 __all__ = ['cast_any_str']
 
-from typing import AnyStr, Union
+from typing import AnyStr, Union, cast
 
 
 def cast_any_str(target_cls: type[AnyStr], src_any_str: Union[str, bytes], encoding: str = 'utf-8') -> AnyStr:
-    """
-    Casts an AnyStr into the supplied target AnyStr concrete class.
-
-    A new instance is always returned, even if the source AnyStr object's class and the target AnyStr concrete class are the same.
-    """
-
     casted_any_str: AnyStr
 
     if isinstance(src_any_str, target_cls):
-        casted_any_str = target_cls(src_any_str)
+        casted_any_str = cast(target_cls, src_any_str)
     else:
         casted_any_str = target_cls(src_any_str, encoding)
 
