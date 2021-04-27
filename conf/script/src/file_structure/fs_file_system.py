b930e4bfe3f155c421fe106c038a2a0c499d3094
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-27 17:03:16 -0400

    Replace  utf-8 usage from`fs_file_system.py`

--- conf/script/src/file_structure/fs_file_system.py
+++ conf/script/src/file_structure/fs_file_system.py
@@ -1,9 +1,5 @@
-__all__ = ['BUILD_DIR_PERMISSIONS',
-           'UTF_8']
-
-from encodings import utf_8
+__all__ = ['BUILD_DIR_PERMISSIONS']
 
 from ._type_alias import *
 
 BUILD_DIR_PERMISSIONS: Final[int] = 0o770
-UTF_8: TAlias_Name = utf_8.getregentry().name
