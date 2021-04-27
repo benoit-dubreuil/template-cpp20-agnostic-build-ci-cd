6dfba9cd5209560191ebca6e34857a99be763c08
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-27 17:11:07 -0400

    Added the `TUnion_Path` constant

--- conf/script/src/ext/utils/path/path_typing.py
+++ conf/script/src/ext/utils/path/path_typing.py
@@ -9,8 +9,9 @@ from typing import TypeVar, Union
 
 from ..string import *
 
-TUnion_PathLike = Union[Path, os.PathLike, TUnion_AnyStr]
-TUnion_AnyPath = Union[Path, os.PathLike, TUnion_AnyStr, type(None)]
+TUnion_Path = Union[Path, os.PathLike]
+TUnion_PathLike = Union[TUnion_Path, TUnion_AnyStr]
+TUnion_AnyPath = Union[TUnion_PathLike, type(None)]
 
 T_PathLike = TypeVar("T_PathLike", Path, os.PathLike, TUnion_AnyStr)
 T_AnyPath = TypeVar("T_AnyPath", Path, os.PathLike, TUnion_AnyStr, type(None))
