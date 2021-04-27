346287b4c1ee6527b6cea561d94dfbd95894d918
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-27 11:44:45 -0400

    Supply class instead of instance to func

--- conf/script/src/host/env/env_var.py
+++ conf/script/src/host/env/env_var.py
@@ -76,9 +76,8 @@ class EnvVar(GenericClassProxyInjectorMixin, Mapping[T_Env_Key, TAlias_Env_Value
         return f'{self.get_env_key()}={joined_values}'
 
     @staticmethod
-    def __get_env_values_sep(joined_values: AnyStr):
-        generic_any_str = type(joined_values)
-        return generic_any_str(os.pathsep)
+    def __get_env_values_sep(joined_values_cls: type[AnyStr]) -> AnyStr:
+        return joined_values_cls(os.pathsep)
 
     def cast_values_to_any_str(self, str_cls: type[AnyStr] = AnyStr) -> list[AnyStr]:
         return [str_cls(value) for value in self.get_env_values()]
