50edf20d6a8aab797cfe94583421dc7043152273
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-27 17:04:06 -0400

    Integrated `encoding_names.py` locally

--- conf/script/src/ext/utils/string/more_str.py
+++ conf/script/src/ext/utils/string/more_str.py
@@ -2,10 +2,11 @@ __all__ = ['cast_any_str']
 
 from typing import AnyStr, cast
 
+from .encoding_names import *
 from .str_typing import *
 
 
-def cast_any_str(target_cls: type[AnyStr], src_any_str: TUnion_AnyStr, encoding: str = 'utf-8') -> AnyStr:
+def cast_any_str(target_cls: type[AnyStr], src_any_str: TUnion_AnyStr, encoding: str = UTF_8) -> AnyStr:
     casted_any_str: AnyStr
 
     if isinstance(src_any_str, target_cls):
