eb0f2c8653645de9bb582df1a9d7316829d89fda
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-27 17:05:30 -0400

    Removed useless constants

--- conf/script/src/ext/utils/path/path_typing.py
+++ conf/script/src/ext/utils/path/path_typing.py
@@ -1,6 +1,4 @@
-__all__ = ['TConstraints_PathLike',
-           'TConstraints_AnyPath',
-           'TUnion_PathLike',
+__all__ = ['TUnion_PathLike',
            'TUnion_AnyPath',
            'T_PathLike',
            'T_AnyPath']
@@ -10,9 +8,6 @@ from pathlib import Path
 from typing import TypeVar, Union
 
 from ..string import *
-
-TConstraints_PathLike = (Path, os.PathLike, TUnion_AnyStr)
-TConstraints_AnyPath = (Path, os.PathLike, TUnion_AnyStr, type(None))
 
 TUnion_PathLike = Union[Path, os.PathLike, TUnion_AnyStr]
 TUnion_AnyPath = Union[Path, os.PathLike, TUnion_AnyStr, type(None)]
