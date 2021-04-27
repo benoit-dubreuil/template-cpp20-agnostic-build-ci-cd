367d28318c8e74189780abeae8334e19a8860280
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-27 10:34:46 -0400

    Optimized import

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -2,11 +2,11 @@ __all__ = ['T_EnvVar',
            'T_Key',
            'T_Single_Value']
 
-import typing
+from typing import AnyStr, TypeVar
 
 from ext.utils.path import *
 
-T_EnvVar = typing.TypeVar('T_EnvVar', bound='EnvVar')
+T_EnvVar = TypeVar('T_EnvVar', bound='EnvVar')
 
-T_Key = typing.AnyStr
+T_Key = AnyStr
 T_Single_Value = T_PathLike
