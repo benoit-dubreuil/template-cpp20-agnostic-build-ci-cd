708951a157ad6929ade9f55a2e919ff31b975b02
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-28 18:47:07 -0400

    Added simple tests

--- conf/script/src/test/test_host/test_env/test_env_var.py
+++ conf/script/src/test/test_host/test_env/test_env_var.py
@@ -3,16 +3,20 @@
 __all__ = ['TestEnvVar']
 
 import unittest
-from typing import Final
-from unittest import mock
-from unittest.mock import MagicMock
+from pathlib import Path
+from os import PathLike
 
-import host.env.env_var
+from host.env.env_var import *
 from ext.meta_prog.introspection import *
 
 
 class TestEnvVar(unittest.TestCase):
-    ...
+
+    def test_ref_cls_no_generics(self):
+        _ = EnvVar
+
+    def test_ref_cls_valid_generics(self):
+        _ = EnvVar[str, str]
 
 
 if is_caller_main():
