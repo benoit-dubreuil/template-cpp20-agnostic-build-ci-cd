3f145998f7adfb5e432406626a42d4ae86991fbe
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-30 11:11:42 -0400

    Use `T_PurePathLike` in `more_path.py`

--- conf/script/src/ext/utils/path/more_path.py
+++ conf/script/src/ext/utils/path/more_path.py
@@ -12,8 +12,8 @@ def is_dir_empty(dir_path: Path) -> bool:
     return not any(dir_path.iterdir())
 
 
-def cast_path_like(target_cls: type[T_PathLike], src_path_like: TUnion_PathLike, encoding: str = UTF_8) -> T_PathLike:
-    casted_path_like: T_PathLike
+def cast_path_like(target_cls: type[T_PurePathLike], src_path_like: TUnion_PathLike, encoding: str = UTF_8) -> T_PurePathLike:
+    casted_path_like: T_PurePathLike
 
     if isinstance(src_path_like, target_cls):
         casted_path_like = cast(target_cls, src_path_like)
@@ -23,6 +23,6 @@ def cast_path_like(target_cls: type[T_PathLike], src_path_like: TUnion_PathLike,
         if isinstance(src_path_like, Path):
             src_path_like = str(src_path_like)
 
-        casted_path_like = cast_any_str(target_cls=target_cls, src_any_str=src_path_like)
+        casted_path_like = cast_any_str(target_cls=target_cls, src_any_str=src_path_like, encoding=encoding)
 
     return casted_path_like
