ff3b7a1f5847c27395e7fd0c40809a5b4fecc1a4
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-30 15:15:04 -0400

    Reorder

--- conf/script/src/test/test_host/test_env/test_env_var.py
+++ conf/script/src/test/test_host/test_env/test_env_var.py
@@ -5,29 +5,12 @@ __all__ = ['TestEnvVar']
 import unittest
 from typing import Callable, Optional
 
-from ext.meta_prog.introspection import *
+from ext.meta_prog.introspection.caller import *
 from host.env.env_var import *
 from .env_var_test_param_data import EnvVarTestParamData as test_data
-from ext.meta_prog.introspection.caller import *
 
 
 class TestEnvVar(unittest.TestCase):
-
-    def test_ref_cls_no_generics(self):
-        _ = EnvVar
-
-    @staticmethod
-    def __for_valid_generic_types(test_func: Callable[[type, type], None]):
-        for key_type in test_data.valid_key_types:
-            for values_type in test_data.valid_values_data_by_type:
-                test_func(key_type, values_type)
-
-    def __with_valid_generic_types(self, test_func: Callable[[type, type], None], msg: Optional[str] = None):
-        def wrap_subtest(key_type: type, values_type: type):
-            with self.subTest(msg=msg, key_type=key_type, values_type=values_type):
-                test_func(key_type, values_type)
-
-        self.__for_valid_generic_types(wrap_subtest)
 
     def test_ref_cls_valid_generic_types(self):
         def _test_impl(key_type: type, values_type: type):
@@ -52,6 +35,22 @@ class TestEnvVar(unittest.TestCase):
         with self.assertRaises(TypeError):
             _ = EnvVar(values=[])
 
+    def test_ref_cls_no_generics(self):
+        _ = EnvVar
+
+    @staticmethod
+    def __for_valid_generic_types(test_func: Callable[[type, type], None]):
+        for key_type in test_data.valid_key_types:
+            for values_type in test_data.valid_values_data_by_type:
+                test_func(key_type, values_type)
+
+    def __with_valid_generic_types(self, test_func: Callable[[type, type], None], msg: Optional[str] = None):
+        def wrap_subtest(key_type: type, values_type: type):
+            with self.subTest(msg=msg, key_type=key_type, values_type=values_type):
+                test_func(key_type, values_type)
+
+        self.__for_valid_generic_types(wrap_subtest)
+        
     # TODO
     # def test_constructor_valid_generics_no_args_raises(self):
     #     with self.assertRaises(TypeError):
