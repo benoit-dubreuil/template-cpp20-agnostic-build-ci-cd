d17cfbb8db6fd7646d545aceddad54db80efa3ec
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-30 11:11:21 -0400

    Use `T_PurePathLike` in `EnvVar`

--- conf/script/src/host/env/env_var_fwd.py
+++ conf/script/src/host/env/env_var_fwd.py
@@ -10,6 +10,6 @@ from ext.utils.path import *
 T_EnvVar = TypeVar('T_EnvVar', bound='EnvVar')
 
 T_Env_Key = AnyStr
-T_Env_Single_Val = T_PathLike
+T_Env_Single_Val = T_PurePathLike
 
 TAlias_Env_Values = list[T_Env_Single_Val]
