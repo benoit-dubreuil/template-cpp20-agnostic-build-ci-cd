954e21993d727da2ce9c367e2bd47d6d8b1a7fb3
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-30 15:05:38 -0400

    Added helper funcs

--- conf/script/src/test/test_host/test_env/test_env_var.py
+++ conf/script/src/test/test_host/test_env/test_env_var.py
@@ -3,10 +3,11 @@
 __all__ = ['TestEnvVar']
 
 import unittest
+from typing import Callable, Optional
 
 from ext.meta_prog.introspection import *
 from host.env.env_var import *
-from .env_var_test_param_data import *
+from .env_var_test_param_data import EnvVarTestParamData as test_data
 
 
 class TestEnvVar(unittest.TestCase):
@@ -14,7 +15,18 @@ class TestEnvVar(unittest.TestCase):
     def test_ref_cls_no_generics(self):
         _ = EnvVar
 
-    def test_ref_cls_valid_generics(self):
+    @staticmethod
+    def __for_valid_generic_types(test_func: Callable[[type, type], None]):
+        for key_type in test_data.valid_key_types:
+            for values_type in test_data.valid_values_data_by_type:
+                test_func(key_type, values_type)
+
+    def __with_valid_generic_types(self, test_func: Callable[[type, type], None], msg: Optional[str] = None):
+        def wrap_subtest(key_type: type, values_type: type):
+            with self.subTest(msg=msg, key_type=key_type, values_type=values_type):
+                test_func(key_type, values_type)
+
+        self.__for_valid_generic_types(wrap_subtest)
         _ = EnvVar[str, str]
 
     def test_constructor_no_generics_no_args_raises(self):
