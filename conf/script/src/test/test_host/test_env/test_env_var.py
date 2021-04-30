ef0fe16c63fc2907873c67639b37c841a0d0e9c1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-30 11:49:33 -0400

    Extracted test param data

--- conf/script/src/test/test_host/test_env/test_env_var.py
+++ conf/script/src/test/test_host/test_env/test_env_var.py
@@ -3,91 +3,13 @@
 __all__ = ['TestEnvVar']
 
 import unittest
-from os import pathsep
-from pathlib import PurePath
-from typing import Any, Final
 
 from ext.meta_prog.introspection import *
-from ext.utils.string import *
 from host.env.env_var import *
+from .test_env_var_param_data import *
 
 
 class TestEnvVar(unittest.TestCase):
-    __TAlias_param_types = Final[list[type]]
-    __TAlias_param_data_by_type = Final[dict[type: list[Any]]]
-    __TAlias_param_data_str = Final[list[str]]
-    __TAlias_param_data_path = Final[list[PurePath]]
-
-    __valid_key_types: __TAlias_param_types = [str, bytes]
-    __valid_values_types: __TAlias_param_types = [str, bytes, PurePath]
-
-    __invalid_key_types: __TAlias_param_types = [type(None), int, bool, float]
-    __invalid_values_types: __TAlias_param_types = [type(None), int, bool, float]
-
-    __valid_key_data_str: __TAlias_param_data_str = ['', 'key', 'test', '123', 'KEY', 'key_', '_key', 'z-abc', 'space space']
-
-    __valid_values_windows_path_data_str: __TAlias_param_data_str = [
-        'C:\\',
-        'C:\\Users',
-        'C:\\Users\\',
-        'C:\\Users\\Public',
-        'C:\\Users\\Public\\',
-        'C:\\Users\\Public\\wow.doge',
-        '.\\',
-        '.\\Users',
-        '.\\Users\\',
-        '.\\Users\\Public',
-        '.\\Users\\Public\\',
-        '.\\Users\\Public\\wow.doge']
-    __valid_values_unix_path_data_str: __TAlias_param_data_str = [
-        '/usr',
-        '/usr/',
-        '/usr/tmp',
-        '/usr/tmp/',
-        '~']
-    __valid_values_data_str: __TAlias_param_data_str = \
-        __valid_key_data_str + \
-        [f'%{key_str}%' for key_str in __valid_key_data_str] + \
-        __valid_values_windows_path_data_str + \
-        __valid_values_unix_path_data_str + \
-        ['val1;',
-         'val1;val2',
-         'val1;val2;',
-         'many_postfix_sep;;;;',
-         ';;;;;many_prefix_sep']
-    __valid_values_data_path: __TAlias_param_data_path = \
-        [PurePath(values_path_str) for values_path_str in __valid_values_windows_path_data_str] + \
-        [PurePath(values_path_str) for values_path_str in __valid_values_unix_path_data_str]
-
-    __valid_joined_values_data_str: __TAlias_param_data_str = [
-        pathsep.join(__valid_key_data_str),
-        pathsep.join(__valid_values_windows_path_data_str),
-        pathsep.join(__valid_values_unix_path_data_str)]
-
-    __valid_key_data_by_type: __TAlias_param_data_by_type = {
-        str: __valid_key_data_str,
-        bytes: [key_str.encode(UTF_8) for key_str in __valid_key_data_str]
-    }
-
-    __valid_values_data_by_type: __TAlias_param_data_by_type = {
-        str: __valid_values_data_str,
-        bytes: [values_str.encode(UTF_8) for values_str in __valid_values_data_str],
-        PurePath: []
-    }
-
-    __invalid_key_data_by_type: __TAlias_param_data_by_type = {
-        type(None): [],
-        int: [],
-        bool: [],
-        float: []
-    }
-
-    __invalid_values_data_by_type: __TAlias_param_data_by_type = {
-        type(None): [],
-        int: [],
-        bool: [],
-        float: []
-    }
 
     def test_ref_cls_no_generics(self):
         _ = EnvVar
@@ -110,6 +32,10 @@ class TestEnvVar(unittest.TestCase):
     def test_constructor_no_generics_only_empty_values_arg_raises(self):
         with self.assertRaises(TypeError):
             _ = EnvVar(values=[])
+
+    def test_constructor_valid_generics_no_args_raises(self):
+        with self.assertRaises(TypeError):
+            _ = EnvVar[]()
 
 
 if is_caller_main():
