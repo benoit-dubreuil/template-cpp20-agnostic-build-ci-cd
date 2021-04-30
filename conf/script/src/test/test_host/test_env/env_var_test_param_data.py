4f3d970c661454131c56d735439d80ef96352508
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-04-30 14:21:01 -0400

    Made some transitional vars private

--- conf/script/src/test/test_host/test_env/env_var_test_param_data.py
+++ conf/script/src/test/test_host/test_env/env_var_test_param_data.py
@@ -24,8 +24,8 @@ class EnvVarTestParamData:
     invalid_values_types: __TAlias_param_types = [type(None), int, bool, float]
     invalid_joined_values_types: __TAlias_param_types = [type(None), int, bool, float, PurePath]
 
-    valid_key_data_str: __TAlias_param_data_str = ['', 'key', 'test', '123', 'KEY', 'key_', '_key', 'z-abc', 'space space']
-    valid_values_windows_path_data_str: __TAlias_param_data_str = [
+    __valid_key_data_str: __TAlias_param_data_str = ['', 'key', 'test', '123', 'KEY', 'key_', '_key', 'z-abc', 'space space']
+    __valid_values_windows_path_data_str: __TAlias_param_data_str = [
         'C:\\',
         'C:\\Users',
         'C:\\Users\\',
@@ -38,42 +38,42 @@ class EnvVarTestParamData:
         '.\\Users\\Public',
         '.\\Users\\Public\\',
         '.\\Users\\Public\\wow.doge']
-    valid_values_unix_path_data_str: __TAlias_param_data_str = [
+    __valid_values_unix_path_data_str: __TAlias_param_data_str = [
         '/usr',
         '/usr/',
         '/usr/tmp',
         '/usr/tmp/',
         '~']
-    valid_values_data_str: __TAlias_param_data_str = \
-        valid_key_data_str + \
-        [f'%{key_str}%' for key_str in valid_key_data_str] + \
-        valid_values_windows_path_data_str + \
-        valid_values_unix_path_data_str + \
+    __valid_values_data_str: __TAlias_param_data_str = \
+        __valid_key_data_str + \
+        [f'%{key_str}%' for key_str in __valid_key_data_str] + \
+        __valid_values_windows_path_data_str + \
+        __valid_values_unix_path_data_str + \
         ['val1;',
          'val1;val2',
          'val1;val2;',
          'many_postfix_sep;;;;',
          ';;;;;many_prefix_sep']
-    valid_values_data_path: __TAlias_param_data_path = \
-        [PurePath(values_path_str) for values_path_str in valid_values_windows_path_data_str] + \
-        [PurePath(values_path_str) for values_path_str in valid_values_unix_path_data_str]
-    valid_joined_values_data_str: __TAlias_param_data_str = [
-        pathsep.join(valid_key_data_str),
-        pathsep.join(valid_values_windows_path_data_str),
-        pathsep.join(valid_values_unix_path_data_str)]
+    __valid_values_data_path: __TAlias_param_data_path = \
+        [PurePath(values_path_str) for values_path_str in __valid_values_windows_path_data_str] + \
+        [PurePath(values_path_str) for values_path_str in __valid_values_unix_path_data_str]
+    __valid_joined_values_data_str: __TAlias_param_data_str = [
+        pathsep.join(__valid_key_data_str),
+        pathsep.join(__valid_values_windows_path_data_str),
+        pathsep.join(__valid_values_unix_path_data_str)]
 
-    valid_key_data_by_type: __TAlias_param_data_by_type = {
-        str: valid_key_data_str,
-        bytes: [key_str.encode(UTF_8) for key_str in valid_key_data_str]
+    __valid_key_data_by_type: __TAlias_param_data_by_type = {
+        str: __valid_key_data_str,
+        bytes: [key_str.encode(UTF_8) for key_str in __valid_key_data_str]
     }
-    valid_values_data_by_type: __TAlias_param_data_by_type = {
-        str: valid_values_data_str,
-        bytes: [values_str.encode(UTF_8) for values_str in valid_values_data_str],
-        PurePath: valid_values_data_path
+    __valid_values_data_by_type: __TAlias_param_data_by_type = {
+        str: __valid_values_data_str,
+        bytes: [values_str.encode(UTF_8) for values_str in __valid_values_data_str],
+        PurePath: __valid_values_data_path
     }
-    valid_joined_values_data_by_type: __TAlias_param_data_by_type = {
-        str: valid_joined_values_data_str,
-        bytes: [values_str.encode(UTF_8) for values_str in valid_joined_values_data_str],
+    __valid_joined_values_data_by_type: __TAlias_param_data_by_type = {
+        str: __valid_joined_values_data_str,
+        bytes: [values_str.encode(UTF_8) for values_str in __valid_joined_values_data_str],
     }
 
     # TODO
